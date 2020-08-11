8d04e433f43f6f5d7ddddea5479821258ef12d76
Ben Winks <ben.winks@icap.irad>
2020-08-11 10:26:38 -0400

    Committing changes to the documentation of the code including return type hints and function documentation for common.py

--- pipeline/common.py
+++ pipeline/common.py
@@ -9,22 +9,44 @@ IMAGE_DATETIME = str(datetime.utcnow()).replace(' ', 'T')
 _no_version_warning = "Version number must be set - example: '1.0' || Filename is incomplete without a version value"
 
 
-def get_publish_base_url():
-    # PREVIOUSLY remoteReportDirectory equalled datetime_BUILDNUMBER
+def get_basename(path) -> str:
+    """
+    A simple function that will return the basename of a file/image given a specific path
+    Return: String
+    """
+    return path.split('/')[-1]
+
+
+def get_publish_base_url() -> str:
+    """
+    Function that returns the base URL for artifacts
+    This was altered slightly from the Groovy implementation due to the JOB_ID GitLab CI variable which replaces the Jenkins BUILDNUMBER
+    Return: String
+    """
     return f"{DCAR_URL}/{S3_REPORT_BUCKET}/{get_root_path()}/{get_datetime()}_{JOB_ID}"
 
 
-# Made this a function because there were multiple places where the datetime was used
-# in the replaced groovy code, so this way the timestamp is consistent for each job
-def get_datetime():
+def get_datetime() -> str:
+    """
+    Function returns the global IMAGE_DATETIME so that all usage of the datetime will be of consistent value
+    Return: String
+    """
     return IMAGE_DATETIME
 
 
-def get_root_path():
+def get_root_path() -> str:
+    """
+    Function that returns the project-specific bucket path, whether that be testing or production
+    Return: String
+    """
     return f"{get_base_bucket_directory()}/{PROJECT_NAME}"
 
 
-def get_base_bucket_directory():
+def get_base_bucket_directory() -> str:
+    """
+    Function that returns the bucket directory for a project, dependent on if it is a production branch (Master/Development) or not
+    Return: String
+    """
     try:
         path = "container-scan-reports"
         if is_production_branch(False):
@@ -35,20 +57,27 @@ def get_base_bucket_directory():
         print(e)
 
 
-def is_production_branch(echo=True):
+def is_production_branch(echo=True) -> bool:
+    """
+    Function that returns True/False if the branch is a production branch
+    If the branch is Master/Development then it is a production branch
+    Otherwise, returns False
+    Args: echo - This bool will determine if the status is printed to the console or not, defaults to True
+    Return: bool
+    """
     # CI_COMMIT_REF_NAME
     # CI_COMMIT_BRANCH
     production = (CURRENT_BRANCH.lower() == "master") or (CURRENT_BRANCH.lower() == "development")
     if echo:
-        print(f"is_production_branch: {CURRENT_BRANCH} {'YES' if production else 'NO'}")
+        print(f"is_production_branch: {CURRENT_BRANCH} - {'YES' if production else 'NO'}")
     return production
 
 
-def get_basename(path):
-    return path.split('/')[-1]
-
-
-def get_git_url():
+def get_git_url() -> str:
+    """
+    Function will return the constant GITLAB_URL unless it is not set
+    Return: String
+    """
     try:
         return GITLAB_URL
     except Exception as e:
@@ -56,11 +85,20 @@ def get_git_url():
 
 
 def set_image_version(version):
+    """
+    Function will hold the value of the current image version and expose its value to all functions when needed
+    This was necessary due to the Jenkins context no longer being necessary
+    """
     global IMAGE_VERSION
     IMAGE_VERSION = version
 
 
-def get_tar_filename():
+def get_tar_filename() -> str:
+    """
+    Funciton that will return the filename for the tarball for the project
+    NOTE: Function will raise a TypeError if there is no IMAGE_VERSION set
+    Return: String
+    """
     try:
         if IMAGE_VERSION is not None:
             return f"{PROJECT_NAME}-{IMAGE_VERSION}-reports-signature.tar.gz"
@@ -70,7 +108,12 @@ def get_tar_filename():
         print(e)
 
 
-def get_image_signature_filename():
+def get_image_signature_filename() -> str:
+    """
+    Function will return the signature file's filename
+    NOTE: Function will raise a TypeError if there is no IMAGE_VERSION set
+    Return: String
+    """
     try:
         if IMAGE_VERSION is not None:
             return f"{PROJECT_NAME}-{IMAGE_VERSION}.sig"
@@ -80,7 +123,12 @@ def get_image_signature_filename():
         print(e)
 
 
-def get_image_filename():
+def get_image_filename() -> str:
+    """
+    Function will return the filename of the tar that will be used by "docker load -i"
+    NOTE: Function will raise a TypeError if there is no IMAGE_VERSION set
+    Return: String
+    """
     try:
         if IMAGE_VERSION is not None:
             return f"{PROJECT_NAME}-{IMAGE_VERSION}.tar"
@@ -90,7 +138,12 @@ def get_image_filename():
         print(e)
 
 
-def get_version():
+def get_version() -> str:
+    """
+    Function that returns the image version number if it has been set
+    NOTE: Function will raise a TypeError if there it no IMAGE_VERSION set
+    Return: String
+    """
     try:
         if IMAGE_VERSION is not None:
             return IMAGE_VERSION
@@ -100,21 +153,30 @@ def get_version():
         print(e)
 
 
-def get_lockname():
-    # returned the lockname if it was set, which was set to the GIT_URL
-    return GITLAB_URL
+def get_lockname() -> str:
+    """
+    Function will return the "lockname", which in the groovy implementation was set to the GIT_URL which is now the CI variable CI_REPOSITORY_URL 
+    NOTE: This CI variable is subject to be swapped upon confirmation of what the original GIT_URL was
+    Return: String
+    """
+    return LOCK_URL
 
 
-def get_simple_image_path():
+def get_simple_image_path() -> str:
+    """
+    Function will return the simple image path ex. sonarqube/sonarqube to hide details of the layout of the internal registry
+    Return: String
+    """
     return f"{PROJECT_NAME}/{PROJECT_NAME}"
 
 
-def get_tag():
+def get_tag() -> str:
     """
     Get tag will return image tags like:
     master branch:              1.2.3
     development branch:         1.2.3-development
     contributor branches:       1.2.3-testing
+    Return: String
     """
     if CURRENT_BRANCH.lower() == 'development':
         return f"{get_version()}-development"
@@ -125,10 +187,11 @@ def get_tag():
         return f"{get_version()}-testing"
 
 
-def get_public_image_tag():
+def get_public_image_tag() -> str:
     """
     Image path and tag for the internal registry details are removed.
     This is for tagging images before they are exported to a file so we
     don't export the internal registy service address with the image.
+    Return: String
     """
     return f"{get_simple_image_path()}:{get_tag()}"
