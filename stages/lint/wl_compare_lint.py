fb5549589916e0da1388ec6eb9c4c679ba88875f
David Freeman <davidfreeman@seed-innovations.com>
2020-08-25 13:03:42 -0600

    update im_name var before calling funcs

--- stages/lint/wl_compare_lint.py
+++ stages/lint/wl_compare_lint.py
@@ -22,6 +22,8 @@ def main():
   gitlab_key = args.glkey
   wl_branch = args.wlbranch
 
+  im_name = '/'.join(im_name.split('/')[1::])
+
   # Make sure image name follows convention of depth of three directories e.g. 'redhat/ubi/ubi8'
   # If not, throw error
   # check_image_name_length(im_name)
@@ -41,8 +43,7 @@ def main():
 def does_image_exist(proj, im_name, im_tag, wl_branch):
   filename = get_whitelist_filename(proj, im_name, im_tag)
   wl = get_whitelist_file_contents(proj, filename, wl_branch)
-  im_name = '/'.join(im_name.split('/')[1::])
-  if wl['image_name'] != im_name[1::] or wl['image_tag'] != im_tag:
+  if wl['image_name'] != im_name or wl['image_tag'] != im_tag:
     print("Whitelist retrieval error. Check that the project's GitLab reponame matches the whitelist's image name and that the version in the Jenkinsfile matches the whitelist's image tag.\nRepo name and Jenkinsfile version: " + im_name + ":" + im_tag + "\nWhitelist image_name and image_tag: " + wl['image_name'] + ":" + wl['image_tag'], file=sys.stderr)
     sys.exit(1)
   return
@@ -64,8 +65,8 @@ def get_complete_whitelist_for_image(proj, im_name, im_tag, wl_branch):
   return
 
 def get_whitelist_filename(project, im_name, im_tag):
-  dccscr_project = im_name.split('/')[1::]
-  greylist_name = dccscr_project[-1] + '.greylist'
+  dccscr_project = im_name.split('/')
+  greylist_name = im_name[-1] + '.greylist'
   dccscr_project.append(greylist_name)
   filename = '/'.join(dccscr_project)
   return filename
