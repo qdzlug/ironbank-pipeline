c7dd99039d2f7e9b56e26ed04063fd5dc20bf078
Tim Seagren <seagren.tim@solute.local>
2020-09-08 12:27:51 -0700

    adding s3_upload.py

---
+++ stages/publish/s3_upload.py
@@ -0,0 +1,53 @@
+import logging
+import boto3
+import os
+import argparse
+import datetime
+from botocore.exceptions import ClientError
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    
+    access_key = os.environ["S3_ACCESS_KEY"]
+    secret_key = os.environ["S3_SECRET_KEY"]
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=access_key,
+                             aws_secret_access_key=secret_key
+                      )
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name, 
+                                         ExtraArgs={
+						    'ACL':'x-amz-acl:private',  
+                                                    'ContentType': 'application/octet-stream'
+                                                   }
+                                        )
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description = 'Uploading various reports and files to DCCSCR S3')
+
+    parser.add_argument('--filename',   help='File to upload')
+    parser.add_argument('--bucket',  help='Bucket to upload to')
+    parser.add_argument('--dest-path',   help='S3 object path')
+    args = parser.parse_args()
+
+    upload_file(filename = args.filename,
+                bucket   = args.bucket,
+                destname = args.dest-name)
