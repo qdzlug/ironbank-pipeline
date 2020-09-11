6c22e4c7302be1534b331636f81cec577d3bd2f9
James Petersen <jpetersenames@gmail.com>
2020-09-10 20:12:41 -0600

    refactor anchore to use class
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/scanning/anchore_scan.py
+++ stages/scanning/anchore_scan.py
@@ -1,278 +1,167 @@
-#!/usr/bin/python3
-import argparse
+#!/usr/bin/env python3
 import re
 import requests
 import json
-import csv
-import subprocess
-
-# Hopefully this is there
-# TODO: Import only needed components
 import os
-
-#
-## Creates JSON reports for all image vulnerabilities
-# support both python2 and python3 url encoding modules
-#
-try:
-    from urllib.parse import quote_plus,unquote_plus, quote, unquote
-except:
-    from urllib import quote_plus,unquote_plus, quote, unquote
-
-#
-# Get the arguments
-#
-parser = argparse.ArgumentParser(description="Anchore Image Vuln Report Generator")
-parser.add_argument('--username', metavar='user', default='admin', help='Anchore admin username (default=admin)')
-parser.add_argument('--password', metavar='pass', default='foobar', help='Anchore admin password (default=foobar)')
-parser.add_argument('--url', metavar='url', default='http://localhost:8228/v1/', help='Anchore Engine API Service endpoint URL (default=http://localhost:8228/v1/)')
-parser.add_argument('--verify', metavar='verify', default=True, help='Accept self-signed certificates when using TLS/https for anchore endpoint')
-parser.add_argument('--image', metavar='image', default='none', help='Full image tag to get vulnerability info. ex. docker.io/library/alpine:latest')
-parser.add_argument('--output', metavar='output', default='.', help='Output directory for reports.')
-
-args = parser.parse_args()
-endpoint_url = re.sub("\/+$", '', args.url)
+import sys
 
 
-#
-# Get image digest
-#
-def getImageDigest():
-    request_url = f"{endpoint_url}/images"
-    payload = {'fulltag': args.image, 'history': 'false'}
 
-    try:
-        r = requests.get(
-                request_url,
-                auth=(args.username, args.password),
-                params=payload,
-                verify=args.verify
-        )
-        body = r.text
+class Anchore():
+    """
+    Anchore Scanner
 
-        if r.status_code == 200:
-            # test that the response is valid JSON
-            try:
-                image_list = json.loads(body)
-                image_digest = image_list[0]["imageDigest"]
-                print("Anchore Image Digest = " + image_digest)
-                if image_digest == None:
-                    raise Exception("Image Digest does not Exist")
-                return image_digest
-            except:
-                raise Exception("Got 200 response but is not valid JSON")
-        else:
-            raise Exception("Non-200 response recieved from Anchore " + str(r.status_code) + " - " + r.text)
+    """
 
-    except Exception as err:
-        raise err
+    def __init__(self, url, username, password, verify, image, output, debugon = True):
+        self.url      = url
+        self.username = username
+        self.password = password
+        self.verify   = verify
+        self.image    = image
+        self.output   = output
+        self.debugon  = debugon
+        self.digest   = self.__get_image_digest()
 
 
-#
-# Get vulnerabilities for image
-#
-def get_vulndb(report_dir, digest):
-    request_url = f"{endpoint_url}/images/{digest}/vuln/all"
+    """
 
-    try:
-        r = requests.get(
-                request_url,
-                auth=(args.username, args.password),
-                verify=args.verify
-        )
-        body = r.text
+    """
+    def __debug(self, msg):
+        if self.debugon:
+            print(msg)
 
-        if r.status_code == 200:
-            # test that the response is valid json
-            try:
-                vuln_dict = json.loads(body)
-                for vulnerability in vuln_dict['vulnerabilities']:
-                    # If VulnDB record found, retrive set of reference URLs associated with the record.
-                    if (vulnerability["feed_group"] == "vulndb:vulnerabilities"):
-                        # "http://anchore-anchore-engine-api:8228/v1" or URL to replace may need to be modified when changes to the Anchore installation occur
-                        vulndb_request_url = re.sub("http:\/\/([a-z-_0-9:]*)\/v1", endpoint_url, vulnerability["url"])
-                        r = requests.get(vulndb_request_url, auth=(args.username, args.password))
-                        body = r.text
+    """
 
-                        if r.status_code == 200:
-                        # test that the response is valid json
-                            try:
-                                vulndb_dict = json.loads(body)
-                                for vulndb_vuln in vulndb_dict["vulnerabilities"]:
-                                    vulnerability['url'] = vulndb_vuln["references"]
+    """
+    def __get_anchore_api_json(self, url, payload = ""):
+        self.__debug(f"Fetching {url}")
+        try:
+            r = requests.get(
+                    url,
+                    auth   = (self.username, self.password),
+                    params = payload,
+                    verify = self.verify
+            )
+            body = r.text
 
-                            except:
-                                raise Exception("Got 200 response, but data isn't valid JSON")
-                vuln_dict["imageFullTag"] = args.image
-                # Create json report called anchore_vulns_new.json
+            if r.status_code == 200:
+                # test that the response is valid JSON
+                self.__debug(f"Got response from Anchore. Testing if valid json")
                 try:
-                    with open('anchore_security.json', 'w') as fp:
-                        json.dump(vuln_dict, fp)
+                    json.loads(body)
+                except:
+                    raise Exception("Got 200 response but is not valid JSON")
+            else:
+                raise Exception("Non-200 response recieved from Anchore " + str(r.status_code) + " - " + r.text)
+        except Exception as err:
+            raise err
 
-                    with open(os.path.join(report_dir, 'anchore_security.json'), 'w') as fp:
-                        json.dump(vuln_dict, fp)
-
-                except Exception as err:
-                    raise err
-
-            except:
-                raise Exception("Got 200 response, but data isn't valid JSON")
-
-    except Exception as err:
-        # if any report fails, raise the error and failstop program
-        raise err
+        self.__debug(f"Json is valid")
+        return json.loads(body)
 
 
-def get_gates(report_dir, digest):
+    """
 
-    request_url = f"{endpoint_url}/images/{digest}/check?tag={args.image}&detail=true"
+    """
+    def __get_image_digest(self):
+        url = f"{self.url}/images"
+        payload = {'fulltag': self.image, 'history': 'false'}
+        image_list = self.__get_anchore_api_json(url, payload)
+        image_digest = image_list[0]["imageDigest"]
+        if image_digest == None:
+            raise Exception("Image Digest does not Exist")
+        return image_digest
 
-    try:
-        r = requests.get(
-                request_url,
-                auth = (args.username, args.password),
-                verify = args.verify
-        )
-        body = r.text
-        if r.status_code == 200:
+
+
+    def get_version(self):
+        url = f"{self.url}/version"
+        version_json = self.__get_anchore_api_json(url)
+        filename = os.path.join(self.output, "anchore-version.txt")
+        self.__debug(f"Writing to {filename}")
+        with open(filename, "w") as f:
+            json.dump(version_json["service"]["version"], f)
+
+    """
+
+    """
+    def get_vulns(self):
+        try:
+            vuln_dict = self.__get_anchore_api_json(f"{self.url}/images/{self.digest}/vuln/all")
+
+            for vulnerability in vuln_dict['vulnerabilities']:
+                # If VulnDB record found, retrive set of reference URLs associated with the record.
+                if (vulnerability["feed_group"] == "vulndb:vulnerabilities"):
+                    # "http://anchore-anchore-engine-api:8228/v1" or URL to replace may need to be modified when changes to the Anchore installation occur
+                    vulndb_request_url = re.sub("http:\/\/([a-z-_0-9:]*)\/v1", self.url, vulnerability["url"])
+                    vulndb_dict = self.__get_anchore_api_json(vulndb_request_url)
+                    for vulndb_vuln in vulndb_dict["vulnerabilities"]:
+                        vulnerability['url'] = vulndb_vuln["references"]
+
+            vuln_dict["imageFullTag"] = self.image
+            # Create json report called anchore_security.json
             try:
-                body_json = json.loads(body)
-
-                # Save the API response
-                # with open("anchoreengine-api-response-evaluation-1.json", "w") as f:
-                #     f.write(body)
-
-                imageid = body_json[0][digest][args.image][0]["detail"]["result"]["image_id"]
-                results = body_json[0][digest][args.image][0]["detail"]["result"]["result"]
-
-                results_dict = dict()
-
-                # Grab the subset of data used in anchore_gates.json
-                results_dict[imageid] = results[imageid]
-
-                with open(os.path.join(report_dir, "anchore_gates.json"), "w") as f:
-                    json.dump(results_dict, f)
+                filename = os.path.join(self.output, 'anchore_security.json')
+                self.__debug(f"Writing to {filename}")
+                with open(filename, 'w') as fp:
+                    json.dump(vuln_dict, fp)
 
             except Exception as err:
                 raise err
-        else:
-            raise Exception("Non-200 response recieved from Anchore " + str(r.status_code) + " - " + r.text)
 
-    except Exception as err:
-        raise err
-
-
-#
-#
-#
-def get_security(report_dir, digest):
-
-    request_url = f"{endpoint_url}/images/{digest}/vuln/all"
-
-    try:
-        r = requests.get(
-                request_url,
-                auth = (args.username, args.password),
-                verify = args.verify
-        )
-        body = r.text
-        if r.status_code == 200:
-            try:
-                body_json = json.loads(body)
-                print(body_json)
-
-                results_dict = {
-                        "columns": [
-                            { "title": "Tag" },
-                            { "title": "CVE ID" },
-                            { "title": "Severity" },
-                            { "title": "Vulnerability Package" },
-                            { "title": "Fix Available" },
-                            { "title": "URL" }
-                        ],
-                        "data": [ ]
-                    }
-
-                vulns = body_json["vulnerabilities"]
-
-                for i in range(len(vulns)):
-                    try:
-                        results_dict["data"].append([
-                            args.image,
-                            vulns[i]["vuln"],
-                            vulns[i]["severity"],
-                            vulns[i]["package"],
-                            vulns[i]["fix"],
-                            vulns[i]["url"]
-                        ])
-                    except Exception as err:
-                        raise err
-
-                with open(os.path.join(report_dir, "anchore_security.json"), "w") as f:
-                    json.dump(results_dict, f)
-
-            except Exception as err:
-                raise err
-        else:
-            raise Exception("Non-200 response recieved from Anchore " + str(r.status_code) + " - " + r.text)
-
-    except Exception as err:
-        raise err
+        except Exception as err:
+            # if any report fails, raise the error and failstop program
+            raise err
 
 
-#
-#
-#
-def get_version(report_dir):
+    """
 
-    request_url = f"{endpoint_url}/version"
+    """
+    def get_compliance(self):
+        request_url = f"{self.url}/images/{self.digest}/check?tag={self.image}&detail=true"
+        body_json = self.__get_anchore_api_json(request_url)
 
-    try:
-        r = requests.get(
-                request_url,
-                auth = (args.username, args.password),
-                verify = args.verify
-        )
-        body = r.text
-        if r.status_code == 200:
-            try:
-                body_json = json.loads(body)
-                print(f"Anchore Version: {body_json['service']['version']}")
+        # Save the API response
+        filename = os.path.join(self.output, "anchore_api_gates_full.json")
+        self.__debug(f"Writing to {filename}")
+        with open(filename, "w") as f:
+            json.dump(body_json, f)
+        imageid = body_json[0][self.digest]["docker.io/" + self.image][0]["detail"]["result"]["image_id"]
+        results = body_json[0][self.digest]["docker.io/" + self.image][0]["detail"]["result"]["result"]
 
-                with open(os.path.join(report_dir, "anchore-version.txt"), "w") as f:
-                    json.dump(results_dict, f)
+        results_dict = dict()
 
-            except Exception as err:
-                raise err
-        else:
-            raise Exception("Non-200 response recieved from Anchore " + str(r.status_code) + " - " + r.text)
+        # Grab the subset of data used in anchore_gates.json
+        results_dict[imageid] = results[imageid]
 
-    except Exception as err:
-        raise err
+        filename = os.path.join(self.output, "anchore_gates.json")
+        self.__debug(f"Writing to {filename}")
+        with open(filename, "w") as f:
+            json.dump(results_dict, f)
 
 
-#
-# Generat the reports (entrypoint)
-#
-def generate_reports():
-    # Need to grab BRANCH_NAME and BUILD_NUMBER
-    branch_name  = os.getenv("BRANCH_NAME",  default = "branchname")
-    build_number = os.getenv("BUILD_NUMBER", default = "buildnumber")
 
-    report_dir = args.output
 
-    if not os.path.exists(report_dir):
-        os.makedirs(report_dir, 0o755)
+def main():
 
-    print(f"Created Anchore Report Directory: {report_dir}")
+    endpoint_url = re.sub("\/+$", '', os.getenv("ANCHORE_CLI_URL", default = "http://localhost:8228/v1/"))
 
-    digest = getImageDigest()
+    anchore = Anchore(
+            url      = endpoint_url,
+            username = os.getenv("ANCHORE_CLI_USER",       default = "admin"),
+            password = os.getenv("ANCHORE_CLI_PASS",       default = "foobar"),
+            verify   = os.getenv("ANCHORE_VERIFY",         default = True),
+            image    = os.getenv("IMAGE_NAME",             default = "none"),
+            output   = os.getenv("ANCHORE_SCAN_DIRECTORY", default = ".")
+    )
 
-    get_gates    (report_dir = report_dir, digest = digest)
-    get_vulndb   (report_dir = report_dir, digest = digest)
-    get_version  (report_dir = report_dir)
+    anchore.get_vulns()
+    anchore.get_compliance()
+    anchore.get_version()
 
-#
-# Entrypoint
-#
-generate_reports()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
