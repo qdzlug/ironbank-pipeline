f135c21632a3f751e4d3b7bcac1813c30fb22536
Tim Seagren <seagren.tim@solute.local>
2020-09-15 19:06:30 -0700

    adding more debug

--- stages/csv-output/pipeline_csv_gen.py
+++ stages/csv-output/pipeline_csv_gen.py
@@ -93,6 +93,7 @@ def convert_to_excel():
 
 #Blank OSCAP Report
 def generate_blank_oscap_report():
+    print('DEBUG: ' + str(csv_dir))
     oscap_report = open(csv_dir + 'oscap.csv', 'w', encoding="utf-8")
     csv_writer = csv.writer(oscap_report)
     csv_writer.writerow(['','','','','','','','',''])
