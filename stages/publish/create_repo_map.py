010f3722c71631382e7d414b71a661c412805943
David Freeman <davidfreeman@seed-innovations.com>
2020-09-23 15:00:22 -0600

    update var names

--- stages/publish/create_repo_map.py
+++ stages/publish/create_repo_map.py
@@ -32,8 +32,8 @@ parser.add_argument('--summary_report', help='')
 parser.add_argument('--full_report', help='')
 parser.add_argument('--openscap_report', help='')
 parser.add_argument('--oval_report', help='')
-parser.add_argument('--license_file', help='')
-parser.add_argument('--readme', help='')
+parser.add_argument('--project_license', help='')
+parser.add_argument('--project_readme', help='')
 parser.add_argument('--job_type', help='')
 parser.add_argument('--output_dir', dest='output_dir', help='directory in which to write CSV output', default='./')
 args = parser.parse_args()
@@ -62,8 +62,8 @@ summary_report = args.summary_report
 full_report = args.full_report
 openscap_report = args.openscap_report
 oval_report = args.oval_report
-license_file = args.license_file
-readme = args.readme
+project_license = args.project_license
+project_readme = args.project_readme
 signature_name = args.signature_name
 output_dir = args.output_dir
 
@@ -91,8 +91,8 @@ new = {build_number:
         'Image_Name': image_name,
         'Version_Documentation': version_documentation,
         'OVAL_Report': oval_report,
-        'LICENSE_FILE': license_file,
-        'README': readme,
+        'PROJECT_FILE': project_license,
+        'PROJECT_README': project_readme,
         'Tar_Location': tar_location,
         'Full_Report': full_report,
         'Repo_Name': repo_name
