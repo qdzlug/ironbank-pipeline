c1d828615888a1277c4d5d630fac809b8061f74b
Eric <schmitt.eric@solute.us>
2020-09-30 16:32:04 -0700

    update for if no download yaml or json

--- stages/import-artifacts/import-run.sh
+++ stages/import-artifacts/import-run.sh
@@ -11,6 +11,7 @@ if [ -f "${CI_PROJECT_DIR}"/download.json ]; then
     touch "${ARTIFACT_DIR}"/converted-file/download.yaml
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/json_yaml_converter.py" -i "${CI_PROJECT_DIR}/download.json" -o "${ARTIFACT_DIR}"/converted-file/download.yaml
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${ARTIFACT_DIR}"/converted-file/download.yaml -d "${ARTIFACT_DIR}"
-else
+fi
+if [ -f ${CI_PROJECT_DIR}/download.yaml ]; then
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${CI_PROJECT_DIR}/download.yaml" -d "${ARTIFACT_DIR}"
 fi
