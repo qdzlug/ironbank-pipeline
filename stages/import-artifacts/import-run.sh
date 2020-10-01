0887d4a3681b66fa1f3e5a760fe8294d4d54fd23
David Freeman <davidfreeman@seed-innovations.com>
2020-10-01 08:52:10 -0600

    Add allowance for no download file

--- stages/import-artifacts/import-run.sh
+++ stages/import-artifacts/import-run.sh
@@ -11,6 +11,8 @@ if [ -f "${CI_PROJECT_DIR}"/download.json ]; then
     touch "${ARTIFACT_DIR}"/converted-file/download.yaml
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/json_yaml_converter.py" -i "${CI_PROJECT_DIR}/download.json" -o "${ARTIFACT_DIR}"/converted-file/download.yaml
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${ARTIFACT_DIR}"/converted-file/download.yaml -d "${ARTIFACT_DIR}"
-else
+elif [ -f download.yaml ]; then
     python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${CI_PROJECT_DIR}/download.yaml" -d "${ARTIFACT_DIR}"
+else
+    echo "No Download file found"
 fi
