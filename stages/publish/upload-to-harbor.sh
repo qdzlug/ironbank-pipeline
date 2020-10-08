478316697cb0bab0adc7290b24a0de290bdebef7
David Freeman <davidfreeman@seed-innovations.com>
2020-10-08 17:32:54 -0600

    fix indentation

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -8,6 +8,6 @@ echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >> staging_auth.json
 echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >> prod_auth.json
 
 # Upload image to prod Harbor
-    skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
-    "docker://${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}-${CI_PIPELINE_ID}" \
-    "docker://${REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
+skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
+"docker://${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}-${CI_PIPELINE_ID}" \
+"docker://${REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
