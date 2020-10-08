c31c3edeeb5fe0fcdcdcc649c866654cd8027b82
Eric <schmitt.eric@solute.us>
2020-10-08 10:11:25 -0700

    added for s3 and harbor

--- stages/vat/vat-run.sh
+++ stages/vat/vat-run.sh
@@ -6,7 +6,6 @@ export REMOTE_REPORT_DIRECTORY
 export S3_HTML_LINK="${S3_REPORT_BUCKET}/${BASE_BUCKET_DIRECTORY}/${CI_PROJECT_NAME}/${IMG_VERSION}/${REMOTE_REPORT_DIRECTORY}"
 IM_NAME=$(echo "${CI_PROJECT_PATH}" | sed -e 's/.*dsop\/\(.*\)/\1/')
 export IM_NAME
-pip3 install --upgrade pip setuptools wheel pandas mysql mysql-connector-python minepy python-gitlab
 # python3 "${PIPELINE_REPO_DIR}/stages/vat/get_parent_info.py" --image "${CI_PROJECT_PATH}" --tag "${IMG_VERSION}" \
 #   --glkey "${PYTHON_GL_KEY}" --wlbranch "${WL_TARGET_BRANCH}" --output 'environment.sh'
 # source environment.sh
@@ -16,6 +15,8 @@ echo "${CI_PROJECT_DIR}" | grep -e 'pipeline-test-project';
 if [[ $? = 0 ]]; then
   echo "Skipping vat. Cannot push to VAT when working with pipeline test projects..."
 else
+  pip3 install --upgrade pip setuptools wheel pandas mysql mysql-connector-python minepy python-gitlab
+
   python3 "${PIPELINE_REPO_DIR}/stages/vat/vat_import.py" \
     --db "${vat_db_database_name}" \
     --user "${vat_db_connection_user}" \
