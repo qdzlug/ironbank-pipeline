099d3201d3a731cb8c3b2c6029f53fbf431499f4
David Freeman <davidfreeman@seed-innovations.com>
2020-10-08 16:09:49 -0600

    update harbor upload

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -Eeuo pipefail
-
+if [[ $(echo "${CI_PROJECT_DIR}" | grep -e 'pipeline-test-project') ]]; then
+    echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects..."
+    exit 0
+fi
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >> staging_auth.json
 echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >> prod_auth.json
 
 # Upload image to prod Harbor
-echo "${CI_PROJECT_DIR}" | grep -e 'pipeline-test-project';
-if [[ $? = 0 ]]; then
-    echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects..."
-else
     skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
     "docker://${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}-${CI_PIPELINE_ID}" \
     "docker://${REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
-fi
