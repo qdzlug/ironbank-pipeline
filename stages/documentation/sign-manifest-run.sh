99dc3d3ed5303f542c25c8fdee53505beef3ca12
David Freeman <davidfreeman@seed-innovations.com>
2020-10-12 11:40:56 -0600

    reset signing scripts to match development

--- stages/documentation/sign-manifest-run.sh
+++ stages/documentation/sign-manifest-run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -Eeuo pipefail
-podman load -i "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar" "${STAGING_REGISTRY_URL}/opensource/pipeline-test-project/mongo-test:${IMG_VERSION}"
+podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d > key
-mkdir -m 0600 tmp_gpg
-mkdir -p "${ARTIFACT_DIR}"
+mkdir -p tmp_gpg "${ARTIFACT_DIR}"
 GPG_VERSION=$(gpg --version | grep '(?<=gpg .GnuPG.)([^0-9]+)([0-9]+[.][0-9]+[.][0-9]+)' -oP | sed -E 's/ //g')
-IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar" | grep -E '^[a-zA-Z0-9]+' -o)
-IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/opensource/pipeline-test-project/mongo-test:${IMG_VERSION}")
+IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)
+IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}")
 # Create manifest.json
 
 cat <<EOF > manifest.json
@@ -18,7 +17,7 @@ cat <<EOF > manifest.json
             "image-tar-sha256-checksum": "${IMAGE_TAR_SHA}"
         },
     "identity": {
-        "podman-reference": "${STAGING_REGISTRY_URL}/opensource/pipeline-test-project/mongo-test:${IMG_VERSION}"
+        "podman-reference": "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
         }
 },
 "optional": {
@@ -31,7 +30,6 @@ cat manifest.json
 rm -rf tmp_gpg/*
 # Sign manifest.json
 gpg --homedir ./tmp_gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
-ls -al ./tmp_gpg
 echo "pinentry-mode loopback" >> ./tmp_gpg/gpg.conf
 gpg --detach-sign --homedir ./tmp_gpg -o "${SIG_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" manifest.json
 # Stage manifest for upload
