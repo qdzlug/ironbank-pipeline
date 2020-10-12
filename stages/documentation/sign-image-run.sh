88419cbe7b082ed41ce50524767f84354c69d227
David Freeman <davidfreeman@seed-innovations.com>
2020-10-12 11:31:18 -0600

    remove creation of custom homedir

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -4,12 +4,14 @@ podman load -i "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar" "${STAGING_REGIS
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d > key
 echo "${HOME}"
 ls -al
-ls -al $HOME
-ls -al $HOME/.gnupg
-mkdir -m 0700 tmp_gpg
+ls -al "${HOME}"
+# mkdir -m 0700 tmp_gpg
 mkdir -p "${ARTIFACT_DIR}"
-gpg --homedir ./tmp_gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
-echo "pinentry-mode loopback" >> ./tmp_gpg/gpg.conf
-gpg --detach-sign --homedir ./tmp_gpg -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar"
+# gpg --homedir ./tmp_gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
+gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
+ls -al "${HOME}"/.gnupg
+echo "pinentry-mode loopback" >> "${HOME}"/.gnupg/gpg.conf
+# gpg --detach-sign --homedir ./tmp_gpg -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar"
+gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar"
 # Stage image for upload
 mv "${ARTIFACT_STORAGE}/build/mongo-test-4.2.9.tar" "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
