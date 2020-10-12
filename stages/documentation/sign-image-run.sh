e41f583cc3977957e5e0b2403e0f31d595f7ae97
David Freeman <davidfreeman@seed-innovations.com>
2020-10-12 10:56:41 -0600

    chmod gpg dir

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMG_VERSION}"
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d > key
-mkdir -p tmp_gpg "${ARTIFACT_DIR}"
+mkdir -m 0600 -p tmp_gpg "${ARTIFACT_DIR}"
 gpg --homedir ./tmp_gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
 echo "pinentry-mode loopback" >> ./tmp_gpg/gpg.conf
 gpg --detach-sign --homedir ./tmp_gpg -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar"
