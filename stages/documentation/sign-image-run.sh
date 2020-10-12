794526d03de8284e0630a37786fb071131130249
David Freeman <davidfreeman@seed-innovations.com>
2020-10-12 12:20:24 -0600

    remove ref to temp gpg dir

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -4,7 +4,7 @@ podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${STAGING_REGISTRY
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d > key
 mkdir -p "${ARTIFACT_DIR}"
 gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
-echo "pinentry-mode loopback" >> ./tmp_gpg/gpg.conf
+echo "pinentry-mode loopback" >> "${HOME}"/gpg.conf
 gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar"
 # Stage image for upload
 mv "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
