d6c74ebc0c2738d73df174ba0de54573d5dea2d1
Tim Seagren <tim.seagren.ctr@us.af.mil>
2020-10-29 17:08:25 -0700

    adding function

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -17,33 +17,33 @@ tag="${2}"
 echo "${DOCKER_AUTH_CONFIG_TEST}" | base64 -d >prod_auth.json
 echo "${NOTARY_DELEGATION_KEY}" | base64 -d >delegation.key
 
-# Load image from tarball
-podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "$gun:$tag" --storage-driver=vfs
-podman tag "$gun:$tag" "$gun:latest" --storage-driver=vfs
-
-# Upload image to prod Harbor
-podman push --authfile prod_auth.json \
-  "docker://$gun:$tag" \
-  --storage-driver=vfs
-  --digestfile tag_digest
-
-# Copy from staging to prod with latest tag
-podman push --authfile prod_auth.json \
-  "docker://$gun:latest" \
-  --storage-driver=vfs \
-  --digestfile latest_digest
-
-# Can we remove the skopeo dependency here? podman inspect doesn't output --raw and therefore might mess with the manifest.json sha
-skopeo inspect --authfile prod_auth.json --raw "docker://${gun}:${tag}" >tag_manifest.json
-skopeo inspect --authfile prod_auth.json --raw "docker://${gun}:latest" >latest_manifest.json
-
-# There's a chance for a TOCTOU attack/bug here. Make sure the digest matches this file:
-echo "$(cut -d: -f2 tag_digest) tag_manifest.json" | sha256sum --check
-echo "$(cut -d: -f2 latest_digest) latest_manifest.json" | sha256sum --check
-
 # Import the delegation key
 notary -d trust-dir-delegate/ key import delegation.key
 
-# Sign the image with the delegation key
-notary -v -s "${NOTARY_URL}" -d trust-dir-delegate add -p --roles=targets/releases "$gun" "$tag" "manifest.json"
-notary -v -s "${NOTARY_URL}" -d trust-dir-delegate add -p --roles=targets/releases "$gun" latest "manifest.json"
+# Load image from tarball
+podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "currentimage" --storage-driver=vfs
+
+push_and_sign() {
+
+    current_tag=${1}
+
+    podman tag "currentimage" "$gun:$current_tag" --storage-driver=vfs
+
+    # Upload image to prod Harbor
+    podman push --authfile prod_auth.json \
+      "docker://$gun:$current_tag" \
+      --storage-driver=vfs \
+      --digestfile "${current_tag}_digest"
+
+    # Can we remove the skopeo dependency here? podman inspect doesn't output --raw and therefore might mess with the manifest.json sha
+    skopeo inspect --authfile prod_auth.json --raw "docker://${gun}:${current_tag}" >"${current_tag}_manifest.json"
+
+    # There's a chance for a TOCTOU attack/bug here. Make sure the digest matches this file:
+    echo "$(cut -d: -f2 "${current_tag}_digest") ${current_tag}_manifest.json" | sha256sum --check
+
+    # Sign the image with the delegation key
+    notary -v -s "${NOTARY_URL}" -d trust-dir-delegate add -p --roles=targets/releases "$gun" "$current_tag" "${current_tag}_manifest.json"
+}
+
+push_and_sign "$tag"
+push_and_sign latest
