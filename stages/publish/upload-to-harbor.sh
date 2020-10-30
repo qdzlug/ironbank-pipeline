5a8c593cf9fe6830d9a043e9c8e76fe874c898d3
Kenneth Maguire <kennethmaguire@Kenneths-MacBook-Air.local>
2020-10-30 15:36:52 -0600

    remove changes to publish stage

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
-
 if [[ $(echo "${CI_PROJECT_DIR}" | grep -e 'pipeline-test-project') ]]; then
   echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects..."
   exit 0
 fi
-
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
 echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >prod_auth.json
 
