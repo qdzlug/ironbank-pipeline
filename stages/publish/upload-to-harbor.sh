8fa9e65d43bd40aaa4630a923ac0d690846ff59c
Kenneth Maguire <kennethmaguire@Kenneths-MacBook-Air.local>
2020-10-30 12:05:38 -0600

    add approval status check

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -Eeuo pipefail
+
+#check image approval status, fail if not approved
+if [ "${CI_COMMIT_BRANCH}" == "master" ] && [ "${IMAGE_APPROVAL_STATUS}" != "approved" ]; then
+  echo "Skipping publish. Cannot publish images that are not approved."
+  exit 1
+fi
+
 if [[ $(echo "${CI_PROJECT_DIR}" | grep -e 'pipeline-test-project') ]]; then
   echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects..."
   exit 0
 fi
+
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
 echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >prod_auth.json
 
