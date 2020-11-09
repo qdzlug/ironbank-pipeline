c6245e26112f63560ebb240bf569035d34ed9e7c
Gavin Scallon <gavin.scallon@parsons.com>
2020-11-09 12:49:24 -0700

    Reinstate download.yaml

--- stages/import-artifacts/import-run.sh
+++ stages/import-artifacts/import-run.sh
@@ -3,11 +3,9 @@ set -Eeuo pipefail
 mkdir -p "${ARTIFACT_DIR}/external-resources/" "${ARTIFACT_DIR}/images/"
 touch artifact.env
 
-# pip3 install --user requests boto3
-
-# Run the download script on the ironbank.yaml file
-if [ -f "${CI_PROJECT_DIR}"/ironbank.yaml ]; then
-  python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${CI_PROJECT_DIR}/ironbank.yaml" -d "${ARTIFACT_DIR}"
+# Run the download script on the download.yaml file
+if [ -f "${CI_PROJECT_DIR}"/download.yaml ]; then
+  python3 "${PIPELINE_REPO_DIR}/stages/import-artifacts/downloader.py" -i "${CI_PROJECT_DIR}/download.yaml" -d "${ARTIFACT_DIR}"
 else
-  echo "No ironbank.yaml file found"
+  echo "No download.yaml file found"
 fi
