24bcf611078f9f1af66684078fcf3f3627034d57
Blake Burkhart <blake.burkhart@us.af.mil>
2020-11-09 15:30:05 -0600

    Parse or generate ironbank.yaml

--- stages/preflight/source_variables.py
+++ stages/preflight/source_variables.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+import json
 import logging
-import sys
 import os
-import getopt
-import yaml
-import subprocess
 import re
+import subprocess
 import sys
+from pathlib import Path
+
+import jsonschema
+import yaml
+
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), "../../scripts/"))
+import ironbank_yaml.generate
 
 
 def main():
@@ -22,101 +27,57 @@ def main():
         logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
         logging.info("Log level set to info")
 
-    ##### Parse commandline arguments
-    # Use the project description.yaml file path if one exists
-    # Use the generated description.yaml file path if not
-    inputFile = ""
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:", ["ifile="])
-    except getopt.GetoptError:
-        print("source_variables.py -i <inputfile>")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-i", "--ifile"):
-            inputFile = arg
-        if inputFile == "":
-            print("No input file specified.")
-            sys.exit(1)
+    ironbank_yaml_path = Path("ironbank.yaml")
+    if ironbank_yaml_path.exists():
+        # Use the project description.yaml file path if one exists
+        content = yaml.safe_load(ironbank_yaml_path)
+        validate_yaml(content)
+    else:
+        # Use the generated description.yaml file path if not
+        logging.warning("ironbank.yaml does not exist, autogenerating")
+        project_path = os.environ["CI_PROJECT_PATH"].split("/")
+        assert len(project_path) > 2 and project_path[0] == "dsop"
+        greylist_path = "/".join((*project_path[1:], f"{project_path[-1]}.greylist"))
+        ironbank_yaml_string = ironbank_yaml.generate.generate(
+            greylist_path=greylist_path, repo1_url="https://repo1.dsop.io/"
+        )
+        content = yaml.safe_load(ironbank_yaml_string)
+        # Generated ironbank.yaml is already validated
 
-    # print("Input file:", inputFile, file=sys.stderr)
-
-    ##### Read description.yaml file
-    with open(inputFile, "r") as file:
-        content = yaml.safe_load(file)
-
-    for type in content:
-        if type == "path":
-            try:
-                f = open("path.txt", "w+")
-                path_str = content["path"]
-                f.write("IMAGE_PATH=" + path_str)
-            except:
-                print(
-                    "There was an issue with retrieving the image path in description.yaml",
-                    file=sys.stderr,
-                )
-
-        if type == "tags":
-            try:
-                f = open("tags.txt", "w+")
-                tag_list = content["tags"]
-                x = 0
-                for item in tag_list:
-                    tag = content["tags"][x]
-                    f.write(tag + "\n")
-                    x = x + 1
-            except:
-                print(
-                    "There was an issue sourcing the tag/image version from description.yaml",
-                    file=sys.stderr,
-                )
-
-        if type == "args":
-            try:
-                f = open("args.txt", "w+")
-                args_list = content["args"]
-                base_args = yaml.dump(args_list)
-                base_args_content = base_args.split("\n")
-                for item in base_args_content:
-                    if len(item) > 0:
-                        arg_output = retrieve_content(item)
-                        f.write(arg_output + "\n")
-            except:
-                print(
-                    "There was an issue sourcing the args from description.yaml",
-                    file=sys.stderr,
-                )
-
-        if type == "labels":
-            try:
-                f = open("labels.txt", "w+")
-                labels_list = content["labels"]
-                labels_content = yaml.dump(labels_list)
-                labels = labels_content.split("\n")
-                for item in labels:
-                    if len(item) > 0:
-                        label_output = retrieve_content(item)
-                        print(f"{label_output}")
-                        f.write(label_output + "\n")
-            except:
-                print(
-                    "There was an issue sourcing the labels from description.yaml",
-                    file=sys.stderr,
-                )
-
-        # "resources" intentionally left out
-
-        # Maintainers field is used for POC information and won't be parsed
+    process_yaml(content)
 
 
-def retrieve_content(yaml_content):
-    build_string = ""
+def validate_yaml(content):
+    logging.info("Validating schema")
+    schema_path = Path(__file__).parent / "../../schema/ironbank.schema.json"
+    with schema_path.open("r") as s:
+        schema_s = s.read()
+    schema = json.loads(schema_s)
+    jsonschema.validate(content, schema)
 
-    build_string += yaml_content
 
-    output_string = build_string.replace(": ", "=")
+def process_yaml(content):
+    artifact_dir = Path(os.environ["ARTIFACT_DIR"])
 
-    return output_string
+    with (artifact_dir / "path.env").open("w") as f:
+        f.write(f"IMAGE_PATH={content['path']}")
+
+    with (artifact_dir / "tags.txt").open("w") as f:
+        for tag in content["tags"]:
+            f.write(tag)
+            f.write("\n")
+
+    with (artifact_dir / "args.txt").open("w") as f:
+        for key, value in content["args"].items():
+            f.write(f"{key}={value}\n")
+
+    with (artifact_dir / "labels.txt").open("w") as f:
+        for key, value in content["labels"].items():
+            f.write(f"{key}={value}\n")
+
+    # "resources" intentionally left out
+
+    # Maintainers field is used for POC information and won't be parsed
 
 
 if __name__ == "__main__":
