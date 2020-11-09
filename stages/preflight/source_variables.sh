77d7ef577bf0b87f90320a8a12f2c97bf3b5390a
Gavin Scallon <gavin.scallon@parsons.com>
2020-11-09 13:10:56 -0700

    Remove source.env

--- stages/preflight/source_variables.sh
+++ stages/preflight/source_variables.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 # Parse the files if it does
 # Generate an description.yaml file if it does not
 if [ -f "${CI_PROJECT_DIR}"/description.yaml ]; then
-  python3 "${PIPELINE_REPO_DIR}/stages/preflight/source_variables.py" -i "${CI_PROJECT_DIR}/description.yaml" >>source.env
+  python3 "${PIPELINE_REPO_DIR}/stages/preflight/source_variables.py" -i "${CI_PROJECT_DIR}/description.yaml"
 else
   # TODO: add code for generating description.yaml
   echo "No description.yaml file found. Creating one..."
