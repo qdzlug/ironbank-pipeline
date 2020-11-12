cd209aa56b812574fc0657124c43b401847f6481
Kenneth Maguire <kennethmaguire@Kenneths-MacBook-Air.local>
2020-11-12 14:20:26 -0700

    loop through tags

--- stages/publish/special-upload-to-harbor.sh
+++ stages/publish/special-upload-to-harbor.sh
@@ -8,10 +8,8 @@ echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >>staging_auth.json
 echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >>prod_auth.json
 
 # Upload image to prod Harbor for special images
+while IFS= read -r tag; do
 skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
   "docker://${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}-${CI_PIPELINE_ID}" \
-  "docker://${REGISTRY_URL}/${SPECIAL_IMAGE_PATH}:${IMAGE_VERSION}"
-# Copy from staging to prod with latest tag
-skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
-  "docker://${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}-${CI_PIPELINE_ID}" \
-  "docker://${REGISTRY_URL}/${SPECIAL_IMAGE_PATH}:latest"
+  "docker://${REGISTRY_URL}/${SPECIAL_IMAGE_PATH}:${tag}"
+done <"${ARTIFACT_DIR}/preflight/tags.txt"
