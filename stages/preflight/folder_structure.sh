d1d0c33ecf3f68629e3d221f8b774732bf98c07d
David Freeman <davidfreeman@seed-innovations.com>
2020-11-17 17:03:14 -0700

    update spacing

--- stages/preflight/folder_structure.sh
+++ stages/preflight/folder_structure.sh
@@ -3,14 +3,14 @@ set -Eeuo pipefail
 # Check if required files exist
 # Testing with `-f` avoids the scenario where a malicious symlink is is named `README` in the repository
 if ! [[ -f README.md ]]; then
-	echo "README.md not found"
+  echo "README.md not found"
   exit 1
 fi
 if ! [[ -f Dockerfile ]]; then
-	echo "Dockerfile not found"
+  echo "Dockerfile not found"
   exit 1
 fi
 if ! [[ -f "LICENSE" ]]; then
-	echo "LICENSE not found"
+  echo "LICENSE not found"
   exit 1
 fi
