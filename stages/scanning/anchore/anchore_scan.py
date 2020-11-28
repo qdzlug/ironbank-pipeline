fb853ba1b680f5491e91ee3f6e9de5ecf64f76fe
James Petersen <jpetersenames@gmail.com>
2020-11-27 20:00:27 -0700

    change anchore var
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/scanning/anchore/anchore_scan.py
+++ stages/scanning/anchore/anchore_scan.py
@@ -21,7 +21,7 @@ def main():
         logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
         logging.info("Log level set to info")
 
-    anchore = Anchore(
+    anchore_scan = Anchore(
         url=os.environ["ANCHORE_SERVER_ADDRESS"],
         username=os.environ["ANCHORE_USERNAME"],
         password=os.environ["ANCHORE_PASSWORD"],
@@ -39,11 +39,11 @@ def main():
         logging.exception("Could not build up full image tag")
         sys.exit(1)
 
-    digest = anchore.image_add(image)
-    anchore.image_wait(digest=digest)
-    anchore.get_vulns(digest=digest, image=image, artifacts_path=artifacts_path)
-    anchore.get_compliance(digest=digest, image=image, artifacts_path=artifacts_path)
-    anchore.get_version(artifacts_path=artifacts_path)
+    digest = anchore_scan.image_add(image)
+    anchore_scan.image_wait(digest=digest)
+    anchore_scan.get_vulns(digest=digest, image=image, artifacts_path=artifacts_path)
+    anchore_scan.get_compliance(digest=digest, image=image, artifacts_path=artifacts_path)
+    anchore_scan.get_version(artifacts_path=artifacts_path)
 
 
 if __name__ == "__main__":
