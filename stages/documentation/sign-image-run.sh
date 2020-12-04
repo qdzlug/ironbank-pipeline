f401e7499e260852e73eadd678bfb760a9c016c6
James Petersen <jpetersenames@gmail.com>
2020-12-04 15:18:55 -0700

    remove commented out pieces
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
-# podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}"
+
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d >key
 mkdir -p "${ARTIFACT_DIR}"
+
 gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
 echo "pinentry-mode loopback" >>"${HOME}"/.gnupg/gpg.conf
 gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar"
+
 # Stage image for upload
 mv "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
--- stages/documentation/write-json-docs-run.sh
+++ stages/documentation/write-json-docs-run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -Eeo pipefail
+
 dnf install jq -y
-# podman load -i "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}"
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d >key
 mkdir -p tmp_gpg "${ARTIFACT_DIR}/reports"
+
 # Gather info for scan-metadata.json
 GPG_VERSION_INFO=$(gpg --version | grep "gpg")
-# TODO add anchore endpoint
-#- ANCHORE_VERSION=$(curl -k ${anchore_server_address}/version)
+
 if [[ "${DISTROLESS:-}" ]]; then
   ANCHORE_VERSION=$(cat "${ANCHORE_VERSION_FILE}" | sed 's/"//g')
   TWISTLOCK_VERSION=$(cat "${TWISTLOCK_VERSION_FILE}" | sed 's/"//g')
@@ -16,12 +16,11 @@ else
   ANCHORE_VERSION=$(cat "${ANCHORE_VERSION_FILE}" | sed 's/"//g')
   TWISTLOCK_VERSION=$(cat "${TWISTLOCK_VERSION_FILE}" | sed 's/"//g')
 fi
-#- OPENSCAP_VERSION=$(cat ${OPENSCAP_VERSION})
-# IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)
-# IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}")
-GPG_PUB_KEY=$(awk '{printf "%s\\n", $0}' "${IB_CONTAINER_GPG_PUBKEY}")
-# Create manifest.json
 
+#- OPENSCAP_VERSION=$(cat ${OPENSCAP_VERSION})
+GPG_PUB_KEY=$(awk '{printf "%s\\n", $0}' "${IB_CONTAINER_GPG_PUBKEY}")
+
+# Create manifest.json
 export IMAGE_VERSION
 export CI_COMMIT_SHA
 export IMAGE_APPROVAL_STATUS
@@ -36,6 +35,7 @@ export ANCHORE_VERSION
 export CI_PIPELINE_ID
 TIMESTAMP="$(date --utc '+%FT%T.%3NZ')"
 export TIMESTAMP
+
 jq -n '
 {
   "buildTag": env.IMAGE_VERSION,
@@ -90,3 +90,4 @@ jq -n '
 }' >documentation.json
 cat documentation.json
 mv documentation.json "${ARTIFACT_DIR}/reports"
+
