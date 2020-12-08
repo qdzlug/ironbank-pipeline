5f2d59035631e87aab938cfd57aac071031c3cce
James Petersen <jpetersenames@gmail.com>
2020-12-07 18:38:39 -0700

    move commands to get distinct return codes
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/build/build-run.sh
+++ stages/build/build-run.sh
@@ -79,20 +79,25 @@ echo "Read the tags"
 while IFS= read -r tag; do
   echo "buildah push --storage-driver=vfs --authfile staging_auth.json ${STAGING_REGISTRY_URL}/$IM_NAME:${tag}"
   buildah push --storage-driver=vfs --authfile staging_auth.json "${STAGING_REGISTRY_URL}/$IM_NAME:${tag}"
-done <"${ARTIFACT_STORAGE}/preflight/tags.txt"
+done <"${ARTIFACT_DIR}/preflight/tags.txt"
+# This is the solution
+# done <"${ARTIFACT_STORAGE}/preflight/tags.txt"
 
 # Provide tar for use in later stages, matching existing tar naming convention
 echo "skopeo copy --src-authfile staging_auth.json docker://${STAGING_REGISTRY_URL}/$IM_NAME:${CI_PIPELINE_ID} docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 skopeo copy --src-authfile staging_auth.json "docker://${STAGING_REGISTRY_URL}/$IM_NAME:${CI_PIPELINE_ID}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 
 echo "IMAGE_ID=sha256:$(podman inspect --storage-driver=vfs "${STAGING_REGISTRY_URL}/$IM_NAME" --format '{{.Id}}')"
-echo "IMAGE_ID=sha256:$(podman inspect --storage-driver=vfs "${STAGING_REGISTRY_URL}/$IM_NAME" --format '{{.Id}}')" >>build.env
+IMAGE_ID=sha256:$(podman inspect --storage-driver=vfs "${STAGING_REGISTRY_URL}/$IM_NAME" --format '{{.Id}}')
+echo "IMAGE_ID=${IMAGE_TAR_SHA}" >>build.env
 
 echo "IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)"
-echo "IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)" >>build.env
+IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)
+echo "IMAGE_TAR_SHA=${IMAGE_TAR_SHA}" >>build.env
 
-echo "IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${CI_PIPELINE_ID}")"
-echo "IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${CI_PIPELINE_ID}")" >>build.env
+echo "IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}")"
+IMAGE_PODMAN_SHA=$(podman inspect --format '{{.Digest}}' "${STAGING_REGISTRY_URL}/${IM_NAME}:${IMAGE_VERSION}")
+echo "IMAGE_PODMAN_SHA=${IMAGE_PODMAN_SHA}" >>build.env
 
 echo "IMAGE_FILE=${IMAGE_FILE}" >>build.env
 
