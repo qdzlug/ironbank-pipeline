e490d8d5e75ee54a4918644203ba8becb3bf4bab
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2020-12-11 11:48:46 -0700

    fix typo

--- stages/preflight/metadata.py
+++ stages/preflight/metadata.py
@@ -82,7 +82,7 @@ def process_yaml(content):
             f.write(f"{key}={value}\n")
 
     #optional field,if keywords key in yaml, create file. source_values() in create_repo_map checks if file exists, if not pass empty list
-    if "mil.dso.ironbank.image.keywords" in content.labels:
+    if "mil.dso.ironbank.image.keywords" in content["labels"]:
         with (artifact_dir / "keywords.txt").open("w") as f:
             labels = [k.strip() for k in content.labels["mil.dso.ironbank.image.keywords"].split(",")]
             for label in labels:
