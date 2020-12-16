f67f594a24b14ab4916303c4a1a2efe00c8cdc8a
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2020-12-16 14:10:20 -0700

    reformat

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -146,7 +146,7 @@ def _pipeline_whitelist_compare(image_name, hardening_manifest, lint=False):
     )
 
     wl_set = set()
-    #approval status is checked when retrieving image_whitelist
+    # approval status is checked when retrieving image_whitelist
     for image in image_whitelist:
         wl_set.add(image.vulnerability)
 
@@ -301,7 +301,7 @@ def get_vulns_from_query(row):
     vuln_dict["vulnerability"] = row[3]
     vuln_dict["vuln_source"] = row[4]
     vuln_dict["status"] = row[6]
-    #logging.debug(vuln_dict)
+    # logging.debug(vuln_dict)
     return vuln_dict
 
 
@@ -318,12 +318,12 @@ def _next_ancestor(image_path, greylist, hardening_manifest=None):
 
     # Try to get the parent image out of the local hardening_manifest.
     if hardening_manifest:
-       return hardening_manifest["args"]["BASE_IMAGE"]
+        return hardening_manifest["args"]["BASE_IMAGE"]
 
     # Try to load the hardening manifest from a remote repo.
     hm = _load_remote_hardening_manifest(project=image_path)
     if hm is not None:
-       return hm["args"]["BASE_IMAGE"]
+        return hm["args"]["BASE_IMAGE"]
 
     try:
         return greylist["image_parent_name"]
@@ -351,9 +351,8 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
     # logging.info(f"Grabbing CVEs for: {image_name}")
     result = vat_vuln_query(os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"])
 
-
     if result is None:
-        new_scan = True #probably won't need new scan since it will pass on dev when first importing cves into vat
+        new_scan = True  # probably won't need new scan since it will pass on dev when first importing cves into vat
         logging.debug("result none")
     else:
         new_scan = False
@@ -363,8 +362,9 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
                 total_whitelist.append(Vuln(vuln_dict, image_name))
                 logging.debug(vuln_dict["vulnerability"])
 
-    logging.debug("Length of total whitelist for source image: " + str(len(total_whitelist)))
-
+    logging.debug(
+        "Length of total whitelist for source image: " + str(len(total_whitelist))
+    )
 
     # need to swap this for hardening_manifest.yaml
     # need backwards compat (maybe)
@@ -401,7 +401,6 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
                 i += 1
 
         logging.debug("CVEs added from parent image: " + str(i))
-
 
         parent_image = _next_ancestor(
             image_path=parent_image,
