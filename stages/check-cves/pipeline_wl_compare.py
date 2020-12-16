36088f2f83d28448d08a970c26d39f07e2cafd73
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2020-12-16 12:23:37 -0700

    test python36

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -148,7 +148,6 @@ def _pipeline_whitelist_compare(image_name, hardening_manifest, lint=False):
     wl_set = set()
     for image in image_whitelist:
         if image.status == "Approve":
-            logging.debug(image.status)
             wl_set.add(image.vulnerability)
 
     # Don't go any further if just linting
@@ -317,14 +316,14 @@ def _next_ancestor(image_path, greylist, hardening_manifest=None):
 
     """
 
-    # Try to get the parent image out of the local hardening_manifest.
-    if hardening_manifest:
-       return hardening_manifest["args"]["BASE_IMAGE"]
+    # # Try to get the parent image out of the local hardening_manifest.
+    # if hardening_manifest:
+    #    return hardening_manifest["args"]["BASE_IMAGE"]
 
-    # Try to load the hardening manifest from a remote repo.
-    hm = _load_remote_hardening_manifest(project=image_path)
-    if hm is not None:
-       return hm["args"]["BASE_IMAGE"]
+    # # Try to load the hardening manifest from a remote repo.
+    # hm = _load_remote_hardening_manifest(project=image_path)
+    # if hm is not None:
+    #    return hm["args"]["BASE_IMAGE"]
 
     try:
         return greylist["image_parent_name"]
@@ -350,7 +349,7 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
         image_path=image_name, branch=whitelist_branch
     )
     # logging.info(f"Grabbing CVEs for: {image_name}")
-    result = vat_vuln_query(os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"])
+    result = vat_vuln_query("redhat/python/python36", "3.6")
     greylist_comp = set()
     # TODO: Implement new scan logic post feedback
     if result is None:
@@ -474,7 +473,7 @@ def main():
     image = hardening_manifest["name"]
 
     _pipeline_whitelist_compare(
-        image_name=image,
+        image_name="redhat/python/python36",
         hardening_manifest=hardening_manifest,
         lint=args.lint,
     )
