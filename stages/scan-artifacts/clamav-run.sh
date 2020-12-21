c6ec16304428bae4f60a891a3a4243b5f12dfd96
David Freeman <davidfreeman@seed-innovations.com>
2020-12-21 15:15:43 -0700

    check if CLAMAV_WHITELIST var exists

--- stages/scan-artifacts/clamav-run.sh
+++ stages/scan-artifacts/clamav-run.sh
@@ -2,8 +2,12 @@
 set -Eeuo pipefail
 shopt -s nullglob # Allow import-artifacts/*/* to match nothing
 
+if [[ "${CLAMAV_WHITELIST:-}" ]]; then
+  mv "${PIPELINE_REPO_DIR}/stages/scan-artifacts/clamav-whitelist.ign2" /usr/local/share/clamav/clamav_whitelist.ign2
+fi
+
 freshclam --config-file /clamav/conf/freshclam.conf
-for filename in ${ARTIFACT_STORAGE}/import-artifacts/*/*; do
+for filename in "${ARTIFACT_STORAGE}"/import-artifacts/*/*; do
   # TODO: fix or temporarily skip scans of large files
   clamscan -irv --max-filesize=1800M --max-scansize=1800M "${filename}" | tee "${ARTIFACT_DIR}/import-artifacts-clamav-report.txt"
 done
