e1495a81b8a3050f73f09e348599987c2a367ba3
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2020-12-22 11:22:52 -0700

    change scan paths for testing

--- stages/csv-output/csv-output.sh
+++ stages/csv-output/csv-output.sh
@@ -10,9 +10,9 @@ touch "${env_filename}"
 # pip3 install bs4 pandas argparse openpyxl gitpython
 if [[ "${DISTROLESS:-}" ]]; then
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
-    --twistlock "${ARTIFACT_STORAGE}/scan-results/twistlock/twistlock_cve.json" \
-    --anchore-sec "${ARTIFACT_STORAGE}/scan-results/anchore/anchore_security.json" \
-    --anchore-gates "${ARTIFACT_STORAGE}/scan-results/anchore/anchore_gates.json" \
+    --twistlock "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/twistlock/twistlock_cve.json" \
+    --anchore-sec "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/anchore/anchore_security.json" \
+    --anchore-gates "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/anchore/anchore_gates.json" \
     --output-dir "${CSV_REPORT}"/
 else
   # output OSCAP link variables for the VAT stage to use
@@ -21,11 +21,11 @@ else
   #echo "OSCAP_COMPLIANCE_URL=${OSCAP_COMPLIANCE_URL}${report_artifact_path}" >>"${env_filename}"
   cat "${env_filename}"
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
-    --oscap "${ARTIFACT_STORAGE}/scan-results/openscap/report.html" \
-    --oval "${ARTIFACT_STORAGE}/scan-results/openscap/report-cve.html" \
-    --twistlock "${ARTIFACT_STORAGE}/scan-results/twistlock/twistlock_cve.json" \
-    --anchore-sec "${ARTIFACT_STORAGE}/scan-results/anchore/anchore_security.json" \
-    --anchore-gates "${ARTIFACT_STORAGE}/scan-results/anchore/anchore_gates.json" \
+    --oscap "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/openscap/report.html" \
+    --oval "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/openscap/report-cve.html" \
+    --twistlock "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/twistlock/twistlock_cve.json" \
+    --anchore-sec "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/anchore/anchore_security.json" \
+    --anchore-gates "${PIPELINE_REPO_DIR}/stages/csv-output/ci-artifacts/scan-results/anchore/anchore_gates.json" \
     --output-dir "${CSV_REPORT}"/
 fi
 python3 "${PIPELINE_REPO_DIR}"/stages/csv-output/justifier.py -i "${CSV_REPORT}"/all_scans.xlsx -o "${CSV_REPORT}"/"${CI_PROJECT_NAME}":"${IMAGE_VERSION}"-"${CI_PIPELINE_ID}"-justifications.xlsx -s "${IM_NAME}"
