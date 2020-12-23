95852fc9ab42a6d5322dbddd8e588395e4587737
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2020-12-23 15:38:58 -0700

    update error message

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -417,7 +417,7 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
             pipeline_branch = os.getenv("CI_COMMIT_BRANCH")
             if pipeline_branch == "master":
                 logging.error(
-                    "This is container is listed as unapproved in the VAT. Unapproved images cannot run on master branch. Failing stage."
+                    "This container is not noted as an approved image in VAT. Unapproved images cannot run on master branch. Failing stage."
                 )
                 sys.exit(1)
         f.write(f"BASE_IMAGE={hardening_manifest['args']['BASE_IMAGE']}\n")
