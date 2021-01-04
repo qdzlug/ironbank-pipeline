95c5316d187b23d4bc7d68309976e2b9745dcb07
James Petersen <jpetersenames@gmail.com>
2021-01-04 16:50:30 -0700

    add more data from the anchore api response
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/csv-output/scanners/anchore.py
+++ stages/csv-output/scanners/anchore.py
@@ -7,34 +7,99 @@ import pathlib
 from scanners.helper import _write_csv_from_dict_list
 
 
-# ANCHORE SECURITY CSV
+def _vulnerability_record(fulltag, vuln):
+
+    vuln_record = dict()
+    vuln_record["tag"] = fulltag
+    vuln_record["cve"] = vuln["vuln"]
+    vuln_record["severity"] = vuln["severity"]
+    vuln_record["feed"] = vuln["feed"]
+    vuln_record["feed_group"] = vuln["feed_group"]
+    vuln_record["package"] = vuln["package"]
+    vuln_record["package_path"] = vuln["package_path"]
+    vuln_record["package_type"] = vuln["package_type"]
+    vuln_record["package_version"] = vuln["package_version"]
+    vuln_record["fix"] = vuln["fix"]
+    vuln_record["url"] = vuln["url"]
+    vuln_record["inherited"] = vuln.get("inherited_from_base") or "no_data"
+
+    if vuln["nvd_data"] and vuln["nvd_data"][0]["cvss_v3"]:
+        vuln_record["nvd_cvss_v3_base_score"] = vuln["nvd_data"][0]["cvss_v3"]["base_score"]
+        vuln_record["nvd_cvss_v3_exploitability_score"] = vuln["nvd_data"][0]["cvss_v3"]["exploitability_score"]
+        vuln_record["nvd_cvss_v3_impact_score"] = vuln["nvd_data"][0]["cvss_v3"]["impact_score"]
+    else:
+        vuln_record["nvd_cvss_v3_base_score"] = ""
+        vuln_record["nvd_cvss_v3_exploitability_score"] = ""
+        vuln_record["nvd_cvss_v3_impact_score"] = ""
+
+    if vuln["nvd_data"] and vuln["nvd_data"][0]["cvss_v2"]:
+        vuln_record["nvd_cvss_v2_base_score"] = vuln["nvd_data"][0]["cvss_v2"]["base_score"]
+        vuln_record["nvd_cvss_v2_exploitability_score"] = vuln["nvd_data"][0]["cvss_v2"]["exploitability_score"]
+        vuln_record["nvd_cvss_v2_impact_score"] = vuln["nvd_data"][0]["cvss_v2"]["impact_score"]
+    else:
+        vuln_record["nvd_cvss_v2_base_score"] = ""
+        vuln_record["nvd_cvss_v2_exploitability_score"] = ""
+        vuln_record["nvd_cvss_v2_impact_score"] = ""
+
+    if vuln["vendor_data"] and vuln["vendor_data"][0]["cvss_v3"]:
+        vuln_record["vendor_cvss_v3_base_score"] = vuln["vendor_data"][0]["cvss_v3"]["base_score"]
+        vuln_record["vendor_cvss_v3_exploitability_score"] = vuln["vendor_data"][0]["cvss_v3"]["exploitability_score"]
+        vuln_record["vendor_cvss_v3_impact_score"] = vuln["vendor_data"][0]["cvss_v3"]["impact_score"]
+    else:
+        vuln_record["vendor_cvss_v3_base_score"] = ""
+        vuln_record["vendor_cvss_v3_exploitability_score"] = ""
+        vuln_record["vendor_cvss_v3_impact_score"] = ""
+
+    if vuln["vendor_data"] and vuln["vendor_data"][0]["cvss_v2"]:
+        vuln_record["vendor_cvss_v2_base_score"] = vuln["vendor_data"][0]["cvss_v2"]["base_score"]
+        vuln_record["vendor_cvss_v2_exploitability_score"] = vuln["vendor_data"][0]["cvss_v2"]["exploitability_score"]
+        vuln_record["vendor_cvss_v2_impact_score"] = vuln["vendor_data"][0]["cvss_v2"]["impact_score"]
+    else:
+        vuln_record["vendor_cvss_v2_base_score"] = ""
+        vuln_record["vendor_cvss_v2_exploitability_score"] = ""
+        vuln_record["vendor_cvss_v2_impact_score"] = ""
+
+    return vuln_record
+
+
 def vulnerability_report(csv_dir, anchore_security_json):
+    """
+    Generate the anchore vulnerability report
+
+    """
     with open(anchore_security_json, mode="r", encoding="utf-8") as f:
         json_data = json.load(f)
-        cves = [
-            {
-                "tag": json_data["imageFullTag"],
-                "cve": d["vuln"],
-                "severity": d["severity"],
-                "package": d["package"],
-                "package_path": d["package_path"],
-                "fix": d["fix"],
-                "url": d["url"],
-                "inherited": d.get("inherited_from_base") or "no_data",
-            }
-            for d in json_data["vulnerabilities"]
-        ]
+        cves = [_vulnerability_record(fulltag=json_data["imageFullTag"], vuln=d) for d in json_data["vulnerabilities"]]
 
-    fieldnames = [
-        "tag",
-        "cve",
-        "severity",
-        "package",
-        "package_path",
-        "fix",
-        "url",
-        "inherited",
-    ]
+    if cves:
+        fieldnames = list(cves[0].keys())
+    else:
+        fieldnames = [
+            "tag",
+            "cve",
+            "severity",
+            "feed",
+            "feed_group",
+            "package",
+            "package_path",
+            "package_type",
+            "package_version",
+            "fix",
+            "url",
+            "inherited",
+            "nvd_cvss_v3_base_score",
+            "nvd_cvss_v3_exploitability_score",
+            "nvd_cvss_v3_impact_score",
+            "nvd_cvss_v2_base_score",
+            "nvd_cvss_v2_exploitability_score",
+            "nvd_cvss_v2_impact_score",
+            "vendor_cvss_v3_base_score",
+            "vendor_cvss_v3_exploitability_score",
+            "vendor_cvss_v3_impact_score",
+            "vendor_cvss_v2_base_score",
+            "vendor_cvss_v2_exploitability_score",
+            "vendor_cvss_v2_impact_score",
+        ]
 
     _write_csv_from_dict_list(
         csv_dir=csv_dir,
@@ -46,8 +111,11 @@ def vulnerability_report(csv_dir, anchore_security_json):
     return len(cves)
 
 
-# ANCHORE GATES CSV
 def compliance_report(csv_dir, anchore_gates_json):
+    """
+    Generate the anchore compliance report
+
+    """
     with open(anchore_gates_json, encoding="utf-8") as f:
         json_data = json.load(f)
         sha = list(json_data.keys())[0]
