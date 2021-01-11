06fabf275eef236df4762da5eb16a099b1d1fa30
Blake Burkhart <blake.burkhart@us.af.mil>
2021-01-11 14:14:25 -0700

    Allow pipeline-test-project to push to ironbank-testing

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -Eeuo pipefail
-if echo "${CI_PROJECT_DIR}" | grep -q -F 'pipeline-test-project'; then
-  echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects..."
-  exit 0
+
+if echo "${CI_PROJECT_DIR}" | grep -q -F 'pipeline-test-project' && [ -z "${DOCKER_AUTH_CONFIG_TEST:-}" ]; then
+  echo "Skipping Harbor Upload. Cannot push to Harbor when working with pipeline test projects unless DOCKER_AUTH_CONFIG_TEST is set..."
+  exit 1
 fi
-echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
-echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >prod_auth.json
+
+echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >>staging_auth.json
+
+if [ -z "${DOCKER_AUTH_CONFIG_TEST:-}" ]; then
+  echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >dest_auth.json
+else
+  echo "${DOCKER_AUTH_CONFIG_TEST}" | base64 -d >dest_auth.json
+fi
 
 # Copy from staging to prod with each tag listed in descriptions.yaml
 while IFS= read -r tag; do
-  skopeo copy --src-authfile staging_auth.json --dest-authfile prod_auth.json \
+  skopeo copy --src-authfile staging_auth.json --dest-authfile dest_auth.json \
     "docker://${STAGING_REGISTRY_URL}/${IM_NAME}@${IMAGE_PODMAN_SHA}" \
     "docker://${REGISTRY_URL}/${IM_NAME}:${tag}"
 done <"${ARTIFACT_STORAGE}/preflight/tags.txt"
