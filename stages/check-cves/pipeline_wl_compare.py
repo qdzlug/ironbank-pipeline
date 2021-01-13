77a120ca4a907babfa729045003a01de8ccfb35c
David Freeman <davidfreeman@seed-innovations.com>
2021-01-12 17:10:15 -0700

    add findings from parent images

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -449,9 +449,11 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
         # TODO: remove this after 30 day hardening_manifest merge cutof
         # TODO: swap this for hardening manifest after 30 day merge cutoff
         result = _vat_vuln_query(parent_image_name, parent_image_version)
+        vat_findings[image_name] = []
 
         for row in result:
             vuln_dict = _get_vulns_from_query(row)
+            vat_findings[image_name].append(row)
             if vuln_dict["status"] and vuln_dict["status"].lower() == "approve":
                 total_whitelist.append(Vuln(vuln_dict, image_name))
 
