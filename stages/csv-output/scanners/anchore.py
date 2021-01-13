c26a30898da79705a6cd25d6df14c30c70ecadec
James Petersen <jpetersenames@gmail.com>
2021-01-13 16:21:33 -0700

    add the description field
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- stages/csv-output/scanners/anchore.py
+++ stages/csv-output/scanners/anchore.py
@@ -7,7 +7,12 @@ import pathlib
 from scanners.helper import _write_csv_from_dict_list
 
 
-def _vulnerability_record(fulltag, vuln):
+def _vulnerability_record(fulltag, justifications, vuln):
+
+    cve_justification = ""
+    id = vuln["vuln"] + "-" + vuln["package"]
+    if id in justifications.keys():
+        cve_justification = justifications[id]
 
     vuln_record = dict()
     vuln_record["tag"] = fulltag
@@ -22,6 +27,7 @@ def _vulnerability_record(fulltag, vuln):
     vuln_record["fix"] = vuln["fix"]
     vuln_record["url"] = vuln["url"]
     vuln_record["inherited"] = vuln.get("inherited_from_base") or "no_data"
+    vuln_record["description"] = vuln["extra"]["description"]
 
     if vuln["nvd_data"] and vuln["nvd_data"][0]["cvss_v3"]:
         vuln_record["nvd_cvss_v3_base_score"] = vuln["nvd_data"][0]["cvss_v3"][
@@ -83,180 +89,67 @@ def _vulnerability_record(fulltag, vuln):
         vuln_record["vendor_cvss_v2_exploitability_score"] = ""
         vuln_record["vendor_cvss_v2_impact_score"] = ""
 
+    vuln_record["Justification"] = cve_justification
+
     return vuln_record
 
 
-# def vulnerability_report(csv_dir, anchore_security_json):
-#     """
-#     Generate the anchore vulnerability report
-#
-#     """
-#     with open(anchore_security_json, mode="r", encoding="utf-8") as f:
-#         json_data = json.load(f)
-#         cves = [
-#             _vulnerability_record(fulltag=json_data["imageFullTag"], vuln=d)
-#             for d in json_data["vulnerabilities"]
-#         ]
-#
-#     if cves:
-#         fieldnames = list(cves[0].keys())
-#     else:
-#         fieldnames = [
-#             "tag",
-#             "cve",
-#             "severity",
-#             "feed",
-#             "feed_group",
-#             "package",
-#             "package_path",
-#             "package_type",
-#             "package_version",
-#             "fix",
-#             "url",
-#             "inherited",
-#             "nvd_cvss_v3_base_score",
-#             "nvd_cvss_v3_exploitability_score",
-#             "nvd_cvss_v3_impact_score",
-#             "nvd_cvss_v2_base_score",
-#             "nvd_cvss_v2_exploitability_score",
-#             "nvd_cvss_v2_impact_score",
-#             "vendor_cvss_v3_base_score",
-#             "vendor_cvss_v3_exploitability_score",
-#             "vendor_cvss_v3_impact_score",
-#             "vendor_cvss_v2_base_score",
-#             "vendor_cvss_v2_exploitability_score",
-#             "vendor_cvss_v2_impact_score",
-#         ]
-#
-#     _write_csv_from_dict_list(
-#         csv_dir=csv_dir,
-#         dict_list=cves,
-#         fieldnames=fieldnames,
-#         filename="anchore_security.csv",
-#     )
-#
-#     return len(cves)
-#
-#
-# def compliance_report(csv_dir, anchore_gates_json):
-#     """
-#     Generate the anchore compliance report
-#
-#     """
-#     with open(anchore_gates_json, encoding="utf-8") as f:
-#         json_data = json.load(f)
-#         sha = list(json_data.keys())[0]
-#         anchore_data = json_data[sha]["result"]["rows"]
-#
-#     gates = list()
-#     stop_count = 0
-#     image_id = "unable_to_determine"
-#     for ad in anchore_data:
-#         gate = {
-#             "image_id": ad[0],
-#             "repo_tag": ad[1],
-#             "trigger_id": ad[2],
-#             "gate": ad[3],
-#             "trigger": ad[4],
-#             "check_output": ad[5],
-#             "gate_action": ad[6],
-#             "policy_id": ad[8],
-#         }
-#
-#         if ad[7]:
-#             gate["matched_rule_id"] = ad[7]["matched_rule_id"]
-#             gate["whitelist_id"] = ad[7]["whitelist_id"]
-#             gate["whitelist_name"] = ad[7]["whitelist_name"]
-#         else:
-#             gate["matched_rule_id"] = ""
-#             gate["whitelist_id"] = ""
-#             gate["whitelist_name"] = ""
-#
-#         try:
-#             gate["inherited"] = ad[9]
-#             if gate["gate"] == "dockerfile":
-#                 gate["inherited"] = False
-#         except IndexError:
-#             gate["inherited"] = "no_data"
-#
-#         gates.append(gate)
-#
-#         if gate["gate_action"] == "stop":
-#             stop_count += 1
-#
-#         image_id = gate["image_id"]
-#
-#     fieldnames = [
-#         "image_id",
-#         "repo_tag",
-#         "trigger_id",
-#         "gate",
-#         "trigger",
-#         "check_output",
-#         "gate_action",
-#         "policy_id",
-#         "matched_rule_id",
-#         "whitelist_id",
-#         "whitelist_name",
-#         "inherited",
-#     ]
-#
-#     _write_csv_from_dict_list(
-#         csv_dir=csv_dir,
-#         dict_list=gates,
-#         fieldnames=fieldnames,
-#         filename="anchore_gates.csv",
-#     )
-#     return {"stop_count": stop_count, "image_id": image_id}
+def vulnerability_report(csv_dir, justifications, anchore_security_json):
+    """
+    Generate the anchore vulnerability report
 
-# Get results from Anchore security for csv export, becomes anchore cve spreadsheet
-def vulnerability_report(anchore_security_json, justifications, csv_dir):
+    """
     with open(anchore_security_json, mode="r", encoding="utf-8") as f:
         json_data = json.load(f)
-        cves = []
-        for d in json_data["vulnerabilities"]:
-            cve_justification = ""
-            id = d["vuln"] + "-" + d["package"]
-            if id in justifications.keys():
-                cve_justification = justifications[id]
-            cves.append(
-                {
-                    "tag": json_data["imageFullTag"],
-                    "cve": d["vuln"],
-                    "severity": d["severity"],
-                    "package": d["package"],
-                    "package_path": d["package_path"],
-                    "fix": d["fix"],
-                    "url": d["url"],
-                    "inherited": d.get("inherited_from_base") or "no_data",
-                    "Justification": cve_justification,
-                }
-            )
+        cves = [
+            _vulnerability_record(fulltag=json_data["imageFullTag"], justifications=justifications, vuln=d)
+            for d in json_data["vulnerabilities"]
+        ]
 
-    fieldnames = [
-        "tag",
-        "cve",
-        "severity",
-        "package",
-        "package_path",
-        "fix",
-        "url",
-        "inherited",
-        "Justification",
-    ]
+    if cves:
+        fieldnames = list(cves[0].keys())
+    else:
+        fieldnames = [
+            "tag",
+            "cve",
+            "severity",
+            "feed",
+            "feed_group",
+            "package",
+            "package_path",
+            "package_type",
+            "package_version",
+            "fix",
+            "url",
+            "inherited",
+            "description",
+            "nvd_cvss_v3_base_score",
+            "nvd_cvss_v3_exploitability_score",
+            "nvd_cvss_v3_impact_score",
+            "nvd_cvss_v2_base_score",
+            "nvd_cvss_v2_exploitability_score",
+            "nvd_cvss_v2_impact_score",
+            "vendor_cvss_v3_base_score",
+            "vendor_cvss_v3_exploitability_score",
+            "vendor_cvss_v3_impact_score",
+            "vendor_cvss_v2_base_score",
+            "vendor_cvss_v2_exploitability_score",
+            "vendor_cvss_v2_impact_score",
+            "Justification",
+        ]
 
     _write_csv_from_dict_list(
+        csv_dir=csv_dir,
         dict_list=cves,
         fieldnames=fieldnames,
         filename="anchore_security.csv",
-        csv_dir=csv_dir,
     )
 
     return len(cves)
 
 
 # Get results of Anchore gates for csv export, becomes anchore compliance spreadsheet
-def compliance_report(anchore_gates_json, justifications, csv_dir):
+def compliance_report(csv_dir, anchore_gates_json, justifications):
     with open(anchore_gates_json, encoding="utf-8") as f:
         json_data = json.load(f)
         sha = list(json_data.keys())[0]
