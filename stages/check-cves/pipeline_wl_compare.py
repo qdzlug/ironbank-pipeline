574edde27c8ca0e8bc68e7c20b2ad542bb86a3e8
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2021-02-22 13:55:50 -0700

    fail pipeline if image name and project path do not match

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -518,13 +518,12 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
     logging.info(f"Grabbing CVEs for: {image_name}")
     # get cves from vat
     if os.environ["IMAGE_NAME"] != os.environ["PROJ_PATH"]:
-        query_im_name = os.environ["PROJ_PATH"]
-        query_im_version = os.environ["IMAGE_VERSION"]
-    else:
-        query_im_name = os.environ["IMAGE_NAME"]
-        query_im_version = os.environ["IMAGE_VERSION"]
+        logging.error("Name in hardening_manifest does not match GitLab project name (e.g. redhat/ubi/ubi8)")
+        logging.error("Quickfix: Edit the name in the hardening_manifest to match the GitLab project name")
+        logging.error("Issue is known and solution is in progress.")
+        sys.exit(1)
 
-    result = _vat_vuln_query(query_im_name, query_im_version)
+    result = _vat_vuln_query(os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"])
     # parse CVEs from VAT query
     # empty list is returned if no entry or no cves. NoneType only returned if error.
     if result is None:
@@ -536,9 +535,11 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
             vat_findings[image_name].append(finding_dict)
 
     # get container approval from separate query
-    _vat_findings_query(query_im_name, query_im_version)
+    _vat_findings_query(os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"])
 
-    approval_status, approval_text = _vat_approval_query(query_im_name, query_im_version)
+    approval_status, approval_text = _vat_approval_query(
+        os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"]
+    )
 
     logging.info("CONTAINER APPROVAL STATUS")
     logging.info(approval_status)
