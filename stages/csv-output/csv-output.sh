5f2b9d20ab4826e321240450532c7d92aafbaccb
David Freeman <davidfreeman@seed-innovations.com>
2021-02-26 14:49:53 -0700

    comment out env vars for testing

--- stages/csv-output/csv-output.sh
+++ stages/csv-output/csv-output.sh
@@ -9,8 +9,8 @@ touch "${env_filename}"
 # pip3 install --upgrade pip
 # pip3 install bs4 pandas argparse openpyxl gitpython
 if [[ "${DISTROLESS:-}" ]]; then
-  echo "OSCAP_CVE_URL=''" >>"${env_filename}"
-  echo "OSCAP_COMPLIANCE_URL=''" >>"${env_filename}"
+  # echo "OSCAP_CVE_URL=''" >>"${env_filename}"
+  # echo "OSCAP_COMPLIANCE_URL=''" >>"${env_filename}"
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
     --twistlock "${ARTIFACT_STORAGE}/scan-results/twistlock/twistlock_cve.json" \
     --anchore-sec "${ARTIFACT_STORAGE}/scan-results/anchore/anchore_security.json" \
@@ -19,9 +19,9 @@ if [[ "${DISTROLESS:-}" ]]; then
     --output-dir "${CSV_REPORT}"/
 else
   # output OSCAP link variables for the VAT stage to use
-  report_artifact_path='/artifacts/browse/ci-artifacts/scan-results/openscap/'
-  echo "OSCAP_CVE_URL=${OSCAP_CVE_URL}${report_artifact_path}" >>"${env_filename}"
-  echo "OSCAP_COMPLIANCE_URL=${OSCAP_COMPLIANCE_URL}${report_artifact_path}" >>"${env_filename}"
+  # report_artifact_path='/artifacts/browse/ci-artifacts/scan-results/openscap/'
+  # echo "OSCAP_CVE_URL=${OSCAP_CVE_URL}${report_artifact_path}" >>"${env_filename}"
+  # echo "OSCAP_COMPLIANCE_URL=${OSCAP_COMPLIANCE_URL}${report_artifact_path}" >>"${env_filename}"
   cat "${env_filename}"
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
     --oscap "${ARTIFACT_STORAGE}/scan-results/openscap/report.html" \
