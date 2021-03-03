7218b4afd972df183c262ae35668af2da4597027
David Freeman <davidfreeman@seed-innovations.com>
2021-03-03 12:22:19 -0700

    update anchore class to use tuple

--- stages/csv-output/scanners/anchore.py
+++ stages/csv-output/scanners/anchore.py
@@ -70,7 +70,11 @@ def _vulnerability_record(fulltag, justifications, vuln):
         logging.debug(f"no {key}")
 
     vuln_record["Justification"] = ""
-    id = vuln["vuln"] + "-" + vuln["package"] + "-" + vuln["package_path"]
+    id = (
+        vuln["vuln"],
+        vuln["package"],
+        vuln["package_path"] if vuln["package_path"] != "pkgdb" else None,
+    )
     if id in justifications.keys():
         vuln_record["Justification"] = justifications[id]
 
