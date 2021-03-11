1005908235483160e05f71185536b5735b8a87db
gavin.scallon <gavin.scallon@parsons.com>
2021-03-11 14:05:19 -0700

    use this

--- stages/publish/upload-to-harbor.sh
+++ stages/publish/upload-to-harbor.sh
@@ -8,6 +8,16 @@ fi
 
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >>staging_auth.json
 
+gun="${REGISTRY_URL}/${IM_NAME}"
+
+# Perform vault login
+vault login -method=userpass username="${NOTARY_STAGING_USERNAME}" password="${NOTARY_STAGING_PASSWORD}"
+vault kv get -field=delegation.key kv/il2/notary/lod1 | base64 -d >delegation.key
+
+echo "${NOTARY_DELEGATION_KEY}" | base64 -d >delegation.key
+
+notary -d trust-dir-delegate/ key import delegation.key
+
 if [ -z "${DOCKER_AUTH_CONFIG_TEST:-}" ]; then
   echo "${DOCKER_AUTH_CONFIG_PROD}" | base64 -d >dest_auth.json
 else
@@ -16,7 +26,11 @@ fi
 
 # Copy from staging to prod with each tag listed in descriptions.yaml
 while IFS= read -r tag; do
+  # Sign the image with the delegation key
+  notary -v -s "${NOTARY_STAGING_ADDR}" -d trust-dir-delegate add -p --roles=targets/releases "$gun" "${tag}" "${tag}_manifest.json"
+
   skopeo copy --src-authfile staging_auth.json --dest-authfile dest_auth.json \
     "docker://${STAGING_REGISTRY_URL}/${IM_NAME}@${IMAGE_PODMAN_SHA}" \
     "docker://${REGISTRY_URL}/${IM_NAME}:${tag}"
+
 done <"${ARTIFACT_STORAGE}/preflight/tags.txt"
