11f0d23c3ce3977818ffac169f386144b1b3091c
David Freeman <davidfreeman@seed-innovations.com>
2021-03-15 17:19:20 -0600

    remove extra copy of image tarball

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -9,4 +9,6 @@ echo "pinentry-mode loopback" >>"${HOME}"/.gnupg/gpg.conf
 gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar"
 
 # Stage image for upload
-mv "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
+mv "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
+
+mv "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${ARTIFACT_DIR}/${IMAGE_FILE}-${IMAGE_VERSION}.tar"
