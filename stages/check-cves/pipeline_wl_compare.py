b8ec044bb31bab9d2d91f2f298990fec4b4f39d2
David Freeman <davidfreeman@seed-innovations.com>
2021-03-17 18:12:10 -0600

    use oval dict in check cve

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -188,7 +188,7 @@ def _pipeline_whitelist_compare(image_name, hardening_manifest, lint=False):
 
         oval_cves = oscap.get_oval(oval_file)
         for oval in oval_cves:
-            vuln_set.add(Finding("oscap_cve", oval, None, None))
+            vuln_set.add(Finding("oscap_cve", oval["ref"], oval["pkg"], None))
 
     twistlock_cves = twistlock.get_full()
     for tl in twistlock_cves:
