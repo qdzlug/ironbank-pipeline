f7bda126443e6feea9c9dfddee091779a9249cf4
David Freeman <davidfreeman@seed-innovations.com>
2021-03-29 16:08:38 -0600

    update resource paths

--- stages/vat-finding-compare/vat_findings.py
+++ stages/vat-finding-compare/vat_findings.py
@@ -58,11 +58,11 @@ def check_existence(delta_api_db, delta_db_api, api_set, db_set):
 def main():
     try:
         with open(
-            f'{os.environ["ARTIFACT_DIR"]}/vat_api_findings.json', "r"
+            f'{os.environ["ARTIFACT_DIR"]}/lint/vat_api_findings.json', "r"
         ) as api_findings:
             api = json.load(api_findings)
         with open(
-            f'{os.environ["ARTIFACT_DIR"]}/vat_findings.json', "r"
+            f'{os.environ["ARTIFACT_DIR"]}/lint/vat_findings.json', "r"
         ) as db_findings:
             db = json.load(db_findings)
     except FileNotFoundError:
