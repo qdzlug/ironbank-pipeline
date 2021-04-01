8cccd3e950088b89917e200bd2d433457e6caf74
Blake Burkhart <blake.burkhart@us.af.mil>
2021-04-01 15:54:31 -0600

    Add retry to init gun

--- scripts/initialize-gun.sh
+++ scripts/initialize-gun.sh
@@ -30,56 +30,58 @@ rootkeyloc="rootkey/$rootrev"
 trustdir=$(mktemp -d)
 
 script_help() {
-    echo
-    echo "Please provide a GUN to initialize (e.g. registry1.dso.mil/ironbank/redhat/ubi/ubi)"
-    echo "You may provide a single GUN or a file containing a list of newline-delimited GUNs"
-    echo "NOTE: The GUN MUST MATCH the 'name' attirbute of the hardening_manifest.yaml for the image being initialized prefixed with the target registry url (e.g. registry1.dso.mil/<name>)"
+  echo
+  echo "Please provide a GUN to initialize (e.g. registry1.dso.mil/ironbank/redhat/ubi/ubi)"
+  echo "You may provide a single GUN or a file containing a list of newline-delimited GUNs"
+  echo "NOTE: The GUN MUST MATCH the 'name' attirbute of the hardening_manifest.yaml for the image being initialized prefixed with the target registry url (e.g. registry1.dso.mil/<name>)"
 }
 
 if [ -z "${1:-}" ]; then
-    script_help
-    exit 1
+  script_help
+  exit 1
 fi
 
 is_installed() {
-    if ! command -v "${1}" > /dev/null; then
-        echo
-        echo "${1} must be installed before continuing, exiting"
-        exit 1
-    fi
+  if ! command -v "${1}" >/dev/null; then
+    echo
+    echo "${1} must be installed before continuing, exiting"
+    exit 1
+  fi
 }
 
 clean() {
-    # Clean up trustdir
-    rm -rf -- "$trustdir"
+  # Clean up trustdir
+  rm -rf -- "$trustdir"
 }
 
 import_root_key() {
-    echo
-    echo "==============================="
-    echo " Retrieve root key from Vault "
-    echo "==============================="
-    echo
+  echo
+  echo "==============================="
+  echo " Retrieve root key from Vault "
+  echo "==============================="
+  echo
 
-    # Login to Vault
-    export VAULT_TOKEN=$(vault login -token-only -method=userpass username=notary-admin)
+  # Login to Vault
+  export VAULT_TOKEN=$(vault login -token-only -method=userpass username=notary-admin)
 
-    # Retrieve root key
-    vault kv get -field=rootkey "/kv/il2/notary/admin/$rootkeyloc" | notary -v -s "$notary_url" -d "$trustdir" key import /dev/stdin --role=root
+  # Retrieve root key
+  vault kv get -field=rootkey "/kv/il2/notary/admin/$rootkeyloc" | notary -v -s "$notary_url" -d "$trustdir" key import /dev/stdin --role=root
 }
 
 init_gun() {
-    gun=$1
-    echo
-    echo "==============================="
-    echo "Initializing GUN $gun in Notary"
-    echo "==============================="
-    echo
+  gun=$1
+  echo
+  echo "==============================="
+  echo "Initializing GUN $gun in Notary"
+  echo "==============================="
+  echo
 
+  for i in $(seq 1 5); do
     # Initialize GUN with root key
     if ! notary init "$gun" -p -d "$trustdir" -s "$notary_url"; then
-        echo "WARNING: target key already exists for $gun, skipping"
-        return
+      echo "WARNING: notary error or target key already exists for $gun, retrying"
+      echo ""
+      continue
     fi
 
     # Add delegation key. `delegation.crt` is already on-disk
@@ -92,6 +94,12 @@ init_gun() {
     decryptedkey=$(notary key export -d "$trustdir/" --gun "$gun" | sed '/:/d' | openssl ec -passin env:NOTARY_TARGETS_PASSPHRASE)
 
     echo -n "$decryptedkey" | vault kv put "/kv/il2/notary/admin/targets/$targetrev/$gun" key=-
+
+    # Success, break from loop
+    return 0
+  done
+
+  return 1
 }
 
 is_installed openssl
@@ -100,40 +108,40 @@ is_installed vault
 
 # Set NOTARY_AUTH if not set
 if [ -z "${NOTARY_AUTH:-}" ]; then
-    echo
-    echo "Enter registry1.dso.mil username: "
-    read -r -s username
-    echo "Enter registry1.dso.mil password: "
-    read -r -s password
-    export NOTARY_AUTH=$(echo -n "$username:$password" | base64)
+  echo
+  echo "Enter registry1.dso.mil username: "
+  read -r -s username
+  echo "Enter registry1.dso.mil password: "
+  read -r -s password
+  export NOTARY_AUTH=$(echo -n "$username:$password" | base64)
 fi
 
 import_root_key
 
 while [[ $# -gt 0 ]]; do
-    key="$1"
-    case "$key" in
-        -f|--file)
-            if [ -z "$2" ]; then
-                echo
-                echo "Must provide argument for -f"
-                exit 1
-            fi
-            filename="$2"
-            while IFS= read -r gun; do
-                init_gun "$gun";
-            done < "$filename"
-            exit 0
-            ;;
-        -h|--help)
-            script_help
-            exit 0
-            ;;
-        *)
-            init_gun "$1"
-            exit 0
-            ;;
-    esac
+  key="$1"
+  case "$key" in
+    -f | --file)
+      if [ -z "$2" ]; then
+        echo
+        echo "Must provide argument for -f"
+        exit 1
+      fi
+      filename="$2"
+      while IFS= read -r gun; do
+        init_gun "$gun"
+      done <"$filename"
+      exit 0
+      ;;
+    -h | --help)
+      script_help
+      exit 0
+      ;;
+    *)
+      init_gun "$1"
+      exit 0
+      ;;
+  esac
 done
 
 # Clean
