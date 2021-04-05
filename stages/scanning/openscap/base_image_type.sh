6d5ad10067389ddff4ffa37eb9b1132f7dc7033c
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2021-04-05 16:49:40 -0600

    fix formatting

--- stages/scanning/openscap/base_image_type.sh
+++ stages/scanning/openscap/base_image_type.sh
@@ -2,14 +2,14 @@ DOCKER_IMAGE_PATH=$(podman load -i "${ARTIFACT_STORAGE}"/build/"${CI_PROJECT_NAM
 export ${DOCKER_IMAGE_PATH}
 BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "com.redhat.component"}}' "${DOCKER_IMAGE_PATH}")
 if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
-    BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "os_type"}}' "${DOCKER_IMAGE_PATH}")
+  BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "os_type"}}' "${DOCKER_IMAGE_PATH}")
+  if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
+    BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "mil.dso.ironbank.os-type"}}' "${DOCKER_IMAGE_PATH}")
     if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
-        BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "mil.dso.ironbank.os-type"}}' "${DOCKER_IMAGE_PATH}")
-        if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
-            labels=$(podman inspect -f '{{index .Labels}}' "${DOCKER_IMAGE_PATH}")
-            echo "Unknown image type. Can't choose security guide. labels: ${labels}"
-            exit 1
-        fi
+      labels=$(podman inspect -f '{{index .Labels}}' "${DOCKER_IMAGE_PATH}")
+      echo "Unknown image type. Can't choose security guide. labels: ${labels}"
+      exit 1
     fi
+  fi
 fi
 export ${BASE_IMAGE_TYPE}
