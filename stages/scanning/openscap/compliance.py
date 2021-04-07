72371411e1eecf9f6feac620b1defd2404b837b8
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2021-04-07 12:20:41 -0600

    change ubuntu profile and update var

--- stages/scanning/openscap/compliance.py
+++ stages/scanning/openscap/compliance.py
@@ -77,10 +77,8 @@ def get_oscap_guide(oscap_version, base_image_type):
             + "/ssg-ubuntu2004-ds.xml",
         },
         "ubuntu1804-container": {
-            "profile": "xccdf_org.ssgproject.content_profile_anssi_np_nt28_high",
-            "securityGuide": "scap-security-guide-"
-            + str(oscap_version)
-            + "/ssg-ubuntu1804-ds.xml",
+            "profile": "xccdf_mil.disa.stig_profile_MAC-3_Public",
+            "securityGuide": "U_CAN_Ubuntu_18-04_V2R1_STIG_SCAP_1-2_Benchmark.xml",
         },
         "ubuntu1604-container": {
             "profile": "xccdf_org.ssgproject.content_profile_anssi_np_nt28_high",
