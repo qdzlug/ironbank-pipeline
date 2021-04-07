c7ad646e4648586c59f3eea7d9ce2b9ed27f55b6
Kenneth Maguire <kennethmaguire@seed-innovations.com>
2021-04-07 11:54:26 -0600

    add scap content for ubuntu

--- stages/scanning/openscap/oscap-compliance-run.sh
+++ stages/scanning/openscap/oscap-compliance-run.sh
@@ -6,16 +6,23 @@ echo "${DOCKER_IMAGE_PATH}"
 OSCAP_VERSION=$(cat "${PIPELINE_REPO_DIR}"/stages/scanning/rhel-oscap-version.json | jq .version | sed -e 's/"//g')
 oscap_container=$(python3 "${PIPELINE_REPO_DIR}/stages/scanning/openscap/compliance.py" --oscap-version "${OSCAP_VERSION}" --image-type "${BASE_IMAGE_TYPE}" | sed s/\'/\"/g)
 echo "${oscap_container}"
-curl -L "https://github.com/ComplianceAsCode/content/releases/download/v${OSCAP_VERSION}/scap-security-guide-${OSCAP_VERSION}.zip" -o scap-security-guide.zip
-unzip -qq -o scap-security-guide.zip
+SCAP_CONTENT="scap-content"
+mkdir -p "${SCAP_CONTENT}"
 
+if [[ "${DOCKER_IMAGE_PATH}" -eq "ubuntu1804-container" ]]; then
+  curl -L "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_CAN_Ubuntu_18-04_V2R1_STIG_SCAP_1-2_Benchmark.zip" -o "${SCAP_CONTENT}/scap-security-guide.zip"
+else
+  curl -L "https://github.com/ComplianceAsCode/content/releases/download/v${OSCAP_VERSION}/scap-security-guide-${OSCAP_VERSION}.zip" -o "${SCAP_CONTENT}/scap-security-guide.zip"
+fi
+
+unzip -qq -o ${SCAP_CONTENT}/scap-security-guide.zip
 profile=$(echo "${oscap_container}" | grep -o '"profile": "[^"]*' | grep -o '[^"]*$')
 securityGuide=$(echo "${oscap_container}" | grep -o '"securityGuide": "[^"]*' | grep -o '[^"]*$')
 echo "profile: ${profile}"
 echo "securityGuide: ${securityGuide}"
-oscap-podman "${DOCKER_IMAGE_PATH}" xccdf eval --verbose ERROR --fetch-remote-resources --profile "${profile}" --report report.html "${securityGuide}" || true
+oscap-podman "${DOCKER_IMAGE_PATH}" xccdf eval --verbose ERROR --fetch-remote-resources --profile "${profile}" --report report.html "${SCAP_CONTENT}/${securityGuide}" || true
 ls report.html
-rm -rf "scap-security-guide.zip scap-security-guide-${OSCAP_VERSION}"
+rm -rf "${SCAP_CONTENT}"
 echo "${OSCAP_VERSION}" >>"${OSCAP_SCANS}/oscap-version.txt"
 cp report.html "${OSCAP_SCANS}/report.html"
 
