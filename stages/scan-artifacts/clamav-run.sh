d14ae5c9a26ed407d964ec9fdfb8c9b8172c24ad
Stephen Swanson <stephen.swanson.1@us.af.mil>
2021-04-08 12:56:55 -0500

    wip: use ironbank clamav image; add initial logic to install definitions

--- stages/scan-artifacts/clamav-run.sh
+++ stages/scan-artifacts/clamav-run.sh
@@ -2,12 +2,22 @@
 set -Eeuo pipefail
 shopt -s nullglob # Allow import-artifacts/*/* to match nothing
 
+## download definitions.tar.gz from storage artifcat registry
+## curl -o definitions.tar.gz --header "JOB-TOKEN:${CI_JOB_TOKEN}" "https://code.il2.dso.mil/api/v4/projects/${CI_PROJECT_ID}/packages/generic/clamav/definitions.tar.gz"
+tar -C /clamav/definitions/ -xzvf definitions.tar.gz
+
+if [[ -f "$DEFPATH/bytecode.cvd" ]] && [[ -f "$DEFPATH/daily.cvd" ]] && [[ -f "$DEFPATH/main.cvd" ]]; then
+  echo "clamav definitions successfully imported."
+  echo "clamav definitions version: $(clamscan --version | cut -d/ -f3)"
+else
+  echo "ERROR: clamav definitions did not import"
+  exit 1
+fi
+
 if [[ "${CLAMAV_WHITELIST:-}" ]]; then
   cp "clamav-whitelist" /usr/local/share/clamav/clamav_whitelist.ign2
 fi
 
-# Commenting out the following because we are getting rate limited
-# freshclam --config-file /clamav/conf/freshclam.conf
 for filename in "${ARTIFACT_STORAGE}"/import-artifacts/*/*; do
   # TODO: fix or temporarily skip scans of large files
   clamscan -irv --max-filesize=4000M --max-scansize=4000M "${filename}" | tee -a "${ARTIFACT_DIR}/import-artifacts-clamav-report.txt"
