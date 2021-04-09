b4c196107ccddb16409fc927b94be65f0a71dda5
Stephen Swanson <stephen.swanson.1@us.af.mil>
2021-04-09 12:21:53 -0500

    Add missing constant; add comments

--- stages/scan-artifacts/clamav-run.sh
+++ stages/scan-artifacts/clamav-run.sh
@@ -2,16 +2,18 @@
 set -Eeuo pipefail
 shopt -s nullglob # Allow import-artifacts/*/* to match nothing
 
+# Install clamav definitions from av-updater using freshclam
+DEFPATH=/clamav/definitions
 curl -L --header "DEPLOY-TOKEN: ${IB_TOOLS_REPO_READ}" "https://repo1.dso.mil/ironbank-tools/av-updater/-/jobs/artifacts/switch-to-generate-def-tar/raw/definitions.tar.gz?job=build" -o definitions.tar.gz
-
 tar -C /clamav/definitions/ -xzvf definitions.tar.gz
 
+# Verify existence of expected files
 if [[ -f "$DEFPATH/bytecode.cvd" ]] && [[ -f "$DEFPATH/daily.cvd" ]] && [[ -f "$DEFPATH/main.cvd" ]]; then
-  echo "clamav definitions successfully imported."
+  echo "clamav definitions successfully installed."
   clamav_definitions_version="$(clamscan --version | cut -d/ -f3)"
   echo "clamav definitions version: $clamav_definitions_version"
 else
-  echo "ERROR: clamav definitions did not import"
+  echo "ERROR: clamav definitions did not install"
   exit 1
 fi
 
