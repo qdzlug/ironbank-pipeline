ed32fb2b63d632968379b8f4d8d384110ad8cb43
Stephen Swanson <stephen.swanson.1@us.af.mil>
2021-04-14 16:23:25 -0500

    fix shfmt issues

--- scripts/analysis/search_pod_logs.sh
+++ scripts/analysis/search_pod_logs.sh
@@ -16,13 +16,12 @@ TARGET_NAMESPACE=${TARGET_NAMESPACE:-twistlock}
 TARGET_PODNAME=${TARGET_PODNAME:-twistlock-defender}
 TARGET_LOG_PATH=${TARGET_LOG_PATH:-var/lib/twistlock/log/defender.log}
 
-logfile="${TARGET_NAMESPACE}_${TARGET_PODNAME}_$(echo $TARGET_LOG_PATH \
-| awk -F'/' '{print $NF}')_$(date +%F_%H-%M-%S).log"
+logfile="${TARGET_NAMESPACE}_${TARGET_PODNAME}_$(echo $TARGET_LOG_PATH |
+  awk -F'/' '{print $NF}')_$(date +%F_%H-%M-%S).log"
 
 echo "log file: $logfile"
 
-
-while read -r line ; do
+while read -r line; do
   echo "searching pod $line"
-  kubectl exec -n "$TARGET_NAMESPACE" "$line" -- cat "$TARGET_LOG_PATH" >> "$logfile"
-done <<< "$(kubectl get pods -n "$TARGET_NAMESPACE" | grep twistlock-defender | awk '{print $1}')"
+  kubectl exec -n "$TARGET_NAMESPACE" "$line" -- cat "$TARGET_LOG_PATH" >>"$logfile"
+done <<<"$(kubectl get pods -n "$TARGET_NAMESPACE" | grep twistlock-defender | awk '{print $1}')"
