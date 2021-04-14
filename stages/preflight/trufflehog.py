7a2a34759d7fae93fd5642138e6a085b48deee41
David Freeman <davidfreeman@seed-innovations.com>
2021-04-14 13:56:11 -0600

    update trufflehog py test

--- stages/preflight/trufflehog.py
+++ stages/preflight/trufflehog.py
@@ -7,17 +7,33 @@ import requests
 import git
 from pathlib import Path
 
-# def main():
-#   report_name = "trufflehog.json"
-branch_name = os.environ["CI_COMMIT_BRANCH"]
-project_id = os.environ["CI_PROJECT_ID"]
-# pipeline_sha = last_pipeline_sha(branch_name, project_id)
 
-# since_commit_cmd = (
-#   ["--since_commit", pipeline_sha[0]['sha']]
-#   if pipeline_sha[0]
-#   else ["--no-history"]
-# )
+def main():
+    # Get logging level, set manually when running pipeline
+    loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
+    if loglevel == "DEBUG":
+        logging.basicConfig(
+            level=loglevel,
+            format="%(levelname)s [%(filename)s:%(lineno)d]: %(message)s",
+        )
+        logging.debug("Log level set to debug")
+    else:
+        logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
+        logging.info("Log level set to info")
+
+    report_name = "trufflehog.json"
+    branch_name = os.environ["CI_COMMIT_BRANCH"]
+    project_id = os.environ["CI_PROJECT_ID"]
+    current_commit_sha = os.environ["CI_COMMIT_SHA"]
+
+    pipelines = last_pipeline_sha(branch_name, project_id)
+    print(pipelines)
+    logging.info(f"Current commit: {current_commit_sha}")
+    logging.info(f"Last pipeline run from API: {pipelines[0]['sha']}")
+
+    since_commit_cmd = since_commit_sha(pipelines, current_commit_sha)
+
+    print(since_commit_cmd)
 
 
 def last_pipeline_sha(branch_name, project_id):
@@ -33,7 +49,7 @@ def last_pipeline_sha(branch_name, project_id):
         logging.exception(f"Could not retrieve last pipeline sha")
         sys.exit(1)
     if r.status_code == 200:
-        pipelines = [x for x in r.json() if x["status"] not in ["canceled", "skipped"]]
+        pipelines = [x for x in r.json() if x["status"] == "success"]
     else:
         logging.error("Non 200 status code returned from pipeline sha retrieval")
         logging.error(f"Response text: {r.text}")
@@ -43,7 +59,13 @@ def last_pipeline_sha(branch_name, project_id):
     return None
 
 
-pipelines = last_pipeline_sha(branch_name, project_id)
-print(pipelines)
-print(f"Current commit: {os.environ['CI_COMMIT_SHA']}")
-print(f"Last pipeline run from API: {pipelines[0]['sha']}")
+def since_commit_sha(pipeline_lst, current_commit_sha):
+    pipeline_sha_lst = []
+    if pipeline_lst:
+        for sha in [x["sha"] for x in pipeline_lst]:
+            pipeline_sha_lst.append(sha)
+    return (
+        ["--since_commit", pipeline_sha_lst[0]]
+        if pipeline_sha_lst
+        else ["--no-history"]
+    )
