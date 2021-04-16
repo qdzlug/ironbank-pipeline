9744b3d2282b7ffa19235a2bdc33faef3d261176
David Freeman <davidfreeman@seed-innovations.com>
2021-04-16 16:39:03 -0600

    update sign image job to copy image from staging to docker archive

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -4,9 +4,10 @@ set -Eeuo pipefail
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d >key
 mkdir -p "${ARTIFACT_DIR}"
 
+skopeo copy --src-authfile staging_auth.json "docker://${STAGING_REGISTRY_URL}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
 echo "pinentry-mode loopback" >>"${HOME}"/.gnupg/gpg.conf
-gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar"
+gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 
 # Stage image for upload
-mv "${ARTIFACT_STORAGE}/build/${IMAGE_FILE}.tar" "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
+mv "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
