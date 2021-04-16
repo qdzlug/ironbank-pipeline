591de6abb503e102f8e2441db43f68b7034b40c1
David Freeman <davidfreeman@seed-innovations.com>
2021-04-16 16:52:14 -0600

    add authfile and podman pull to base_image_type.sh

--- stages/scanning/openscap/base_image_type.sh
+++ stages/scanning/openscap/base_image_type.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -Eeuo pipefail
-podman load -i "${ARTIFACT_STORAGE}"/build/"${CI_PROJECT_NAME}"-"${CI_PIPELINE_ID}".tar
+# podman load -i "${ARTIFACT_STORAGE}"/build/"${CI_PROJECT_NAME}"-"${CI_PIPELINE_ID}".tar
+echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
+podman pull --authfile staging_auth.json "${STAGING_IMAGE_SHA}"
 DOCKER_IMAGE_PATH=$(podman images --noheading | awk '{print $3}')
 export DOCKER_IMAGE_PATH
 BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "com.redhat.component"}}' "${DOCKER_IMAGE_PATH}")
