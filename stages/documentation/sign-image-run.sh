ed6114727847c5e3a2ed706255a3fa11c9d3c6fc
David Freeman <davidfreeman@seed-innovations.com>
2021-04-16 16:51:34 -0600

    add authfile to image sign job

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -4,7 +4,9 @@ set -Eeuo pipefail
 echo "${IB_CONTAINER_GPG_KEY}" | base64 -d >key
 mkdir -p "${ARTIFACT_DIR}"
 
-skopeo copy --src-authfile staging_auth.json "docker://${STAGING_REGISTRY_URL}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
+echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
+
+skopeo copy --src-authfile staging_auth.json "docker://${STAGING_IMAGE_SHA}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
 echo "pinentry-mode loopback" >>"${HOME}"/.gnupg/gpg.conf
 gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
