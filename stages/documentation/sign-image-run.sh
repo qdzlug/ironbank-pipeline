58591c848946a5151f6c21bd8442e50c350ffd6d
David Freeman <davidfreeman@seed-innovations.com>
2021-04-17 11:24:51 -0600

    use existing vars in sign image

--- stages/documentation/sign-image-run.sh
+++ stages/documentation/sign-image-run.sh
@@ -6,13 +6,10 @@ mkdir -p "${ARTIFACT_DIR}"
 
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
 
-skopeo copy --src-authfile staging_auth.json "docker://${STAGING_IMAGE_SHA}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
+skopeo copy --src-authfile staging_auth.json "docker://${STAGING_REGISTRY_URL}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}" "docker-archive:${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 gpg --import --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" key
 echo "pinentry-mode loopback" >>"${HOME}"/.gnupg/gpg.conf
-gpg --detach-sign -o "${IMAGE_FILE}.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
+gpg --detach-sign -o "${ARTIFACT_DIR}/${IMAGE_FILE}.tar.sig" --armor --yes --batch --passphrase "${IB_CONTAINER_SIG_KEY_PASSPHRASE}" "${ARTIFACT_DIR}/${IMAGE_FILE}.tar"
 
 IMAGE_TAR_SHA=$(sha256sum "${ARTIFACT_DIR}/${IMAGE_FILE}.tar" | grep -E '^[a-zA-Z0-9]+' -o)
 echo "IMAGE_TAR_SHA=${IMAGE_TAR_SHA}" >>sign-image.env
-
-# Stage image for upload
-mv "${IMAGE_FILE}.sig" "${ARTIFACT_DIR}"
