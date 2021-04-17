e5daef240482ed099ce2bf979a5cdad70e1f627c
David Freeman <davidfreeman@seed-innovations.com>
2021-04-17 11:45:03 -0600

    update base_image_type to use build env vars

--- stages/scanning/openscap/base_image_type.sh
+++ stages/scanning/openscap/base_image_type.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 # podman load -i "${ARTIFACT_STORAGE}"/build/"${CI_PROJECT_NAME}"-"${CI_PIPELINE_ID}".tar
 echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
-podman pull --authfile staging_auth.json "${STAGING_IMAGE_SHA}"
+podman pull --authfile staging_auth.json "${STAGING_REGISTRY_URL}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}"
 DOCKER_IMAGE_PATH=$(podman images --noheading | awk '{print $3}')
 export DOCKER_IMAGE_PATH
 BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "com.redhat.component"}}' "${DOCKER_IMAGE_PATH}")
