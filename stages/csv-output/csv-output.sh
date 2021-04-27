4986a18b2e23e3be9e39d0dd1faa01486b48a917
David Freeman <davidfreeman@seed-innovations.com>
2021-04-27 13:55:35 -0600

    Only output empty value for OSCAP_CVE_URL

--- stages/csv-output/csv-output.sh
+++ stages/csv-output/csv-output.sh
@@ -9,7 +9,6 @@ touch "${env_filename}"
 # pip3 install --upgrade pip
 # pip3 install bs4 pandas argparse openpyxl gitpython
 if [[ "${DISTROLESS:-}" ]]; then
-  echo "OSCAP_CVE_URL=''" >>"${env_filename}"
   echo "OSCAP_COMPLIANCE_URL=''" >>"${env_filename}"
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
     --twistlock "${ARTIFACT_STORAGE}/scan-results/twistlock/twistlock_cve.json" \
@@ -20,7 +19,6 @@ if [[ "${DISTROLESS:-}" ]]; then
 else
   # output OSCAP link variables for the VAT stage to use
   report_artifact_path='/artifacts/browse/ci-artifacts/scan-results/openscap/'
-  echo "OSCAP_CVE_URL=${OSCAP_CVE_URL}${report_artifact_path}" >>"${env_filename}"
   echo "OSCAP_COMPLIANCE_URL=${OSCAP_COMPLIANCE_URL}${report_artifact_path}" >>"${env_filename}"
   cat "${env_filename}"
   python3 "${PIPELINE_REPO_DIR}/stages/csv-output/pipeline_csv_gen.py" \
@@ -33,3 +31,5 @@ else
     --output-dir "${CSV_REPORT}"/
 fi
 python3 "${PIPELINE_REPO_DIR}"/stages/csv-output/excel_convert.py -i "${CSV_REPORT}"/ -o "${CSV_REPORT}"/"${CI_PROJECT_NAME}":"${IMAGE_VERSION}"-"${CI_PIPELINE_ID}"-justifications.xlsx
+
+echo "OSCAP_CVE_URL=''" >>"${env_filename}"
