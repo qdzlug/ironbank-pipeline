7df197baaed79acf4134bb443ca434aa36ee3915
stephen.swanson <stephen.swanson.1@us.af.mil>
2021-05-04 19:05:39 +0000

    Apply 3 suggestion(s) to 2 file(s)

--- stages/s3/upload-to-s3-run.sh
+++ stages/s3/upload-to-s3-run.sh
@@ -47,16 +47,16 @@ ls reports
 tar -zcvf "${REPORT_TAR_NAME}" reports
 
 python3 "${PIPELINE_REPO_DIR}/stages/s3/s3_upload.py" --file repo_map.json --bucket "${S3_REPORT_BUCKET}" --dest "${BASE_BUCKET_DIRECTORY}/${IMAGE_PATH}/repo_map.json"
-while IFS= read -r file; do
+while IFS= read -r -d '' file; do
   object_path="${file#"$ARTIFACT_STORAGE/documentation/"}"
   python3 "${PIPELINE_REPO_DIR}/stages/s3/s3_upload.py" --file "$file" --bucket "${S3_REPORT_BUCKET}" --dest "${BASE_BUCKET_DIRECTORY}/${IMAGE_PATH}/${IMAGE_VERSION}/${REMOTE_DOCUMENTATION_DIRECTORY}/$object_path"
-done < <(find "${DOCUMENTATION_DIRECTORY}" ! -name "$(printf "*\n*")" -name "*" -type f)
+done < <(find "${DOCUMENTATION_DIRECTORY}" -type f -print0)
 
-while IFS= read -r file; do
+while IFS= read -r -d '' file; do
   report_name=$(echo "$file" | rev | cut -d/ -f1-2 | rev)
   echo "$file"
   python3 "${PIPELINE_REPO_DIR}/stages/s3/s3_upload.py" --file "$file" --bucket "${S3_REPORT_BUCKET}" --dest "${BASE_BUCKET_DIRECTORY}/${IMAGE_PATH}/${IMAGE_VERSION}/${REMOTE_REPORT_DIRECTORY}/$report_name"
-done < <(find "${SCAN_DIRECTORY}" ! -name "$(printf "*\n*")" -name "*" -type f)
+done < <(find "${SCAN_DIRECTORY}" -type f -print0)
 
 python3 "${PIPELINE_REPO_DIR}/stages/s3/s3_upload.py" --file "${PROJECT_README}" --bucket "${S3_REPORT_BUCKET}" --dest "${BASE_BUCKET_DIRECTORY}/${IMAGE_PATH}/${IMAGE_VERSION}/${REMOTE_REPORT_DIRECTORY}/${PROJECT_README}"
 python3 "${PIPELINE_REPO_DIR}/stages/s3/s3_upload.py" --file "${PROJECT_LICENSE}" --bucket "${S3_REPORT_BUCKET}" --dest "${BASE_BUCKET_DIRECTORY}/${IMAGE_PATH}/${IMAGE_VERSION}/${REMOTE_REPORT_DIRECTORY}/${PROJECT_LICENSE}"
