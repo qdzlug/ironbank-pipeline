bc40ccfb4d43ff3460b9eccdbb68290ff82ba183
Dallas Thompson <thompson.dallas@solute.us>
2021-05-10 12:39:01 -0400

    custodial commit

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -333,7 +333,9 @@ def _vat_findings_query(im_name, im_version):
     else:
         logging.warning(f"Unknown response from VAT {r.status_code}")
         logging.warning(r.text)
-        logging.error("Please contact POPs to ensure we are able to receive a response or rerun the pipeline")
+        logging.error(
+            "Please contact POPs to ensure we are able to receive a response or rerun the pipeline"
+        )
         global api_exit_code
         api_exit_code = 3
 
