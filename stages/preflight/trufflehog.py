83ba57fe9c62510665190091a6a40fd5bac7bfdd
David Freeman <davidfreeman@seed-innovations.com>
2021-05-14 15:14:06 -0600

    add newline to get_history_cmd docstring

--- stages/preflight/trufflehog.py
+++ stages/preflight/trufflehog.py
@@ -96,7 +96,8 @@ def main() -> None:
 
 def get_history_cmd(repo_dir: str, diff_branch: str) -> list[str]:
     """
-    Uses gitpython to get a list of commit shasums of feature branch commits that don't exist in development, or for commits in development that aren't in master when CI_COMMIT_BRANCH is development
+    Uses gitpython to get a list of commit shasums of feature branch commits that don't exist in development,
+    or for commits in development that aren't in master when CI_COMMIT_BRANCH is development
     Returns a list of truffleHog3 flags
         [--since-commit, the oldest sha in the commits list]
         if list is empty [--no-history]
