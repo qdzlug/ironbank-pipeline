7622a3ae8372fb3bddb76a2ad9835f581d659347
David Freeman <davidfreeman@seed-innovations.com>
2021-06-03 15:12:41 -0600

    add auth to create-tar

--- stages/documentation/create-tar.sh
+++ stages/documentation/create-tar.sh
@@ -3,4 +3,5 @@ set -Eeuo pipefail
 
 mkdir -p "${ARTIFACT_DIR}/reports"
 
+echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
 skopeo copy --src-authfile staging_auth.json "docker://${STAGING_REGISTRY_URL}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}" "docker-archive:${ARTIFACT_DIR}/reports/${IMAGE_FILE}.tar"
