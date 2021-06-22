fc0e0d9fd37854043a6d4817691d79593b96dba5
cvernooy <cvernooy@oteemo.com>
2021-06-22 18:05:59 -0400

    udpates

--- stages/s3/create_repo_map_default.py
+++ stages/s3/create_repo_map_default.py
@@ -125,9 +125,9 @@ def main():
             "Image_URL": os.environ["image_url"],
             "Anchore_Security_Results": os.environ["anchore_security_results"],
             "Image_Sha": os.environ["image_sha"],
-#            "OpenSCAP_Compliance_Results": os.environ["openscap_compliance_results"],
+            "OpenSCAP_Compliance_Results": os.environ.get("openscap_compliance_results") if not os.environ.get("DISTROLESS") else None,
             "Tar_Name": os.environ["tar_name"],
-#            "OpenSCAP_Report": os.environ["openscap_report"],
+            "OpenSCAP_Report": os.environ.get("openscap_report") if not os.environ.get("DISTROLESS") else None,
             "Image_Tag": os.environ["image_tag"],
             "Manifest_Name": os.environ["manifest_name"],
             "Approval_Status": approval_status,
@@ -140,14 +140,11 @@ def main():
             "Full_Report": os.environ["full_report"],
             "Repo_Name": os.environ["repo_name"],
             "Keywords": keyword_list,
+            "Digest": os.environ['image_podman_sha'],
             "Tags": tag_list,
             "Labels": label_dict,
         }
     }
-
-    if container_type != "distroless":
-        new_data["build_number"]["OpenSCAP_Report"]=os.environ["openscap_report"],
-        new_data["build_number"]["OpenSCAP_Compliance_Results"]=os.environ["openscap_compliance_results"]
 
     logging.debug(f"repo_map data:\n{new_data}")
 
@@ -162,40 +159,12 @@ def main():
         with open("repo_map.json", "w") as outfile:
             json.dump(new_data, outfile, indent=4, sort_keys=True)
 
-##CV: Unsure if we can remove the above upload to S3 or if anything else relies on the file, re-reading and posting to IBFE 
-#to prevent breaking anything. This can be changed if we are confident nothing downstream consumes or expects 
-#repo_map.json. UNIX style processing -- dirty but works
     try:
-        with open('repo_map.json', 'r') as j:
-            json_data = json.loads(j.read())
-    except FileNotFoundError as f_not_found_err:
-        logging.error(f"repo_map.json file not found!\n{f_not_found_err}")
-        exit(1)
-    except Exception as e:
-        logging.error(f"repo_map.json file was found, but an unhandled error occured\n{e}")
-        exit(1)
-    try:
-        requests.post(os.environ["IBFE_API_ENDPOINT"], data = os.environ["IBFE_API_KEY"], json=data[os.environ["build_number"]])
+        requests.post(os.environ["IBFE_API_ENDPOINT"], auth=os.environ["IBFE_API_KEY"], json=new_data[os.environ["build_number"]])
         logging.info("Uploaded container data to IBFE API")
     except requests.exceptions.RequestException as request_e:
         logging.error(f"Error submitting container data to IBFE API\n{request_e}")
-
-##CV: I don't like this and would prefer not to test if variable exists, except to do a thing, else do a different thing. 
-# that feels pretty unclean. UNIX style processing above is hacky but not as bad as below
-#    try: data
-#    except NameError: 
-#        try:
-#            r = requests.post(os.environ["IBFE_API_ENDPOINT"], data = os.environ["IBFE_API_KEY"], json=new_data)
-#            logging.debug(r)
-#        except Exception as post_e:
-#            logging.error(f"Error sending data to IBFE:\n{post_e}")        
-#    else:
-#        try:
-#            r = requests.post(os.environ["IBFE_API_ENDPOINT"], data = os.environ["IBFE_API_KEY"], json=data)
-#            logging.debug(r)
-#        except Exception as post_e:
-#            logging.error(f"Error sending data to IBFE:\n{post_e}")
-
+        sys.exit(1)
 
 if __name__ == "__main__":
     sys.exit(main())
