caaeeb6b3f76532b6753dd2778fb86d56a84af9f
David Freeman <davidfreeman@seed-innovations.com>
2021-06-28 18:33:40 -0600

    remove --format as this version of skopeo doesn't have this option

--- stages/check-cves/pipeline_wl_compare.py
+++ stages/check-cves/pipeline_wl_compare.py
@@ -540,8 +540,6 @@ def _get_complete_whitelist_for_image(image_name, whitelist_branch, hardening_ma
             "--authfile",
             "prod_auth.json",
             f"docker://registry1.dso.mil/ironbank/{parent_image_path}:{base_tag}",
-            "--format",
-            '{{ index .Labels "org.opencontainers.image.source" }}',
         ]
         logging.info(" ".join(cmd))
         try:
