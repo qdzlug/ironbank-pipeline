e0b1eb94f89933f25388e2d8fa1a6f6249721fc9
David Freeman <davidfreeman@seed-innovations.com>
2021-07-14 09:19:51 -0600

    remove commented out code

--- stages/s3/create_repo_map_default.py
+++ stages/s3/create_repo_map_default.py
@@ -163,34 +163,6 @@ def main():
         with open("repo_map.json", "w") as outfile:
             json.dump(new_data, outfile, indent=4, sort_keys=True)
 
-    # if os.environ["CI_COMMIT_BRANCH"] == "master":
-    #     try:
-    #         new_data = new_data[os.environ["build_number"]]
-    #         post_resp = requests.post(
-    #             os.environ["IBFE_API_ENDPOINT"],
-    #             headers={"Authorization": os.environ["IBFE_API_KEY"]},
-    #             json=new_data,
-    #         )
-    #         logging.info("Uploaded container data to IBFE API")
-    #         post_resp.raise_for_status()
-    #     except requests.exceptions.Timeout as req_timeout:
-    #         logging.error(
-    #             f"Unable to reach the IBFE API, TIMEOUT. stack trace\n{req_timeout}"
-    #         )
-    #         sys.exit(1)
-    #     except requests.exceptions.HTTPError as http_err:
-    #         logging.info(f"Got HTTP {post_resp.status_code}")
-    #         logging.error(f"HTTP error\n{http_err}")
-    #         sys.exit(1)
-    #     except requests.exceptions.RequestException as request_e:
-    #         logging.error(f"Error submitting container data to IBFE API\n{request_e}")
-    #         sys.exit(1)
-    #     except Exception as e:
-    #         logging.error(f"Unhandled exception\n{e}")
-    #         sys.exit(1)
-    # else:
-    #     logging.debug("Skipping use of ibfe api build endpoint")
-
 
 if __name__ == "__main__":
     sys.exit(main())
