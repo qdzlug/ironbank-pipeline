b287a277ea2239bec662b759541a9a51e329f582
David Freeman <david.freeman.26.ctr@us.af.mil>
2021-08-11 14:28:07 -0600

    call shell scripts from job yaml

--- stages/documentation/documentation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-
-echo "Creating and signing Manifest JSON"
-"${PIPELINE_REPO_DIR}/stages/documentation/sign-manifest-run.sh"
-
-echo "Creating documentation and scan-metadata JSON files"
-"${PIPELINE_REPO_DIR}/stages/documentation/write-json-docs-run.sh"
