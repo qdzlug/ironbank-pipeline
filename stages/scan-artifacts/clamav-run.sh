b6d12e3d5c3fe4e434d82879184d2c8d68bc04c1
David Freeman <david.freeman.26.ctr@us.af.mil>
2021-09-16 16:45:15 -0600

    run clamscan on import-artifacts dir and remove for loop

--- stages/scan-artifacts/clamav-run.sh
+++ stages/scan-artifacts/clamav-run.sh
@@ -21,7 +21,4 @@ if [[ "${CLAMAV_WHITELIST:-}" ]]; then
   cp "clamav-whitelist" /usr/local/share/clamav/clamav_whitelist.ign2
 fi
 
-for filename in "${ARTIFACT_STORAGE}"/import-artifacts/*/*; do
-  # TODO: fix or temporarily skip scans of large files
-  clamscan -irv --max-filesize=4000M --max-scansize=4000M "${filename}" | tee -a "${ARTIFACT_DIR}/import-artifacts-clamav-report.txt"
-done
+clamscan -irv --max-filesize=4000M --max-scansize=4000M "${ARTIFACT_STORAGE}"/import-artifacts | tee -a "${ARTIFACT_DIR}/import-artifacts-clamav-report.txt"
