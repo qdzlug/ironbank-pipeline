3320ce65148820b4ba7b3b8ce9e9d1375341acab
David Freeman <david.freeman.26.ctr@us.af.mil>
2021-12-07 18:26:47 -0700

    fix var name

--- stages/scanning/openscap/oscap-compliance-run.sh
+++ stages/scanning/openscap/oscap-compliance-run.sh
@@ -18,7 +18,7 @@ mkdir -p "${SCAP_CONTENT}"
 
 # If SCAP_URL var exists, use this to download scap content, else retrieve it based on BASE_IMAGE_TYPE
 if [[ -n ${SCAP_URL:-} ]]; then
-  curl -L "${SCAP_UR}" -o "${SCAP_CONTENT}/scap-security-guide.zip"
+  curl -L "${SCAP_URL}" -o "${SCAP_CONTENT}/scap-security-guide.zip"
 elif [[ "${BASE_IMAGE_TYPE}" == "ubuntu1604-container" ]]; then
   curl -L "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_CAN_Ubuntu_16-04_LTS_V2R2_STIG_SCAP_1-2_Benchmark.zip" -o "${SCAP_CONTENT}/scap-security-guide.zip"
 elif [[ "${BASE_IMAGE_TYPE}" == "ubuntu1804-container" ]]; then
