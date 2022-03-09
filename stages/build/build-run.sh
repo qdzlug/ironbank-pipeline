db066f8eb51d60bcdf195545eb2f7f572569968e
Ariel Shnitzer <arielshnitzer@seed-innovations.com>
2022-03-09 13:52:36 -0700

    update to oci format build

--- stages/build/build-run.sh
+++ stages/build/build-run.sh
@@ -89,7 +89,7 @@ env -i BUILDAH_ISOLATION=chroot PATH="$PATH" buildah bud \
   --label=org.opencontainers.image.source="${CI_PROJECT_URL}" \
   --label=org.opencontainers.image.revision="${CI_COMMIT_SHA}" \
   --authfile /tmp/prod_auth.json \
-  --format=docker \
+  --format=oci \
   --log-level=warn \
   --default-mounts-file="${HOME}"/.config/containers/mounts.conf \
   --storage-driver=vfs \
