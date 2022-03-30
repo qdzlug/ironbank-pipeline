2184c4ffdc22c9d92ca31c92f79844bd1d0d5f74
Cody Miller <codymiller@seed-innovations.com>
2022-03-30 09:43:26 -0600

    Run black formatter

--- scripts/modules/classes/utils/logger.py
+++ scripts/modules/classes/utils/logger.py
@@ -7,6 +7,7 @@ DEFAULT_FORMAT = "%(name)-12s  %(levelname)-8s %(message)s"
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format=DEFAULT_FORMAT)
 
+
 def setup(name="main", level=logging.INFO, format=DEFAULT_FORMAT, debug_file=None):
     logger = logging.getLogger(name)
     logger.setLevel(level)
