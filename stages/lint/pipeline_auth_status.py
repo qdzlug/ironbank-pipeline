37fcaf06b901a014c6c03271346050fca460a63c
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-03-31 19:11:52 -0600

    use api module for check access

--- stages/lint/pipeline_auth_status.py
+++ stages/lint/pipeline_auth_status.py
@@ -7,33 +7,26 @@ import sys
 from requests.models import HTTPError
 from requests.structures import CaseInsensitiveDict
 
+sys.path.append(
+    os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "scripts/modules"
+    )
+)
+
+
+from classes.project import CHT_Project
+from classes.apis import VAT_API
+from hardening_manifest import Hardening_Manifest
+
 
 def main() -> None:
-    base_url = os.environ["VAT_BACKEND_SERVER_ADDRESS"]
-    url_encoded_container_name = urllib.parse.quote(os.environ["IMAGE_NAME"], safe="")
-    url = f"{base_url}/internal/import/check-access/{url_encoded_container_name}"
-    params = {"createRequest": "true"}
-    headers = CaseInsensitiveDict()
-    headers["Content-Type"] = "application/json"
-    headers["Authorization"] = f"Bearer {os.environ['CI_JOB_JWT']}"
-    logging.info(f"Request URL: {url}")
-    try:
-        r = requests.get(url, headers=headers, params=params)
-        r.raise_for_status()
-        logging.info("Retrieve Auth Status from VAT")
-        logging.info(f"Response: {r.text}")
-        logging.debug(f"JSON Response:\n{r.json}")
-    except HTTPError:
-        if r.status_code == 403:
-            logging.info(
-                f"{os.environ['CI_PROJECT_NAME']} is not authorized to use the image name of: {os.environ['IMAGE_NAME']}. Either the name has changed or the container has never been tracked in VAT. An authorization request has automatically been generated. Please create a ticket with the link below for VAT authorization review."
-            )
-            logging.info(
-                f"https://repo1.dso.mil/dsop/dccscr/-/issues/new?issuable_template=VAT%20Pipeline%20Access%20Request&issue[title]=VAT+Pipeline+Access+Request+{urllib.parse.quote(os.environ['CI_PROJECT_URL'], safe='')}"
-            )
-        else:
-            logging.exception("HTTPError")
-        sys.exit(1)
+    cht_project = CHT_Project()
+    hardening_manifest = Hardening_Manifest(cht_project.hardening_manifest_path)
+    vat_api = VAT_API(url=os.environ["VAT_BACKEND_SERVER_ADDRESS"])
+    vat_api.check_access(image_name=hardening_manifest.image_name, create_request=True)
+    logging.info("Retrieve Auth Status from VAT")
+    logging.info(f"Response: {vat_api.response.text}")
+    logging.debug(f"JSON Response:\n{vat_api.response.json}")
 
 
 if __name__ == "__main__":
