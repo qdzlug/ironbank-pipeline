5dea50d52a02f877e04e0f299479d1633d46f04d
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-03-31 19:11:05 -0600

    remove unneeded obj from params and refactor

--- stages/lint/container_status_check.py
+++ stages/lint/container_status_check.py
@@ -18,28 +18,13 @@ from hardening_manifest import Hardening_Manifest
 from vat_container_status import is_approved
 
 
-def main():
-    # approval_status, approval_text = _get_vat_findings_api(
-    #     os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"]
-    # )
-    cht_project = CHT_Project()
-    hardening_manifest = Hardening_Manifest(cht_project.hardening_manifest_path)
-    vat_api = VAT_API(url=f"{os.environ['VAT_BACKEND_SERVER_ADDRESS']}")
-    vat_response = vat_api._get_container(
-        vat_api,
-        image_name=hardening_manifest.image_name,
-        image_tag=hardening_manifest.image_tag,
-    )
-
-    logging.debug(f"VAT response\n{vat_response}")
+def create_api_findings_artifact(vat_response: dict) -> None:
     filename = Path(os.environ["ARTIFACT_DIR"], "vat_api_findings.json")
     with filename.open(mode="w") as f:
         json.dump(vat_response, f)
 
-    approved, _, approval_status, approval_comment = is_approved(vat_response, False)
-    approval_status = approval_status.lower().replace(" ", "_")
-    logging.debug("updated Approval Status: {approval_status}")
 
+def create_approval_artifact(approval_status: str, approval_comment: str) -> None:
     filename = Path(os.environ["ARTIFACT_DIR"], "image_approval.json")
     image_approval = {
         "IMAGE_APPROVAL_STATUS": approval_status,
@@ -47,6 +32,27 @@ def main():
     }
     with filename.open(mode="w") as f:
         json.dump(image_approval, f)
+
+
+def main():
+    # approval_status, approval_text = _get_vat_findings_api(
+    #     os.environ["IMAGE_NAME"], os.environ["IMAGE_VERSION"]
+    # )
+    cht_project = CHT_Project()
+    hardening_manifest = Hardening_Manifest(cht_project.hardening_manifest_path)
+    vat_api = VAT_API(url=os.environ["VAT_BACKEND_SERVER_ADDRESS"])
+    vat_response = vat_api.get_image(
+        image_name=hardening_manifest.image_name, image_tag=hardening_manifest.image_tag
+    )
+
+    logging.debug(f"VAT response\n{vat_response}")
+    create_api_findings_artifact(vat_response)
+
+    approved, _, approval_status, approval_comment = is_approved(vat_response, False)
+    approval_status = approval_status.lower().replace(" ", "_")
+
+    logging.debug("updated Approval Status: {approval_status}")
+    create_approval_artifact(approval_status, approval_comment)
 
     if approved:
         logging.info("This container is noted as an approved image in VAT")
