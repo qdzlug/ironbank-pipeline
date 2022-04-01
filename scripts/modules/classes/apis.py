adbe7747d7b5610d14aab0ceb507456d05a6f31e
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-03-31 19:09:17 -0600

    add check-access endpoint

--- scripts/modules/classes/apis.py
+++ scripts/modules/classes/apis.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import requests
+import os
 import sys
 import logging
 
@@ -29,9 +30,9 @@ class API:
 def request_error_handler(func):
     def _request_error_handler(self, *args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            return func(self, *args, **kwargs)
         except requests.exceptions.HTTPError:
-            if self.response.status_code == 404:
+            if self.response.status_code == 404 and func.__name__ == "get_image":
                 logging.warning(
                     f"{kwargs.get('image_name')}:{kwargs.get('image_tag')} not found in {self.app}"
                 )
@@ -40,6 +41,13 @@ def request_error_handler(func):
                 logging.warning(f"Bad request: {self.url}")
                 logging.warning(self.response.text)
                 sys.exit(1)
+            elif self.response.status_code == 403:
+                logging.warning(
+                    f"{os.environ['CI_PROJECT_NAME']} is not authorized to use the image name of: {kwargs.get('image_name')}. Either the name has changed or the container has never been tracked in VAT. An authorization request has automatically been generated. Please create a ticket with the link below for VAT authorization review."
+                )
+                logging.info(
+                    f"https://repo1.dso.mil/dsop/dccscr/-/issues/new?issuable_template=VAT%20Pipeline%20Access%20Request&issue[title]=VAT+Pipeline+Access+Request+{os.environ['CI_PROJECT_URL']}"
+                )
             else:
                 logging.warning(
                     f"Unknown response from VAT {self.response.status_code}"
@@ -65,11 +73,12 @@ class VAT_API(API):
     container_route: str = "/p1/container"
     import_route: str = "/internal/import"
     import_scan_route: str = f"{import_route}/scan"
+    import_access_route: str = f"{import_route}/check-access"
     import_artifacts_route: str = f"{import_route}/artifacts"
 
     @request_error_handler
-    def _get_container(self, image_name, image_tag) -> dict:
-        logging.info("Running query to vat api")
+    def get_image(self, image_name, image_tag) -> dict:
+        logging.info("Getting image information from vat api")
         self.response = requests.get(
             f"{self.url}{self.container_route}",
             params={"name": image_name, "tag": image_tag},
@@ -79,6 +88,20 @@ class VAT_API(API):
         if self.response.status_code not in [200, 404]:
             sys.exit(1)
         return self.response.json() if self.response.status_code == 200 else None
+
+    @request_error_handler
+    def check_access(self, image_name, create_request=False):
+        logging.info(f"Checking access to {image_name}")
+        logging.info(f"{self.url}{self.import_access_route}/{image_name}")
+        self.response = requests.get(
+            f"{self.url}{self.import_access_route}/{image_name}",
+            params={"createRequest": create_request},
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {os.environ['CI_JOB_JWT']}",
+            },
+        )
+        self.response.raise_for_status()
 
     @request_error_handler
     def _force_400(self) -> dict:
