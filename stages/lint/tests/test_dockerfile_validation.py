e1f01b9f3f07eb2e2a620663f46b3833aa4172c0
Michael Simmons <michael.simmons.50.ctr@us.af.mil>
2022-04-26 15:49:02 -0600

    Install dockerfile pip package for tests

--- stages/lint/tests/test_dockerfile_validation.py
+++ stages/lint/tests/test_dockerfile_validation.py
@@ -16,17 +16,17 @@ logging.basicConfig(level="INFO", format="%(levelname)s: %(message)s")
 
 @pytest.fixture
 def good_dockerfile_path():
-    return "tests/mock/Dockerfile.test-good"
+    return "./mock/Dockerfile.test-good"
 
 
 @pytest.fixture
 def bad_dockerfile_path():
-    return "tests/mock/Dockerfile.test-bad"
+    return "./mock/Dockerfile.test-bad"
 
 
 @pytest.fixture
 def nonexistent_dockerfile_path():
-    return "tests/mock/Dockerfile"
+    return "./mock/Dockerfile"
 
 
 @pytest.fixture
