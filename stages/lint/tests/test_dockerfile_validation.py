7d675b521d602b9ce2169e3cbf6a1151056b6160
Michael Simmons <michael.simmons.50.ctr@us.af.mil>
2022-04-26 14:52:24 -0600

    Change file path for mock dockerfiles

--- stages/lint/tests/test_dockerfile_validation.py
+++ stages/lint/tests/test_dockerfile_validation.py
@@ -16,17 +16,17 @@ logging.basicConfig(level="INFO", format="%(levelname)s: %(message)s")
 
 @pytest.fixture
 def good_dockerfile_path():
-    return "./mock/Dockerfile.test-good"
+    return "tests/mock/Dockerfile.test-good"
 
 
 @pytest.fixture
 def bad_dockerfile_path():
-    return "./mock/Dockerfile.test-bad"
+    return "tests/mock/Dockerfile.test-bad"
 
 
 @pytest.fixture
 def nonexistent_dockerfile_path():
-    return "./mock/Dockerfile"
+    return "tests/mock/Dockerfile"
 
 
 @pytest.fixture
