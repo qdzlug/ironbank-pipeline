6f95bedfa36c91e8a214c3e9031ee027e22a289b
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-05-09 16:49:59 -0600

    refactor to remove need for sessions and additional request mocks

--- scripts/modules/test/test_apis.py
+++ scripts/modules/test/test_apis.py
@@ -22,7 +22,15 @@ def mock_vat_api():
 
 
 @dataclass
-class MockSessionResponse:
+class MockApiSubclass(API):
+    @request_error_handler
+    def mock_wrapped_func(self, mock_response):
+        self.response = mock_response(self.url)
+        self.response.raise_for_status()
+
+
+@dataclass
+class MockResponse:
     status_code: int
     text: str
 
@@ -34,78 +42,38 @@ class MockSessionResponse:
         return {"status_code": self.status_code, "text": self.text}
 
 
-# Various responses for a mocked request object (200, 400, 403, 500)
 @pytest.fixture
-def mock_responses(monkeypatch):
-    def mock200(url=""):
-        return MockSessionResponse(200, "successful_request")
+def mock_responses(url="", params={}, headers={}):
+    def mock200(url="", params={}, headers={}):
+        return MockResponse(200, "successful_request")
 
-    def mock400(url=""):
-        return MockSessionResponse(400, "bad_json_body")
+    def mock400(url="", params={}, headers={}):
+        return MockResponse(400, "bad_json_body")
 
-    def mock403(url=""):
-        return MockSessionResponse(403, "bad_auth")
+    def mock403(url="", params={}, headers={}):
+        return MockResponse(403, "bad_auth")
 
-    def mock500(url=""):
-        return MockSessionResponse(500, "server_ded")
+    def mock404(url="", params={}, headers={}):
+        return MockResponse(404, "not_found")
 
-    def mockRequestException(url=""):
+    def mock500(url="", params={}, headers={}):
+        return MockResponse(500, "server_ded")
+
+    def mockRequestException(url="", params={}, headers={}):
         raise requests.exceptions.RequestException
 
-    def mockRuntimeError(url=""):
+    def mockRuntimeError(url="", params={}, headers={}):
         raise RuntimeError
 
-    session200 = requests.session()
-    session400 = requests.session()
-    session403 = requests.session()
-    session500 = requests.session()
-    sessionReqEx = requests.session()
-    sessionRunErr = requests.session()
-    monkeypatch.setattr(session200, "get", mock200)
-    monkeypatch.setattr(session200, "put", mock200)
-    monkeypatch.setattr(session200, "post", mock200)
-    monkeypatch.setattr(session400, "get", mock400)
-    monkeypatch.setattr(session400, "put", mock400)
-    monkeypatch.setattr(session400, "post", mock400)
-    monkeypatch.setattr(session403, "get", mock403)
-    monkeypatch.setattr(session403, "put", mock403)
-    monkeypatch.setattr(session403, "post", mock403)
-    monkeypatch.setattr(session500, "get", mock500)
-    monkeypatch.setattr(session500, "put", mock500)
-    monkeypatch.setattr(session500, "post", mock500)
-    monkeypatch.setattr(sessionReqEx, "get", mockRequestException)
-    monkeypatch.setattr(sessionReqEx, "put", mockRequestException)
-    monkeypatch.setattr(sessionReqEx, "post", mockRequestException)
-    monkeypatch.setattr(sessionRunErr, "get", mockRuntimeError)
-    monkeypatch.setattr(sessionRunErr, "put", mockRuntimeError)
-    monkeypatch.setattr(sessionRunErr, "post", mockRuntimeError)
     return {
-        "200": session200,
-        "400": session400,
-        "403": session403,
-        "500": session500,
-        "requestErr": sessionReqEx,
-        "runErr": sessionRunErr,
+        "200": mock200,
+        "400": mock400,
+        "403": mock403,
+        "404": mock404,
+        "500": mock500,
+        "requestException": mockRequestException,
+        "runtimeError": mockRuntimeError,
     }
-
-
-class MockRequest:
-    def __init__(self, session):
-        self.image_name = "example/example/example"
-        self.url = "https://example/example.invalid"
-        self.session = session
-        self.response = None
-        self.log = logger.setup("mock_request_log")
-
-    # TODO: refactor this function out of this class and handle raise for status in response
-    def raise_for_status(self):
-        if self.response.status_code != 200:
-            raise requests.exceptions.HTTPError
-
-    @request_error_handler
-    def mock_func(self, image_name="example/example/example"):
-        self.response = self.session.get(url="https://invalid.invalid")
-        self.raise_for_status()
 
 
 @mock.patch.dict(
@@ -115,84 +83,68 @@ class MockRequest:
         "CI_PROJECT_URL": "https://example/example/example",
     },
 )
-def test_request_error_handler(caplog, mock_responses):
+def test_request_error_decorator(caplog, mock_responses):
     caplog.set_level(logging.INFO)
 
     logging.info("It shouldn't throw exception on 200")
-    mock_request = MockRequest(mock_responses["200"])
-    mock_request.mock_func()
-    assert mock_request.response.text == "successful_request"
-    assert mock_request.response.status_code == 200
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["200"])
+    assert api.response.text == "successful_request"
+    assert api.response.status_code == 200
     for record in caplog.records:
         assert record.levelname != "WARNING"
     caplog.clear()
 
-    # TODO: test thrown excpetion is caught
-    logging.info("It should throw exception on 400")
-    mock_request = MockRequest(mock_responses["400"])
-    mock_request.mock_func()
-    assert mock_request.response.text == "bad_json_body"
-    assert mock_request.response.status_code == 400
+    logging.info("It should handle exception on 400")
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["400"])
+    assert api.response.text == "bad_json_body"
+    assert api.response.status_code == 400
     assert "Bad request" in caplog.text
     assert "bad_json_body" in caplog.text
     caplog.clear()
 
-    mock_request = MockRequest(mock_responses["403"])
-    mock_request.mock_func()
-    assert mock_request.response.text == "bad_auth"
-    assert mock_request.response.status_code == 403
+    logging.info("It should handle exception on 403")
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["403"])
+    assert api.response.text == "bad_auth"
+    assert api.response.status_code == 403
     assert "is not authorized to use the image name of:" in caplog.text
     caplog.clear()
 
-    mock_request = MockRequest(mock_responses["500"])
-    mock_request.mock_func()
-    assert mock_request.response.text == "server_ded"
-    assert mock_request.response.status_code == 500
+    logging.info("It should handle exception on 500")
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["500"])
+    assert api.response.text == "server_ded"
+    assert api.response.status_code == 500
     assert "Unknown response from VAT" in caplog.text
     caplog.clear()
 
-    mock_request = MockRequest(mock_responses["requestErr"])
-    mock_request.mock_func()
-    assert mock_request.response is None
+    logging.info("It should handle request exception")
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["requestException"])
+    assert api.response is None
     assert "Could not access VAT" in caplog.text
     caplog.clear()
 
-    mock_request = MockRequest(mock_responses["runErr"])
-    mock_request.mock_func()
-    assert mock_request.response is None
+    logging.info("It should handle runtime error")
+    api = MockApiSubclass(url="https://example.local")
+    api.mock_wrapped_func(mock_responses["runtimeError"])
+    assert api.response is None
     assert "Unexpected exception" in caplog.text
     caplog.clear()
 
 
-def test_get_image_success(monkeypatch, caplog, mock_vat_api):
-    def mock_response(url: str, params: dict):
-        return MockSessionResponse(200, "successful_request")
+def test_get_image(monkeypatch, caplog, mock_vat_api, mock_responses):
 
-    monkeypatch.setattr(requests, "get", mock_response)
+    monkeypatch.setattr(requests, "get", mock_responses["200"])
     mock_vat_api.get_image("example/example/example", "1.0")
     assert "Fetched data from vat successfully" in caplog.text
     caplog.clear()
 
-
-def test_get_image_404(monkeypatch, caplog, mock_vat_api):
-    def mock_response(url: str, params: dict):
-        return MockSessionResponse(404, "not_found")
-
-    monkeypatch.setattr(requests, "get", mock_response)
+    monkeypatch.setattr(requests, "get", mock_responses["404"])
     mock_vat_api.get_image("example/example/example", "1.0")
     assert "not found in" in caplog.text
-    caplog.clear()
-
-
-@mock.patch.dict(os.environ, {"CI_JOB_JWT_V2": "abcdefg"})
-def test_check_access_success(monkeypatch, caplog, mock_vat_api):
-    def mock_response(url: str, params: dict, headers: dict):
-        return MockSessionResponse(200, "successful_request")
-
-    monkeypatch.setattr(requests, "get", mock_response)
-    mock_vat_api.check_access("example/example/example")
-    for record in caplog.records:
-        assert record.levelname != "WARNING"
     caplog.clear()
 
 
@@ -204,11 +156,15 @@ def test_check_access_success(monkeypatch, caplog, mock_vat_api):
         "CI_PROJECT_URL": "https://example.invalid",
     },
 )
-def test_check_access_failure(monkeypatch, caplog, mock_vat_api):
-    def mock_response(url: str, params: dict, headers: dict):
-        return MockSessionResponse(403, "bad_auth")
+def test_check_access(monkeypatch, caplog, mock_vat_api, mock_responses):
 
-    monkeypatch.setattr(requests, "get", mock_response)
+    monkeypatch.setattr(requests, "get", mock_responses["200"])
+    mock_vat_api.check_access("example/example/example")
+    for record in caplog.records:
+        assert record.levelname != "WARNING"
+    caplog.clear()
+
+    monkeypatch.setattr(requests, "get", mock_responses["403"])
     mock_vat_api.check_access("example/example/example")
     assert "is not authorized to use the image name of:" in caplog.text
     caplog.clear()
