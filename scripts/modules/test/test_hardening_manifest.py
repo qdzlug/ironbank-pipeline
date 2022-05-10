db91d2f707a6113461c0cdc83dbaa74c9375083f
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-05-10 14:12:25 -0600

    add tests for validate schema with timeout

--- scripts/modules/test/test_hardening_manifest.py
+++ scripts/modules/test/test_hardening_manifest.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
+import multiprocessing
 import sys
 import os
 import logging
 import pytest
 from pathlib import Path
+from unittest import mock
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -49,9 +52,9 @@ def load_bad_labels():
 @pytest.fixture
 def load_good_maintainers():
     return {
-        "name": "Josh Eason",
-        "username": "jeason",
-        "email": "josheason@seed-innovations.com",
+        "name": "Example Examp",
+        "username": "example",
+        "email": "example@company.com",
     }
 
 
@@ -59,8 +62,8 @@ def load_good_maintainers():
 def load_bad_maintainers():
     return {
         "name": "FIXME",
-        "username": "jeason",
-        "email": "josheason@seed-innovations.com",
+        "username": "example",
+        "email": "example@company.com",
     }
 
 
@@ -71,11 +74,101 @@ def hm():
     )
 
 
+@pytest.fixture
+def mock_empty():
+    def mock_none(_):
+        return None
+
+    def mock_empty_arr(_):
+        return []
+
+    def mock_empty_str(_):
+        return ""
+
+    return {"none": mock_none, "arr": mock_empty_arr, "str": mock_empty_str}
+
+
 def test_find_fixme(
     hm, load_good_labels, load_good_maintainers, load_bad_labels, load_bad_maintainers
 ):
-
     assert hm.check_for_fixme(load_good_labels) == []
     assert hm.check_for_fixme(load_good_maintainers) == []
     assert hm.check_for_fixme(load_bad_labels) == ["org.opencontainers.image.licenses"]
     assert hm.check_for_fixme(load_bad_maintainers) == ["name"]
+
+
+def test_validate(monkeypatch, caplog, hm, mock_empty):
+    caplog.set_level(logging.INFO)
+    monkeypatch.setattr(
+        HardeningManifest, "validate_schema_with_timeout", mock_empty["none"]
+    )
+    monkeypatch.setattr(HardeningManifest, "reject_invalid_labels", mock_empty["arr"])
+    monkeypatch.setattr(
+        HardeningManifest, "reject_invalid_maintainers", mock_empty["arr"]
+    )
+    monkeypatch.setattr(
+        HardeningManifest, "reject_invalid_image_sources", mock_empty["arr"]
+    )
+    hm.validate()
+    logging.info(caplog.text)
+    assert "Checking for" in caplog.text
+    caplog.clear()
+
+
+@dataclass
+class MockProcess:
+    alive: bool = True
+    exitcode: int = 0
+
+    def recv(self):
+        return None
+
+    def start(self):
+        return None
+
+    def is_alive(self):
+        return self.alive
+
+    def terminate(self):
+        self.alive = False
+
+
+@mock.patch.dict(os.environ, {"HM_VERIFY_TIMEOUT": "1"})
+def test_validate_schema_with_timeout(monkeypatch, caplog, hm):
+    def mock_pipe():
+        return (MockProcess(), MockProcess())
+
+    def mock_successful_process(target="", args=()):
+        return MockProcess(alive=False)
+
+    def mock_backtracking_process(target="", args=()):
+        return MockProcess()
+
+    def mock_failed_process(target="", args=()):
+        return MockProcess(alive=False, exitcode=1)
+
+    caplog.set_level(logging.INFO)
+
+    logging.info("It should successfully validate the hardening manifest")
+    monkeypatch.setattr(multiprocessing, "Pipe", mock_pipe)
+    monkeypatch.setattr(multiprocessing, "Process", mock_successful_process)
+    hm.validate_schema_with_timeout()
+    for record in caplog.records:
+        assert record.levelname != "ERROR" and record.levelname != "WARNING"
+    caplog.clear()
+
+    with pytest.raises(SystemExit) as exc_info1:
+        logging.info("It should cause catastrophic backtracking")
+        monkeypatch.setattr(multiprocessing, "Process", mock_backtracking_process)
+        hm.validate_schema_with_timeout()
+    assert exc_info1.type == SystemExit
+    assert "Hardening Manifest validation timeout exceeded" in caplog.text
+    caplog.clear()
+
+    with pytest.raises(SystemExit) as exc_info2:
+        logging.info("It should fail to validate the hardening manifest")
+        monkeypatch.setattr(multiprocessing, "Process", mock_failed_process)
+        hm.validate_schema_with_timeout()
+    assert exc_info2.type == SystemExit
+    assert "Hardening Manifest failed jsonschema validation" in caplog.text
+    caplog.clear()
