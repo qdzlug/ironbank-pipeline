759f00207f2ba4cdfd4457f83940d86cc7fba7f7
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-05-10 14:10:14 -0600

    update logging

--- scripts/modules/hardening_manifest.py
+++ scripts/modules/hardening_manifest.py
@@ -44,10 +44,10 @@ class HardeningManifest:
     def validate(self):
         self.validate_schema_with_timeout()
         # verify no labels have a value of fixme (case insensitive)
-        log.debug("Checking for FIXME values in labels/maintainers")
+        log.info("Checking for FIXME values in labels/maintainers")
         self.invalid_labels = self.reject_invalid_labels()
         self.invalid_maintainers = self.reject_invalid_maintainers()
-        log.debug("Checking for invalid image sources")
+        log.info("Checking for invalid image sources")
         self.invalid_image_sources = self.reject_invalid_image_sources()
 
     def validate_schema_with_timeout(self):
@@ -66,7 +66,7 @@ class HardeningManifest:
             if not process.is_alive():
                 break
         if process.is_alive():
-            log.error("Hardening Manifest validation timeout exceeded.")
+            log.error("Hardening Manifest validation timeout exceeded")
             log.error(
                 "This is likely due to field in the hardening_manifest.yaml being invalid and \
                 causing an infinite loop during validation"
