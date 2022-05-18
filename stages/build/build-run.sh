8eb2d9fa8635ff65e2fa573a6eddbc195d1ebaa5
Michael Simmons <michael.simmons.50.ctr@us.af.mil>
2022-05-18 14:08:27 -0600

    Adjust quotations to fix globbing probability

--- stages/build/build-run.sh
+++ stages/build/build-run.sh
@@ -70,7 +70,7 @@ fi
 # buildah bud ignores this requirement for http/ftp/no proxy envvars, but we're required to do this for anything else.
 cp "${PIPELINE_REPO_DIR}"/stages/build/build-args.txt .
 sed -i '/^FROM /r build-args.txt' Dockerfile
-BASE_SHA=$(grep -Po '(?<="BASE_SHA": ")[^"]*' ${ARTIFACT_STORAGE}/lint/base_image.json) # noqa SC2086
+BASE_SHA=$(grep -Po "(?<='BASE_SHA': ')[^']*" ${ARTIFACT_STORAGE}/lint/base_image.json) # noqa SC2086
 old_ifs=$IFS
 IFS=$'\n'
 echo "Build the image"
