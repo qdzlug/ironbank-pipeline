1291acfbadeb4196769ebc1216f9b6a30a3323f8
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-06-08 17:01:13 -0600

    use environ get

--- scripts/modules/abstract_artifacts.py
+++ scripts/modules/abstract_artifacts.py
@@ -19,7 +19,9 @@ class AbstractArtifact(ABC):
     dest_path: pathlib.Path = None
 
     def __post_init__(self):
-        self.dest_path = self.dest_path or pathlib.Path(f"{os.environ['ARTIFACT_DIR']}")
+        self.dest_path = self.dest_path or pathlib.Path(
+            f"{os.environ.get('ARTIFACT_DIR')}"
+        )
         self.artifact_path = pathlib.Path(self.dest_path, self.filename or self.tag)
 
     def delete_artifact(self):
