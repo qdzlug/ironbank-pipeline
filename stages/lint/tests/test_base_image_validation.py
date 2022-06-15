5e5805a20fb6431a5263b46210ccbee8527abb64
Michael Simmons <michael.simmons.50.ctr@us.af.mil>
2022-06-15 13:03:25 -0600

    Finished tests for skopeo inspect base image validation

--- stages/lint/tests/test_base_image_validation.py
+++ stages/lint/tests/test_base_image_validation.py
@@ -53,22 +53,49 @@ class MockSubprocessReturn:
 def mock_subprocess_run(*args, **kwargs):
     return MockSubprocessReturn("data")
 
+def mock_subprocess_fail(*args, **kwargs):
+    raise subprocess.CalledProcessError(1, ["cmd"])
+
+# TODO: add this to a module
+def raise_(e):
+    raise e
+
 
 
 # TODO: update these tests to mock skopeo calls
 @pytest.mark.only
-def test_skopeo_inspect_good_base_image(monkeypatch, good_base_image):
-    monkeypatch.setenv("STAGING_BASE_IMAGE", 1)
+def test_skopeo_inspect_base_image(monkeypatch, caplog, good_base_image):
+    monkeypatch.setenv("STAGING_BASE_IMAGE", "base")
     monkeypatch.setenv("DOCKER_AUTH_CONFIG_STAGING", "c3RhZ2luZy10ZXN0Cg==") # staging-test -> base64 encoded value
-    monkeypatch.setattr(pathlib.Path, "write_text", lambda x, y: True)
+    monkeypatch.setattr(pathlib.Path, "write_text", lambda self, x: True)
     monkeypatch.setattr(pathlib.Path, "open", mock_open(read_data='data'))
     monkeypatch.setattr(subprocess, "run", mock_subprocess_run)
     monkeypatch.setenv("ARTIFACT_DIR", "right/here")
     monkeypatch.setattr(json, "dump", lambda x,y: True)
 
     skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
+    assert "staging_pull_auth.json" in caplog.text
+    caplog.clear()
 
     monkeypatch.delenv("STAGING_BASE_IMAGE")
     monkeypatch.setenv("DOCKER_AUTH_CONFIG_PULL", "c3RhZ2luZy10ZXN0Cg==")
 
     skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
+    assert "prod_pull_auth.json" in caplog.text
+    caplog.clear()
+
+    monkeypatch.setattr(subprocess, "run", mock_subprocess_fail)
+
+    with pytest.raises(SystemExit):
+        skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
+
+    assert "Failed to inspect BASE_IMAGE:BASE_TAG provided in hardening_manifest." in caplog.text
+    caplog.clear()
+
+    monkeypatch.setattr(subprocess, "run", lambda self: raise_(Exception))
+
+    with pytest.raises(SystemExit):
+        skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
+
+    assert "Unknown failure when attemping to inspect BASE_IMAGE" in caplog.text
+    caplog.clear()
