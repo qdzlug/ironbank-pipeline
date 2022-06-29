e7887bef7892c4f73009d21b643731ba40dbd8a4
Michael Simmons <michael.simmons.50.ctr@us.af.mil>
2022-06-29 08:55:28 -0600

    Added in code to get sbom file name from harbor

---
+++ scripts/analysis/sbom_parser.py
@@ -0,0 +1,121 @@
+import os, sys, re, argparse
+from pathlib import Path
+from collections import namedtuple
+
+sys.path.append(
+    os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "scripts/modules"
+    )
+)
+
+from utils import logger  # noqa: E402
+
+log = logger.setup(name="access_log_parser", format="| %(levelname)-5s | %(message)s")
+
+
+TYPES = {"python": "python-package-cataloger", "rpm": "rpmdb-cataloger"}
+
+Package = namedtuple("Package", ["type", "package", "version"])
+package_tuples = []
+
+
+def rpm_parser(url_path: str) -> Package:
+
+    match = re.match(
+        "(?P<name>.*?)/(:?@v/(?P<version>.*?)\.(?P<ext>[^.]+)|(?P<latest>@latest))$",
+        url_path,
+    )
+
+    if not match:
+        raise ValueError(f"Could not parse go URL: {url_path}")
+
+    if match.group("ext") in ["zip", "info"] or match.group("latest"):
+        return None
+    elif match.group("ext") != "mod":
+        raise ValueError(f"Unexpected go mod extension: {url_path}")
+    else:
+        return Package("go", match.group("name"), match.group("version"))
+
+
+def python_parser(url_path: str) -> Package:
+    pass
+
+
+def null_parser(url: str) -> None:
+    return None
+
+
+PARSERS = {
+    "gosum": null_parser,
+    "go": go_parser,
+    "yum": yum_parser,
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script used to parse access_log files"
+    )
+    parser.add_argument(
+        "--allow-errors",
+        action="store_true",
+        help="allow parsing to continue upon encountering an error",
+    )
+    parser.add_argument(
+        "path",
+        type=str,
+        help="path to access_log file",
+    )
+    args = parser.parse_args()
+
+    nexus_host = "http://nexus-repository-manager.nexus-repository-manager.svc.cluster.local:8081/repository/"
+    nexus_parser = re.compile(
+        f"({re.escape(nexus_host)})(?P<repo_type>[^/]+)/(?P<url>.*)"
+    )
+
+    try:
+        access_log = Path(args.path).open("r")
+        log.info(f"File successfully read. Parsing...")
+        line_count = 0
+    except OSError:
+        log.error("Unable to open file.")
+        sys.exit(1)
+
+    with access_log:
+        for line in access_log.readlines():
+            line_count += 1
+            try:
+                line = line.rstrip("\n")
+
+                if not line.startswith("200"):
+                    continue
+
+                # split on spaces and get the url
+                url = line.split(" ")[-1]
+
+                # match against the nexus repo parser
+                match = nexus_parser.match(url)
+
+                if not match:
+                    raise ValueError(f"Could not find parser for URL: {url}")
+
+                repo_type = match.group("repo_type")
+                # get repository from list
+                if repo_type not in REPOS:
+                    raise ValueError(f"Repository type not supported: {repo_type}")
+
+                repo = REPOS[repo_type]
+                # call desired parser function stored in dictionary
+                package = PARSERS[repo](match.group("url"))
+                if package:
+                    package_tuples.append(package)
+                    log.info(
+                        f"Parsed Package: {package.package} version={package.version} type={package.type}"
+                    )
+
+            except ValueError as e:
+                log.error(f"Unable to parse line: {line_count}")
+                log.error(e)
+                if not args.allow_errors:
+                    sys.exit(1)
+    log.info(f"File Successfully Parsed.")
