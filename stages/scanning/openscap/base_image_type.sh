327d1608a0512fca1ea37002e8360e57949dac58
David Freeman <david.freeman.26.ctr@us.af.mil>
2022-08-17 09:31:08 -0600

    check for ironbank.os-type label, before redhat label

--- stages/scanning/openscap/base_image_type.sh
+++ stages/scanning/openscap/base_image_type.sh
@@ -4,9 +4,9 @@ echo "${DOCKER_AUTH_CONFIG_STAGING}" | base64 -d >staging_auth.json
 podman pull --authfile staging_auth.json "${REGISTRY_URL_STAGING}/${IMAGE_NAME}@${IMAGE_PODMAN_SHA}"
 DOCKER_IMAGE_PATH=$(podman images --noheading | awk '{print $3}')
 export DOCKER_IMAGE_PATH
-BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "com.redhat.component"}}' "${DOCKER_IMAGE_PATH}")
+BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "mil.dso.ironbank.os-type"}}' "${DOCKER_IMAGE_PATH}")
 if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
-  BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "mil.dso.ironbank.os-type"}}' "${DOCKER_IMAGE_PATH}")
+  BASE_IMAGE_TYPE=$(podman inspect -f '{{index .Labels "com.redhat.component"}}' "${DOCKER_IMAGE_PATH}")
   if [[ "${BASE_IMAGE_TYPE}" == "" ]]; then
     labels=$(podman inspect -f '{{index .Labels}}' "${DOCKER_IMAGE_PATH}")
     echo "Unknown image type. Can't choose security guide. labels: ${labels}"
