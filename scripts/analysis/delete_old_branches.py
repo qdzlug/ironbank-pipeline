4e92957beb5d25eff49d1126387309c08cc6a2e6
David Freeman <david.freeman.26.ctr@us.af.mil>
2022-09-06 08:45:02 -0600

    print additional info about branches

--- scripts/analysis/delete_old_branches.py
+++ scripts/analysis/delete_old_branches.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-import os
+"""
+This script will look at all branches for a project and if the mode is delete, remove all branches that haven't been updated in 6 months.
 
-# import json
+"""
+
+import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import gitlab
@@ -13,6 +16,9 @@ def evaluate_branches(branches, mode="dry_run") -> None:
     Loop through GL project's list of branches to delete any that haven't been updated in 6 months
     """
     six_months_ago = (datetime.now() - relativedelta(months=6)).timestamp()
+    stale_branches = []
+    branch_total = len(branches)
+    print(f"Total project branches: {branch_total}")
     for branch in branches:
         branch_name = branch.name
         committed_date = branch.commit["committed_date"]
@@ -20,16 +26,27 @@ def evaluate_branches(branches, mode="dry_run") -> None:
             committed_date, "%Y-%m-%dT%H:%M:%S.%f%z"
         ).timestamp()
         # if the last update date is more than six months ago, and not in tuple of preserved branches, delete the branch.
+        last_updated = datetime.strftime(
+            datetime.utcfromtimestamp(branch_update_age), "%Y-%m-%dT%H:%M:%SZ"
+        )
         if six_months_ago > branch_update_age and branch_name not in ("master"):
             print(
-                f"Stale branch:\t{branch_name}\n"
-                f"Last updated:\t{datetime.strftime(datetime.utcfromtimestamp(branch_update_age), '%Y-%m-%dT%H:%M:%SZ')}"
+                f"""Stale branch:\t{branch_name}\n"""
+                f"""Last updated:\t{last_updated}\n"""
             )
-            if mode == "dry_run":
+            stale_branches.append(branch.name)
+            if mode == "delete":
                 print("Deleting branch")
                 branch.delete()
         else:
-            print(f"Retaining branch: {branch_name}")
+            print(
+                f"""Retaining branch: {branch_name}\n"""
+                f"""Last updated:\t{last_updated}\n"""
+            )
+    print(
+        f"""Total stale branches:\t\t{len(stale_branches)}\n"""
+        f"""Total retained branches:\t{branch_total - len(stale_branches)}"""
+    )
 
 
 def main() -> None:
