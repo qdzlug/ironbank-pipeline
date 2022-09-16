bf6aeaf76f06b14f39722a024bf0b781db33e8f5
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-09-15 18:12:04 -0600

    genericize loading resources from import artifacts

--- stages/build/build.py
+++ stages/build/build.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 import subprocess
 from pathlib import Path
@@ -10,11 +11,22 @@ from ironbank.pipeline.image import Image, ImageFile
 from ironbank.pipeline.utils import logger
 log = logger.setup("build")
 
-
-def load_images(image_path: Path):
-    for file_name in os.listdir(image_path):
-        pass
-
+# resource_type set to file by default so image is explicitly set
+# resource_type is not used unless value = image, but it is set to file for clarity of purpose
+# TODO: consider passing a true "type" for resource_type (i.e. resource_type = Image or resource_type = Path)
+def load_resources(resource_dir: str, resource_type: str = 'file', skopeo: Skopeo = None):
+    for resource_file in os.listdir(resource_dir):
+        resource_file_obj = Path(resource_dir, resource_file)
+        if resource_file_obj.isfile() and not resource_file_obj.is_symlink():
+            if resource_type == "image" and skopeo:
+                manifest_json = subprocess.run(["tar", "-xf", resource_file_obj.as_posix(), "-O", "manifest.json"])
+                image_name = manifest_json[0]['RepoTags']
+                skopeo.copy(ImageFile(file_path=resource_file_obj, transport="docker-archive:"), Image(url=image_name, transport="containers-storage:"))
+            else:
+                os.rename(resource_file_obj, Path(resource_file))
+        else:
+            log.error("Resource type is invalid")
+            sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="Script used for building ironbank images")
@@ -49,12 +61,11 @@ def main():
         pull_creds = os.environ["DOCKER_AUTH_CONFIG_STAGING"]
 
     # gather files and subpaths
-    for image_file in os.listdir(image_dir):
-        image_file_obj = Path(image_file)
-        if image_file_obj.isfile() and not image_file_obj.is_symlink():
-            manifest_json = subprocess.run(["tar", "-xf", image_file_obj.as_posix(), "-O", "manifest.json"])
-            image_name = manifest_json[0]['RepoTags']
-            skopeo.copy(ImageFile(file_path=image_file_obj, transport="docker-archive:"), Image(url=image_name, transport="containers-storage:"))
+    load_resources(resource_dir=image_dir, resource_type="image", skopeo=skopeo)
+    load_resources(resource_dir=resource_dir)
+
+
+
 
     log.info("Load any images used in Dockerfile build")
 
