a56d40dcbb9afad2a83e1c51fb8f1abac6fefcab
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-10-05 13:19:40 -0600

    add local testing vars

---
+++ build_vars.sh
@@ -0,0 +1,17 @@
+export DISTRO_REPO_DIR='ubi-repos'
+export DISTRO_REPO_MOUNT='/etc/yum.repos.d'
+export ARTIFACT_STORAGE='.'
+export ARTIFACT_DIR="$ARTIFACT_STORAGE/build_artifacts"
+
+export CI_PIPELINE_ID='12345'
+
+export REGISTRY_URL_STAGING='no.nah.example.fake/ironbank-staging'
+export BASE_REGISTRY='no.nah.example.fake/ironbank'
+export STAGING_BASE_IMAGE=''
+export DOCKER_AUTH_CONFIG_PULL='e30='
+export DOCKER_AUTH_CONFIG_STAGING='e30='
+export PIPELINE_REPO_DIR='../ib-pipeline'
+export CI_PROJECT_URL='test.test.test'
+export CI_COMMIT_SHA='1d0dd9790b4ef8ed1a871fd634071a15770f83a7228029e19b3b056ee831666c'
+
+
