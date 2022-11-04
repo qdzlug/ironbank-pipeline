e6dbd550668130e577de71df559c4055ee140365
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-11-04 16:10:36 -0400

    add comments and replace commented out tests

--- ironbank/pipeline/container_tools/container_tool.py
+++ ironbank/pipeline/container_tools/container_tool.py
@@ -14,10 +14,14 @@ class ContainerTool(ABC):
 
     @classmethod
     # get sub lists of [flag, k=v] and flatten list
+    # helpful for subprocess commands where you need to need to pass multiple environment variable args with the same flag
+    # e.g. ['--build-arg', 'abc=123', '--build-arg', 'def=345']
     def _generate_arg_list_from_env(cls, flag: str, env_vars: dict) -> list[str]:
         return cls._flatten([[flag, f"{k}={v}"] for k, v in env_vars.items()])
 
     @classmethod
     # get sub lists of [flag, val] and flatten list
+    # helpful for subprocess commands where you need to need to pass multiple args with the same flag
+    # e.g. ['--build-arg', 'abc', '--build-arg', 'def']
     def _generate_arg_list_from_list(cls, flag: str, arg_list: list[str]):
         return cls._flatten([[flag, val] for val in arg_list])
--- ironbank/pipeline/test/test_skopeo.py
+++ ironbank/pipeline/test/test_skopeo.py
@@ -1,85 +1,25 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-# from dataclasses import dataclass
-# import json
-# import pathlib
-# import subprocess
-# import sys
-# import os
-# import logging
-# from unittest.mock import patch
-# from unittest.mock import mock_open
-# import pytest
-# import asyncio
-# from ironbank.pipeline.test.mocks.mock_classes import MockProject, MockHardeningManifest
-# from ironbank.pipeline.utils.testing import raise_
+import pytest
+from ironbank.pipeline.test.mocks.mock_classes import MockImage
+from ironbank.pipeline.utils import logger
+from ironbank.pipeline.container_tools.skopeo import Skopeo
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# import base_image_validation  # noqa E402
-
-# logging.basicConfig(level="INFO", format="%(levelname)s: %(message)s")
+log = logger.setup("test skopeo")
 
 
-# @pytest.fixture
-# def good_base_image():
-#     return ["redhat/ubi/ubi8", "8.5"]
+@pytest.mark.only
+def test_skopeo_init():
+    log.info("Test init container with params results in expected values")
+    skopeo = Skopeo(authfile="authfile.json", docker_config_dir="docker_config.conf")
+    assert skopeo.authfile == "authfile.json"
+    assert skopeo.docker_config_dir == "docker_config.conf"
+
+    log.info("Test init container without params results in None as default")
+    skopeo = Skopeo()
+    skopeo.authfile = None
+    skopeo.docker_config_dir = None
 
 
-# @pytest.fixture
-# def bad_base_image():
-#     return ["redhat/ubi/ubi8", "8.100"]
-
-# @dataclass
-# class MockSubprocessReturn:
-#     stdout: str = "standard out"
-
-
-# def mock_subprocess_run(*args, **kwargs):
-#     return MockSubprocessReturn("data")
-
-
-# def mock_subprocess_fail(*args, **kwargs):
-#     raise subprocess.CalledProcessError(1, ["cmd"])
-
-# # TODO: Update for new Skopeo class - Taken from test_base_image_validation
-
-# def test_skopeo_inspect_base_image(monkeypatch, caplog, good_base_image):
-#     monkeypatch.setenv("STAGING_BASE_IMAGE", "base")
-#     monkeypatch.setenv(
-#         "DOCKER_AUTH_CONFIG_STAGING", "c3RhZ2luZy10ZXN0Cg=="
-#     )  # staging-test -> base64 encoded value
-#     monkeypatch.setattr(pathlib.Path, "write_text", lambda self, x: True)
-#     monkeypatch.setattr(pathlib.Path, "open", mock_open(read_data="data"))
-#     monkeypatch.setattr(subprocess, "run", mock_subprocess_run)
-#     monkeypatch.setenv("ARTIFACT_DIR", "right/here")
-#     monkeypatch.setattr(json, "dump", lambda x, y: True)
-
-#     skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
-#     assert "staging_pull_auth.json" in caplog.text
-#     caplog.clear()
-
-#     monkeypatch.delenv("STAGING_BASE_IMAGE")
-#     monkeypatch.setenv("DOCKER_AUTH_CONFIG_PULL", "c3RhZ2luZy10ZXN0Cg==")
-
-#     skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
-#     assert "prod_pull_auth.json" in caplog.text
-#     caplog.clear()
-
-#     monkeypatch.setattr(subprocess, "run", mock_subprocess_fail)
-
-#     with pytest.raises(SystemExit):
-#         skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
-
-#     assert (
-#         "Failed to inspect BASE_IMAGE:BASE_TAG provided in hardening_manifest."
-#         in caplog.text
-#     )
-#     caplog.clear()
-
-#     monkeypatch.setattr(subprocess, "run", lambda self: raise_(Exception))
-
-#     with pytest.raises(SystemExit):
-#         skopeo_inspect_base_image(good_base_image[0], good_base_image[1])
-
-#     assert "Unknown failure when attemping to inspect BASE_IMAGE" in caplog.text
-#     caplog.clear()
+def test_inspect():
+    image = MockImage(registry="example.com", name="example/test", tag="1.0")
