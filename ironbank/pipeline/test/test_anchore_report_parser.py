0d43fe1d514803ad9be47c80f53e567ea75a1e6c
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-11-28 09:35:03 -0500

    add tests for additional parsing methods

--- ironbank/pipeline/test/test_anchore_report_parser.py
+++ ironbank/pipeline/test/test_anchore_report_parser.py
@@ -9,7 +9,7 @@ log = logger.setup(name="anchore_report_parser")
 
 
 @pytest.fixture
-def mock_anchore_vuln_args():
+def mock_vuln_data():
     return {
         "tag": "registry.example/mock/test:1.0",
         "vuln": "CVE-123-ABC",
@@ -22,39 +22,86 @@ def mock_anchore_vuln_args():
         "package_version": "1.0",
         "fix": "example_fix",
         "url": "https://pypi.example/mock_package",
-        "extra": {"description": "new_description", "example": "data"},
+        "extra": {
+            "description": "new_description",
+            "example": "data",
+            "nvd_data": [{"cvss_v2": {"vector_string": "mock_nvd_vector"}}],
+            "vendor_data": [{"cvss_v2": {"vector_string": "mock_vendor_vector"}}],
+        },
     }
 
 
+class MockAnchoreVuln(AnchoreVuln):
+    def sort_fix(self):
+        self.fix = "sorted_fix"
+
+    def get_nvd_scores(self, ver):
+        setattr(self, f"nvd_cvss_{ver}_vector", "mock_nvd_score")
+
+    def get_vendor_nvd_scores(self, ver):
+        setattr(self, f"vendor_cvss_{ver}_vector", "mock_vendor_score")
+
+    def get_identifiers(self):
+        setattr(self, "identifiers", [*self.identifiers, "CVE-456-DEF"])
+
+
+@pytest.fixture
+def mock_anchore_vuln(mock_vuln_data):
+    return MockAnchoreVuln(**mock_vuln_data)
+
+
 @pytest.mark.only
-def test_anchore_vuln_post_init(monkeypatch, mock_anchore_vuln_args):
+def test_anchore_vuln_post_init(mock_anchore_vuln):
     log.info("Validate post init")
-    mock_extra_vuln = "CVE-456-DEF"
-    monkeypatch.setattr(
-        AnchoreVuln, "sort_fix", lambda self: setattr(self, "fix", "sorted_fix")
-    )
-    monkeypatch.setattr(
-        AnchoreVuln,
-        "get_nvd_scores",
-        lambda self, ver: setattr(self, f"nvd_cvss_{ver}_vector", "mock_nvd_score"),
-    )
-    monkeypatch.setattr(
-        AnchoreVuln,
-        "get_vendor_nvd_scores",
-        lambda self, ver: setattr(
-            self, f"vendor_cvss_{ver}_vector", "mock_vendor_score"
-        ),
-    )
-    monkeypatch.setattr(
-        AnchoreVuln,
-        "get_identifiers",
-        lambda self: setattr(self, "identifiers", [*self.identifiers, mock_extra_vuln]),
-    )
-    mock_anchore_vuln = AnchoreVuln(**mock_anchore_vuln_args)
     assert mock_anchore_vuln.fix == "sorted_fix"
     assert mock_anchore_vuln.description == "new_description"
-    assert mock_anchore_vuln.identifiers == [mock_anchore_vuln.vuln, mock_extra_vuln]
+    assert mock_anchore_vuln.identifiers == [mock_anchore_vuln.vuln, "CVE-456-DEF"]
     assert mock_anchore_vuln.nvd_cvss_v2_vector == "mock_nvd_score"
     assert mock_anchore_vuln.nvd_cvss_v3_vector == "mock_nvd_score"
     assert mock_anchore_vuln.vendor_cvss_v2_vector == "mock_vendor_score"
     assert mock_anchore_vuln.vendor_cvss_v3_vector == "mock_vendor_score"
+
+
+@pytest.mark.only
+def test_anchore_vuln_properties(mock_anchore_vuln):
+    log.info("Validate properties")
+    assert mock_anchore_vuln.inherited == mock_anchore_vuln.inherited_from_base
+    assert mock_anchore_vuln.finding == mock_anchore_vuln.vuln
+    assert mock_anchore_vuln.cve == mock_anchore_vuln.vuln
+    assert mock_anchore_vuln.packagePath == mock_anchore_vuln.package_path
+    assert mock_anchore_vuln.scanSource == mock_anchore_vuln.scan_source
+    assert mock_anchore_vuln.link == mock_anchore_vuln.url
+
+
+@pytest.mark.only
+def test_from_dict(mock_anchore_vuln, mock_vuln_data):
+    log.info("Test initializing class from dictionary with additional keys")
+    mock_vuln_data_extra_vals = {
+        **mock_vuln_data,
+        "mock": "data",
+        "additional": "value",
+    }
+    assert mock_anchore_vuln.from_dict(mock_vuln_data_extra_vals) == mock_anchore_vuln
+
+
+@pytest.mark.only
+def test_get_nvd_score(monkeypatch, mock_vuln_data):
+    log.info("Test nvd score is set correctly")
+    # undo mock
+    monkeypatch.setattr(MockAnchoreVuln, "get_nvd_scores", AnchoreVuln.get_nvd_scores)
+    mock_anchore_vuln_nvd = MockAnchoreVuln(**mock_vuln_data)
+    mock_anchore_vuln_nvd.get_nvd_scores("v2")
+    assert mock_anchore_vuln_nvd.nvd_cvss_v2_vector == "mock_nvd_vector"
+    assert mock_anchore_vuln_nvd.nvd_cvss_v3_vector == None
+
+
+@pytest.mark.only
+def test_get_vendor_score(monkeypatch, mock_vuln_data):
+    log.info("Test vendor score is set correctly")
+    monkeypatch.setattr(
+        MockAnchoreVuln, "get_vendor_nvd_scores", AnchoreVuln.get_vendor_nvd_scores
+    )
+    mock_anchore_vuln_vendor = MockAnchoreVuln(**mock_vuln_data)
+    mock_anchore_vuln_vendor.get_vendor_nvd_scores("v2")
+    assert mock_anchore_vuln_vendor.vendor_cvss_v2_vector == "mock_vendor_vector"
+    assert mock_anchore_vuln_vendor.vendor_cvss_v3_vector == None
