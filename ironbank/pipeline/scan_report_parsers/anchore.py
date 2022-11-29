a8d30dec4bea2fd476303257729d03b4fbb0c077
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2022-11-29 13:53:52 -0500

    add notes for unclear variable assignment intentions

--- ironbank/pipeline/scan_report_parsers/anchore.py
+++ ironbank/pipeline/scan_report_parsers/anchore.py
@@ -46,11 +46,14 @@ class AnchoreVuln(AbstractVuln):
         # allow for multiple names for vuln, allows vat/csv_gen to use different names and parse __dict__ for an AnchoreVuln object
         self.sort_fix()
         self.identifiers.append(self.vuln)
-        self.description = self.extra.get("description") or self.description
+        # intentionally throw key error if description doesn't exist
+        # or evaluates on extra["description"] is some falsey value
+        self.description = self.extra["description"] or self.description
         for ver in self._nvd_versions:
             self.get_nvd_scores(ver)
             self.get_vendor_nvd_scores(ver)
         self.get_identifiers()
+        # no_data is set in the init by default, but if the value for inherited_from_base is "" or some other falsey value, update it to no_data
         self.inherited_from_base = self.inherited_from_base or "no_data"
 
     # add alias from inherited -> inherited_from_base
