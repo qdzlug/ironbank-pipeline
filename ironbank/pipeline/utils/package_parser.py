83f70bdde2b265706aadd8c57a367a5dc1abcac5
David Freeman <david.freeman.26.ctr@us.af.mil>
2022-12-08 19:26:04 -0700

    take kenn's suggestion

--- ironbank/pipeline/utils/package_parser.py
+++ ironbank/pipeline/utils/package_parser.py
@@ -69,7 +69,7 @@ class ApkPackage(ParsedURLPackage):
             return None
 
         match = re.match(
-            r"(?:^|.+/)(?P<name>[^/]+)-(?P<version>[^/]*)-[^/]+.apk",
+            r"(?:^|.+/)(?P<name>[^/]+)-(?P<version>[^/]*)[^/]+.apk",
             url,
         )
 
