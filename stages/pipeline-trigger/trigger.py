08b98d7473cddafacf3ca976fd54137f81cccd34
David Freeman <david.freeman.26.ctr@us.af.mil>
2022-12-16 17:22:27 -0700

    use HM method to parse labels

--- stages/pipeline-trigger/trigger.py
+++ stages/pipeline-trigger/trigger.py
@@ -2,7 +2,12 @@
 
 import os
 from ironbank.pipeline.utils import logger
+from ironbank.pipeline.hardening_manifest import (
+    get_source_keys_values,
+)
 
 log = logger.setup("pipeline_trigger")
 
-log.info(os.environ["mil.dso.ironbank.os-type"])
+artifact_storage = os.environ["ARTIFACT_STORAGE"]
+label_dict = get_source_keys_values(f"{artifact_storage}/lint/labels.env")
+log.info("OS_TYPE: %s", label_dict["mil.dso.ironbank.os-type"])
