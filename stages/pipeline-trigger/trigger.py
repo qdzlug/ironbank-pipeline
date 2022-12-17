3a545ca1363310013a39c35a5c51902be53ffa55
David Freeman <david.freeman.26.ctr@us.af.mil>
2022-12-16 18:08:33 -0700

    add skopeo inspect of base image

--- stages/pipeline-trigger/trigger.py
+++ stages/pipeline-trigger/trigger.py
@@ -1,13 +1,57 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+import tempfile
+from base64 import b64decode
+from pathlib import Path
 from ironbank.pipeline.utils import logger
+from ironbank.pipeline.image import Image
+from ironbank.pipeline.container_tools.skopeo import Skopeo
 from ironbank.pipeline.hardening_manifest import (
     get_source_keys_values,
 )
+from ironbank.pipeline.project import DsopProject
+from ironbank.pipeline.hardening_manifest import HardeningManifest
+from ironbank.pipeline.utils.exceptions import GenericSubprocessError
 
 log = logger.setup("pipeline_trigger")
 
 artifact_storage = os.environ["ARTIFACT_STORAGE"]
 label_dict = get_source_keys_values(f"{artifact_storage}/lint/labels.env")
-log.info("OS_TYPE: %s", label_dict["mil.dso.ironbank.os-type"])
+os_type = label_dict.get("mil.dso.ironbank.os-type")
+if not os_type:
+    dsop_project = DsopProject()
+    manifest = HardeningManifest(dsop_project.hardening_manifest_path)
+    with tempfile.TemporaryDirectory(prefix="DOCKER_CONFIG-") as docker_config_dir:
+
+        docker_config = Path(docker_config_dir, "config.json")
+        if os.environ.get("STAGING_BASE_IMAGE"):
+            # Grab staging pull docker auth
+            pull_auth = b64decode(os.environ["DOCKER_AUTH_CONFIG_STAGING"]).decode(
+                "UTF-8"
+            )
+            registry = os.environ["REGISTRY_URL_STAGING"]
+        else:
+            # Grab prod docker auth
+            pull_auth = b64decode(os.environ["DOCKER_AUTH_CONFIG_PULL"]).decode("UTF-8")
+            registry = os.environ["REGISTRY_URL_PROD"]
+        docker_config.write_text(pull_auth, encoding="utf-8")
+        try:
+            skopeo = Skopeo(docker_config_dir=docker_config_dir)
+            base_image = Image(
+                registry=registry,
+                name=manifest.base_image_name,
+                tag=manifest.base_image_tag,
+                transport="docker://",
+            )
+            base_img_inspect = skopeo.inspect(base_image, log_cmd=True)
+        except GenericSubprocessError:
+            log.error(
+                "Failed to inspect IMAGE:TAG provided in hardening_manifest. \
+                    Please validate this image exists in the registry1.dso.mil/ironbank project."
+            )
+            log.error("Failed 'skopeo inspect' of image: %s", base_image)
+            sys.exit(1)
+    print(base_img_inspect)
+# log.info("OS_TYPE: %s", os_type)
