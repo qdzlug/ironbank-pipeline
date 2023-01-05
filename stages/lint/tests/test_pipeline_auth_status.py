1d622806104cbe496d811780d3a92d289b481c36
massey.robert <massey.robert@solute.us>
2023-01-05 09:57:05 -0800

    Add mock vat api and responses to auth status

--- stages/lint/tests/test_pipeline_auth_status.py
+++ stages/lint/tests/test_pipeline_auth_status.py
@@ -4,12 +4,14 @@ import os
 import sys
 import asyncio
 import pytest
+import requests
 import pathlib
 from unittest.mock import patch
 from ironbank.pipeline.utils import logger
 from ironbank.pipeline.test.mocks.mock_classes import (
     MockHardeningManifest,
 )
+from ironbank.pipeline.apis import VatAPI
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pipeline_auth_status  # noqa E402
@@ -21,9 +23,13 @@ mock_path = pathlib.Path(
     "ironbank/pipeline/test/mocks",
 )
 
+@pytest.fixture
+def mock_vat_api():
+    return VatAPI(url="http://vat-local.example")
+
 
 @patch("pipeline_auth_status.HardeningManifest", new=MockHardeningManifest)
-def test_pipeline_auth_status_main(monkeypatch, caplog):
+def test_pipeline_auth_status_main(monkeypatch, mock_vat_api, mock_responses, caplog):
 
     log.info("Test no backend server address")
     monkeypatch.setenv("VAT_BACKEND_SERVER_ADDRESS", "")
@@ -34,9 +40,9 @@ def test_pipeline_auth_status_main(monkeypatch, caplog):
     assert "Failing Pipeline" in caplog.text
 
     log.info("Test having backend server address")
+    monkeypatch.setattr(requests, "get", mock_responses["200"])
+    mock_vat_api.get_image("example/example/example", "1.0")
     monkeypatch.setenv("VAT_BACKEND_SERVER_ADDRESS", "http://vat-local.example")
     monkeypatch.setenv("CI_JOB_JWT_V2", "http://vat-local.abcdefg")
-    with pytest.raises(SystemExit) as se:
-        asyncio.run(pipeline_auth_status.main())
-    assert se.value.code == 1
-    assert "Failing Pipeline" in caplog.text
+    asyncio.run(pipeline_auth_status.main())
+    assert "Retrieve Auth Status from VAT" in caplog.text
