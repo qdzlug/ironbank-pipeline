9449ddc1292866ee0ce68de53e0345bfc090575c
Tim Seagren <seagren.tim@solute.us>
2023-01-16 15:09:26 -0800

    black

--- ironbank/pipeline/utils/predicates.py
+++ ironbank/pipeline/utils/predicates.py
@@ -4,16 +4,16 @@ from dataclasses import dataclass
 @dataclass(frozen=True, slots=True)
 class Predicates:
     types: dict = {
-            "sbom-cyclonedx-json.json": "cyclonedx",
-            "sbom-spdx.xml": "spdx",
-            "sbom-spdx-json.json": "spdxjson",
-            "sbom-syft-json.json": "https://github.com/anchore/syft#output-formats",
-            "vat_response.json": "https://vat.dso.mil/api/p1/predicate/beta1",
-            "hardening_manifest.json": "https://repo1.dso.mil/dsop/dccscr/-/raw/master/hardening%20manifest/README.md",
+        "sbom-cyclonedx-json.json": "cyclonedx",
+        "sbom-spdx.xml": "spdx",
+        "sbom-spdx-json.json": "spdxjson",
+        "sbom-syft-json.json": "https://github.com/anchore/syft#output-formats",
+        "vat_response.json": "https://vat.dso.mil/api/p1/predicate/beta1",
+        "hardening_manifest.json": "https://repo1.dso.mil/dsop/dccscr/-/raw/master/hardening%20manifest/README.md",
     }
     unattached_predicates: list[str] = [
-            "sbom-spdx-tag-value.txt",
-            "sbom-cyclonedx.xml",
+        "sbom-spdx-tag-value.txt",
+        "sbom-cyclonedx.xml",
     ]
 
     # Defines a map of SBOM output formats provided by syft to their corresponding mediatypes
