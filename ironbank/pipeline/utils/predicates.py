af532599deb7ea2e3515454c4d3c45bf44b7dded
Tim Seagren <seagren.tim@solute.us>
2023-01-19 12:37:22 -0800

    black

--- ironbank/pipeline/utils/predicates.py
+++ ironbank/pipeline/utils/predicates.py
@@ -4,16 +4,15 @@ from dataclasses import dataclass, field
 @dataclass(frozen=True, slots=True)
 class Predicates:
     types: dict = field(
-        default_factory=lambda:
-            {
-                "sbom-cyclonedx-json.json": "cyclonedx",
-                "sbom-spdx.xml": "spdx",
-                "sbom-spdx-json.json": "spdxjson",
-                "sbom-syft-json.json": "https://github.com/anchore/syft#output-formats",
-                "vat_response.json": "https://vat.dso.mil/api/p1/predicate/beta1",
-                "hardening_manifest.json": "https://repo1.dso.mil/dsop/dccscr/-/raw/master/hardening%20manifest/README.md",
-            }
-        )
+        default_factory=lambda: {
+            "sbom-cyclonedx-json.json": "cyclonedx",
+            "sbom-spdx.xml": "spdx",
+            "sbom-spdx-json.json": "spdxjson",
+            "sbom-syft-json.json": "https://github.com/anchore/syft#output-formats",
+            "vat_response.json": "https://vat.dso.mil/api/p1/predicate/beta1",
+            "hardening_manifest.json": "https://repo1.dso.mil/dsop/dccscr/-/raw/master/hardening%20manifest/README.md",
+        }
+    )
     unattached_predicates: list[str] = field(
         default_factory=lambda: [
             "sbom-spdx-tag-value.txt",
