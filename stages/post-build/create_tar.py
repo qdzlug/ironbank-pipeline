b8964cd106e99764894dcf021fc4eed11cc35950
Cody Miller <codymiller@seed-innovations.com>
2023-01-23 16:44:06 -0700

    Fix file name for create tar

--- stages/post-build/create_tar.py
+++ stages/post-build/create_tar.py
@@ -7,17 +7,17 @@ from ironbank.pipeline.container_tools.skopeo import Skopeo
 
 def main():
     src = Image(
-        url=os.environ.get("REGISTRY_URL_STAGING"),
-        name=os.environ.get("IMAGE_NAME"),
-        digest=os.environ.get("IMAGE_PODMAN_SHA"),
+        url=os.environ["REGISTRY_URL_STAGING"],
+        name=os.environ["IMAGE_NAME"],
+        digest=os.environ["IMAGE_PODMAN_SHA"],
         transport="docker://",
     )
     dest = ImageFile(
-        file_path="{}/{}.tar".format(os.environ.get("IMAGE_FILE"), ".tar"),
+        file_path=f"{os.environ['IMAGE_FILE']}.tar",
         transport="docker-archive:",
     )
 
-    skopeo = Skopeo(authfile=os.environ.get("DOCKER_AUTH_CONFIG_FILE_STAGING"))
+    skopeo = Skopeo(authfile=os.environ["DOCKER_AUTH_CONFIG_FILE_STAGING"])
     skopeo.copy(
         src=src,
         dest=dest,
