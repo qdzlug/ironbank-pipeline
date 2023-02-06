51e5b69512ea3353a8be8892ddeca8d8284a9764
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2023-02-06 17:12:15 -0500

    fix project permissions and additional typing issues

--- kickoff_staging_pipeline/kickoff.py
+++ kickoff_staging_pipeline/kickoff.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 from typing import Any
+import typing
 import yaml
 from pathlib import Path
 from git import Repo, Remote, IndexFile
@@ -28,6 +29,8 @@ class Project:
     remote: Remote | Any = None
     gl_project: GLProject | Any = None
     pipeline: GLPipeline | Any = None
+    changes_pushed: bool = False
+    base_image: bool = True
 
 
 @dataclass
@@ -54,8 +57,18 @@ class Config:
     dest_un: str = field(default_factory=lambda: os.environ["DEST_UN"])
     dest_pw: str = field(default_factory=lambda: os.environ["DEST_PW"])
 
+    # TODO: remove this decorator when typing is resolved for self.projects
+    @typing.no_type_check
     def __post_init__(self) -> None:
-        self.projects = [Project(**project) for project in self.projects]  # type: ignore
+        self.projects = [
+            Project(
+                **{
+                    **project,
+                    "branch": project.get("branch") or self.default_project_branch,
+                }
+            )
+            for project in self.projects
+        ]
         self.clone_dir = Path(self.clone_dir)
         self.templates = Path(self.templates)
         self.src_auth_exists = bool(self.src_un and self.src_pw)
@@ -71,8 +84,8 @@ class Config:
             else "https://"
         )
         # configure auth for git pulls from src and pushes to dest
-        self.src_git_url = f"{self.src_url_prefix}{self.src_gitlab_url}"
-        self.dest_git_url = f"{self.dest_url_prefix}{self.dest_gitlab_url}"
+        self.src_git_url: str = f"{self.src_url_prefix}{self.src_gitlab_url}"
+        self.dest_git_url: str = f"{self.dest_url_prefix}{self.dest_gitlab_url}"
         self.src_gitlab_url = f"https://{self.src_gitlab_url}"
         self.dest_gitlab_url = f"https://{self.dest_gitlab_url}"
 
@@ -151,58 +164,121 @@ def create_tester_group_in_dest(gl: gitlab.Gitlab, config: Config) -> GLGroup:
     return group
 
 
+def update_force_push_rules(project: GLProject, branch: str) -> None:
+    project.protectedbranches.delete(branch)
+    maintainer = gitlab.const.MAINTAINER_ACCESS
+    project.protectedbranches.create(
+        {
+            "name": "master",
+            "merge_access_level": maintainer,
+            "push_access_level": maintainer,
+            "allow_force_push": True,
+        }
+    )
+
+
+def generate_remote(project: Project, repo: Repo, config: Config):
+    return (
+        repo.create_remote(
+            "staging",
+            f"{config.dest_git_url}/{config.group}/{config.tester}/{project.dest_project_name}",
+        )
+        if "staging" not in [remote.name for remote in repo.remotes]
+        else repo.remotes.staging
+    )
+
+
+def push_branches(project: Project, repo: Repo, remote: Remote) -> None:
+    branches = [project.branch]
+    branches += (
+        ["development"]
+        if "development" not in [ref.name.split("staging/") for ref in remote.refs]
+        else []
+    )
+    for branch in branches:
+        print(branch)
+        repo.git.checkout(branch)
+        remote.push(force=True).raise_if_error()
+
+
 def push_repos_to_dest(config: Config) -> Config:
     template_ci_file_path = template_ci_file(config)
     for project, i in zip(config.projects, range(len(config.projects))):
         repo: Repo = project.repo
         assert repo.working_dir
-        repo.git.checkout(project.branch or config.default_project_branch)
+        # check out test branch before proceeding (prevent making changes to last branch used)
+        repo.git.checkout(project.branch)
         repo_path: Path = Path(repo.working_dir)
         repo_ci_file: Path = Path(repo_path, config.ci_file)
+        # pull any changes from origin
+        repo.remotes.origin.pull()
+        # delete existing file and copy again
+        # creates no diff if no changes exist
         repo_ci_file.unlink(missing_ok=True)
         shutil.copy2(template_ci_file_path, repo_ci_file)
+        # add change to ci file and commit
         index: IndexFile = repo.index
-        index.add(config.ci_file)
-        index.commit("updating .gitlab-ci.yml")
-        remote = (
-            repo.create_remote(
-                "staging",
-                f"{config.dest_git_url}/{config.group}/{config.tester}/{project.dest_project_name}",
+        # if unstaged changes or untracked files
+        if index.diff(None) or repo.untracked_files:
+            # only add ci file, prevent adding unexpected files
+            index.add([config.ci_file])
+            index.commit("updating .gitlab-ci.yml")
+            remote = generate_remote(project, repo, config)
+            config.projects[i].remote = remote
+            remote_urls = [url for url in remote.urls]
+            # double check repo1 not in dest before push
+            print(remote_urls[0].split("@")[-1])
+            assert len(remote_urls) == 1 and "repo1" not in remote_urls[0]
+            push_branches(project, repo, remote)
+            config.projects[i].changes_pushed = True
+        else:
+            print(
+                f"Nothing to push to staging for {project.dest_project_name}. Skipping"
             )
-            if "staging" not in [remote.name for remote in repo.remotes]
-            else repo.remotes.staging
-        )
-        config.projects[i].remote = remote
-        remote_urls = [url for url in remote.urls]
-        # double check repo1 not in dest before push
-        print(remote_urls)
-        assert len(remote_urls) == 1 and "repo1" not in remote_urls[0]
-        for branch in [
-            project.branch or config.default_project_branch,
-            "development",
-        ]:
-            repo.git.checkout(branch)
-            repo.git.pull()
-            remote.push(force=True)
     return config
 
 
 def update_dest_project_permissions(gl: gitlab.Gitlab, config: Config) -> Config:
     assert "repo1" not in gl.api_url
     for project, i in zip(config.projects, range(len(config.projects))):
-        config.projects[i].gl_project = gl.projects.get(
+        gl_project: GLProject = gl.projects.get(
             f"{config.group}/{config.tester}/{project.dest_project_name}"
         )
-        config.projects[i].gl_project.visibility = "public"
-        config.projects[i].gl_project.save()
+        assert gl_project
+        # TODO: add caching to skip these steps
+        print(f"Updating permissions for {project.dest_project_name}")
+        gl_project.visibility = "public"
+        gl_project.save()
+        update_force_push_rules(gl_project, project.branch)
+        if project.base_image:
+            variable_exists = "LABEL_ALLOWLIST_REGEX" in [
+                var.key for var in gl_project.variables.list()
+            ]
+            if not variable_exists:
+                gl_project.variables.create(
+                    {
+                        "key": "LABEL_ALLOWLIST_REGEX",
+                        "value": r"^mil\.dso\.ironbank\.os-type$",
+                    }
+                )
+
+        config.projects[i].gl_project = gl_project
     return config
 
 
 def kickoff_pipelines(config: Config) -> Config:
     for project, i in zip(config.projects, range(len(config.projects))):
-        config.projects[i].pipeline = project.gl_project.pipelines.create(
-            {"ref": project.branch or config.default_project_branch}
-        )
+        # prevent kicking off second pipeline if ci changes were pushed to remote
+        if not project.changes_pushed:
+            print(f"Kicking off pipeline for {project.dest_project_name}")
+            config.projects[i].pipeline = project.gl_project.pipelines.create(
+                {"ref": project.branch}
+            )
+        else:
+            # pipeline should've been created when changes were pushed
+            print(
+                f"Skipping pipeline creation for {project.dest_project_name}. Pipeline already created."  # noqa E501
+            )
     return config
 
 
@@ -210,8 +286,8 @@ def main():
 
     config_files = ["config.yaml", "secrets.yaml"]
     config_args = []
-    for cf in config_files:
-        with Path(cf).open("r", encoding="utf-8") as f:
+    for conf in config_files:
+        with Path(conf).open("r", encoding="utf-8") as f:
             config_args += [yaml.safe_load(f)]
 
     config = Config(**{k: v for sub_dict in config_args for k, v in sub_dict.items()})
@@ -226,6 +302,7 @@ def main():
 
     assert isinstance(config.clone_dir, Path)
 
+    print("\nCloning images...")
     config = clone_from_src(config)
 
     create_tester_group_in_dest(dest_gl, config)
@@ -237,8 +314,10 @@ def main():
             check=True,
         )
 
+    print(f"\nPushing repos to {config.dest}...")
     config = push_repos_to_dest(config)
 
+    print("\nUpdating destination project permissions")
     config = update_dest_project_permissions(dest_gl, config)
 
     # TODO: check git config before updating it
@@ -247,7 +326,8 @@ def main():
             ["git", "config", "--global", "--unset", "http.proxy"], check=True
         )
 
-    # config = kickoff_pipelines(config)
+    print("\nKicking off Pipelines...")
+    config = kickoff_pipelines(config)
 
 
 if __name__ == "__main__":
