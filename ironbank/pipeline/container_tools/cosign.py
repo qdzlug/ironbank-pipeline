710e81b068277efc224be4299024d782fcf446d4
Adam Martin <adam.martin@rancherfederal.com>
2023-02-09 17:21:43 -0500

    adjust error handing

--- ironbank/pipeline/container_tools/cosign.py
+++ ironbank/pipeline/container_tools/cosign.py
@@ -177,6 +177,7 @@ class Cosign(ContainerTool):
             raise subprocess.CalledProcessError(proc.returncode, cmd)
 
     @classmethod
+    @subprocess_error_handler("Cosign.verify failed")
     def verify(
         cls,
         image: Image,
@@ -192,15 +193,13 @@ class Cosign(ContainerTool):
         cmd += [f"{image.name}"]
         if log_cmd:
             cls.log.info(cmd)
-        try:
-            subprocess.run(
-                args=cmd,
-                capture_output=True,
-                check=True,
-                encoding="utf-8",
-            )
-            cls.log.info("%s Verified", image.name)
-            return True
-        except subprocess.CalledProcessError:
-            cls.log.info("%s Not Verified", image.name)
-            return False
+        
+        subprocess.run(
+            args=cmd,
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
+        )
+        cls.log.info("%s Verified", image.name)
+        return True
+      
