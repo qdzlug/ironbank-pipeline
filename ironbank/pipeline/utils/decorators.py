3291e0ad852407104f520bd72c9821179301cedc
Tim Seagren <seagren.tim@solute.us>
2023-02-14 09:10:03 -0800

    combine retries

--- ironbank/pipeline/utils/decorators.py
+++ ironbank/pipeline/utils/decorators.py
@@ -14,9 +14,9 @@ log: Logger = logger.setup(name="Exception")
 log = logger.setup("retry")
 
 
-def requests_retry(retry_count):
+def request_retry(retry_count):
     """
-    Decorator for retrying a function running a requests call
+    Decorator for retrying a function running a requests or subprocess call
     """
 
     def decorate(func):
@@ -31,24 +31,6 @@ def requests_retry(retry_count):
                         # prevent exception chaining by using from None
                         raise MaxRetriesException() from None
                     log.warning("Request failed, retrying...")
-
-        return wrapper
-
-    return decorate
-
-
-def request_retry(retry_count):
-    """
-    Decorator for retrying a function running a subprocess call
-    """
-
-    def decorate(func):
-        # args and kwargs are passed to allow this decorator to work on any method
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            for retry_num in range(1, retry_count + 1):
-                try:
-                    return func(*args, **kwargs)
                 except subprocess.CalledProcessError:
                     if retry_num >= retry_count:
                         # prevent exception chaining by using from None
