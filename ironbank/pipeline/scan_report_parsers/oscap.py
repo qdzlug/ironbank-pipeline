43ff760118208ccc0bb3be96dd8c7cafd09dadfd
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2023-02-22 19:39:34 -0500

    refactor xml parsing to separate class

--- ironbank/pipeline/scan_report_parsers/oscap.py
+++ ironbank/pipeline/scan_report_parsers/oscap.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
 import bz2
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass, field
 import os
 from pathlib import Path
 import re
-from typing import ClassVar
+from typing import Any, ClassVar, Generator
 
 import requests
 from ironbank.pipeline.scan_report_parsers.report_parser import (
@@ -21,56 +21,41 @@ from ironbank.pipeline.utils.exceptions import (
 from ironbank.pipeline.utils.flatten import flatten
 
 
-@dataclass(slots=True, frozen=True)
-class OscapFinding(AbstractFinding):
-    identifier: str
-    severity: str
-    description: str
-    rule_id: str = None
-    score: str = ""
-    link: str = None
-    package: str = None
-    package_path: str = None
-    title: str = None
-    scan_source: str = "oscap_comp"
-    identifiers: tuple = field(default_factory=lambda: ())
-    result: str = None
-    references: str = None
-    rationale: str = None
-    scanned_date: str = None
-    justification: str = None
+@dataclass
+class RuleInfo:
+    root: InitVar[str]
+    rule_result: InitVar[str]
+    rule_id: str = ""
+    identifiers: list[str] = None
+    identifier: str = ""
+    title: str = ""
+    severity: str = ""
+    time: str = ""
+    result: str = ""
+    references: str = ""
+    rationale: str = ""
+    description: str = ""
     namespaces: ClassVar[dict[str, str]] = {
         "xccdf": "http://checklists.nist.gov/xccdf/1.2",
         "dc": "http://purl.org/dc/elements/1.1/",
         "oval": "http://oval.mitre.org/XMLSchema/oval-definitions-5",
     }
-    _oval_rule: ClassVar[
-        str
-    ] = "xccdf_org.ssgproject.content_rule_security_patches_up_to_date"
+    oval_name: str = ""
+    oval_href: str = ""
 
-    @classmethod
-    def get_findings_from_rule_result(cls, root, rule_result) -> list[object]:
-        """
-        Gather findings from single rule_result
-        If rule_result iss OVAL, this list could include several findings, if rule_result is compliance only one finding will be returned in the list
-        """
-        finding_class = (
-            OscapOVALFinding
-            if rule_result.attrib["idref"] == cls._oval_rule
-            else OscapComplianceFinding
-        )
-        print(rule_result.attrib["idref"])
-        return finding_class.get_findings_from_rule_result(
-            root=root, rule_result=rule_result
-        )
-
-    @property
-    def desc(self):
-        return self.description
-
-    @property
-    def refs(self):
-        return self.references
+    def __post_init__(self, root, rule_result):
+        self.rule_id = rule_result.attrib["idref"]
+        rule = root.find(f".//xccdf:Rule[@id='{self.rule_id}']", self.namespaces)
+        self.set_identifiers(rule)
+        self.title = rule.find("xccdf:title", self.namespaces).text
+        self.severity = rule_result.attrib["severity"]
+        self.time = rule_result.attrib["time"]
+        self.set_result(rule_result)
+        self.set_references(rule)
+        self.set_rationale(rule)
+        self.set_description(rule)
+        if self.rule_id == OscapFinding.oval_rule:
+            self.set_oval_values(rule_result)
 
     @classmethod
     def _format_reference(cls, ref):
@@ -81,93 +66,180 @@ class OscapFinding(AbstractFinding):
             return f"{ref_title.text}: {ref_identifier.text}"
         return ref.text
 
+    def set_identifiers(self, rule) -> tuple:
+        self.identifiers = tuple(
+            ident.text for ident in rule.findall("xccdf:ident", self.namespaces)
+        ) or [self.rule_id]
+        assert len(self.identifiers) == 1
+        self.identifier = self.identifiers[0]
+
+    def set_result(self, rule_result):
+        """
+        Return result of rule check from compliance scan
+        """
+        self.result = rule_result.find("xccdf:result", self.namespaces).text
+
+    def set_description(self, rule):
+        self.description = (
+            etree.tostring(
+                rule.find("xccdf:description", self.namespaces), method="text"
+            )
+            .decode("utf8")
+            .strip()
+        )
+
+    def set_references(self, rule):
+        self.references = "\n".join(
+            self._format_reference(ref)
+            for ref in rule.findall("xccdf:reference", self.namespaces)
+        )
+        assert self.references
+
+    def set_rationale(self, rule):
+        rationale_element = rule.find("xccdf:rationale", self.namespaces)
+        self.rationale = (
+            etree.tostring(rationale_element, method="text").decode("utf-8").strip()
+            if rationale_element is not None
+            else ""
+        )
+
+    def set_oval_values(self, rule_result):
+        for val in ["name", "href"]:
+            setattr(
+                self,
+                f"oval_{val}",
+                rule_result.find(
+                    "xccdf:check/xccdf:check-content-ref", self.namespaces
+                ).attrib[val],
+            )
+
+    @classmethod
+    def get_failed_results(cls, root):
+        return [
+            rule_result
+            for rule_result in root.findall(
+                "xccdf:TestResult/xccdf:rule-result", cls.namespaces
+            )
+            if rule_result.find("xccdf:result", cls.namespaces).text
+            in ["notchecked", "fail", "error"]
+        ]
+
+
+@dataclass(slots=True, frozen=True)
+class OscapFinding(AbstractFinding):
+    identifier: str
+    severity: str
+    description: str
+    rule_id: str = None
+    score: str = ""
+    package: str = None
+    package_path: str = None
+    scan_source: str = "oscap_comp"
+    justification: str = None
+
+    oval_rule: ClassVar[
+        str
+    ] = "xccdf_org.ssgproject.content_rule_security_patches_up_to_date"
+
+    @classmethod
+    def get_findings_from_rule_info(cls, rule_info) -> list[object]:
+        """
+        Gather findings from single rule_result
+        If rule_result iss OVAL, this list could include several findings, if rule_result is compliance only one finding will be returned in the list
+        """
+        finding_class = (
+            OscapOVALFinding
+            if rule_info.rule_id == cls.oval_rule
+            else OscapComplianceFinding
+        )
+        return finding_class.get_findings_from_rule_info(rule_info=rule_info)
+
+    @property
+    def desc(self):
+        return self.description
+
     def __hash__(self):
         return hash(self.identifier)
 
 
 @dataclass(slots=True, frozen=True, eq=True)
 class OscapComplianceFinding(OscapFinding):
+    identifiers: tuple = field(default_factory=lambda: ())
+    title: str = None
+    result: str = None
+    references: str = None
+    rationale: str = None
+    scanned_date: str = None
     _log: logger = logger.setup("OscapComplianceFinding")
 
-    @classmethod
-    def get_identifiers(cls, rule, rule_id) -> tuple:
-        identifiers = tuple(
-            ident.text for ident in rule.findall("xccdf:ident", cls.namespaces)
-        ) or [rule_id]
-        assert len(identifiers) == 1
-        return identifiers
+    @property
+    def refs(self):
+        return self.references
 
     @classmethod
-    def get_description(cls, rule):
-        return (
-            etree.tostring(
-                rule.find("xccdf:description", cls.namespaces), method="text"
-            )
-            .decode("utf8")
-            .strip()
-        )
-
-    @classmethod
-    def get_references(cls, rule):
-        references = "\n".join(
-            cls._format_reference(ref)
-            for ref in rule.findall("xccdf:reference", cls.namespaces)
-        )
-        assert references
-        return references
-
-    @classmethod
-    def get_rationale(cls, rule):
-        rationale_element = rule.find("xccdf:rationale", cls.namespaces)
-        return (
-            etree.tostring(rationale_element, method="text").decode("utf-8").strip()
-            if rationale_element is not None
-            else ""
-        )
-
-    @classmethod
-    def get_findings_from_rule_result(cls, root, rule_result) -> list[object]:
+    def get_findings_from_rule_info(cls, rule_info):
         """
         Generate a single compliance finding from a rule result
-        TODO: add notes on difference between root, rule, and rule_result
+        root: entire xml document tree structure
+        rule_result: portion of xml tree defining the result of the rule in the scan
+        rule: portion of xml tree providing information about the rule itself
+
+        Attributes like description/references/etc. are gathered here instead of a __post_init__ because they depend on the rule object
         """
-        rule_id = rule_result.attrib["idref"]
-        rule = root.find(f".//xccdf:Rule[@id='{rule_id}']", cls.namespaces)
-        identifiers = cls.get_identifiers(rule, rule_id)
-        return [
-            cls(
-                identifier=identifiers[0],
-                identifiers=identifiers,
-                severity=rule_result.attrib["severity"].lower(),
-                rule_id=rule_id,
-                title=rule.find("xccdf:title", cls.namespaces).text,
-                scanned_date=rule_result.attrib["time"],
-                result=rule_result.find("xccdf:result", cls.namespaces).text,
-                description=cls.get_description(rule),
-                references=cls.get_references(rule),
-                rationale=cls.get_rationale(rule),
-            )
-        ]
+        yield cls(
+            identifiers=rule_info.identifiers,
+            identifier=rule_info.identifier,
+            severity=rule_info.severity,
+            rule_id=rule_info.rule_id,
+            title=rule_info.title,
+            scanned_date=rule_info.time,
+            result=rule_info.result,
+            description=rule_info.description,
+            references=rule_info.references,
+            rationale=rule_info.rationale,
+        )
+
+    def __hash__(self):
+        return hash(self.identifier)
 
 
 @dataclass(slots=True, frozen=True)
 class OscapOVALFinding(OscapFinding):
+    link: str = None
     _log: logger = logger.setup("OscapOVALFinding")
 
     @classmethod
-    def get_findings_from_rule_result(cls, root, rule_result) -> list[object]:
+    def get_definition(cls, oval_name, oval_root, rule_info):
+        return oval_root.find(
+            f".//oval:definition[@id='{oval_name}']", rule_info.namespaces
+        )
+
+    @classmethod
+    def get_findings(cls, definition, rule_info):
+        return definition.findall(
+            "oval:metadata/oval:advisory/oval:cve", rule_info.namespaces
+        )
+
+    @classmethod
+    def get_description(cls, definition, rule_info):
+        return definition.find("oval:metadata/oval:title", rule_info.namespaces).text
+
+    @classmethod
+    def get_findings_from_rule_info(cls, rule_info):
         """
         Generate a list of OVAL findings from a rule result
         """
-        oval_name_href = {
-            f"finding_{attr}": rule_result.find(
-                "xccdf:check/xccdf:check-content-ref", cls.namespaces
-            ).attrib[attr]
-            for attr in ["name", "href"]
-        }
-        return cls.get_oval_findings(
-            **oval_name_href, severity=rule_result.attrib["severity"].lower()
-        )
+        oval_url = cls.get_oval_url(rule_info.oval_href)
+        oval_root = etree.parse(cls.download_oval_defintions(oval_url))
+        definition = cls.get_definition(rule_info.oval_name, oval_root, rule_info)
+        for finding in cls.get_findings(definition, rule_info):
+            yield cls(
+                rule_id=rule_info.rule_id,
+                identifier=finding.text,
+                link=finding.attrib["href"],
+                description=cls.get_description(definition, rule_info),
+                severity=rule_info.severity,
+            )
 
     @classmethod
     def get_oval_url(cls, finding_href):
@@ -204,35 +276,14 @@ class OscapOVALFinding(OscapFinding):
                 raise OvalDefintionDownloadFailure
         return artifact_path
 
-    @classmethod
-    def get_oval_findings(cls, finding_name, finding_href, severity) -> list[object]:
-        url = cls.get_oval_url(finding_href)
-        root = etree.parse(cls.download_oval_defintions(url))
-
-        findings: list[OscapOVALFinding] = []
-        definition = root.find(
-            f".//oval:definition[@id='{finding_name}']", cls.namespaces
-        )
-        for finding in definition.findall(
-            "oval:metadata/oval:advisory/oval:cve", cls.namespaces
-        ):
-            findings.append(
-                cls(
-                    identifier=finding.text,
-                    link=finding.attrib["href"],
-                    description=definition.find(
-                        "oval:metadata/oval:title", cls.namespaces
-                    ).text,
-                    severity=severity,
-                )
-            )
-        return findings
-
     def __eq__(self, other):
         """
         Prevent having multiple oval findings with same identifier
         """
         return self.identifier == other.identifier
+
+    def __hash__(self):
+        return hash(self.identifier)
 
 
 @dataclass
@@ -242,19 +293,13 @@ class OscapReportParser(ReportParser):
     @classmethod
     def get_findings(cls, scan_xml: Path) -> list[OscapComplianceFinding]:
         root = etree.parse(scan_xml)
-        failed_compliance_results = [
-            rule_result
-            for rule_result in root.findall(
-                "xccdf:TestResult/xccdf:rule-result", OscapFinding.namespaces
-            )
-            if rule_result.find("xccdf:result", OscapFinding.namespaces).text
-            in ["notchecked", "fail", "error"]
-        ]
-        oscap_findings = flatten(
-            [
-                OscapFinding.get_findings_from_rule_result(root, rule_result)
-                for rule_result in failed_compliance_results
-            ]
-        )
+
+        failed_compliance_results = RuleInfo.get_failed_results(root)
+
+        oscap_findings = []
+        for rule_result in failed_compliance_results:
+            rule_info = RuleInfo(root, rule_result)
+            oscap_findings += OscapFinding.get_findings_from_rule_info(rule_info)
+
         # remove duplicates
-        return list(set(oscap_findings))
+        return list(set(flatten(oscap_findings)))
