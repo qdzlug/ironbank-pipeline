cca31fc8ee2ae2f532544a54e8826989871a05d9
Kenneth Maguire <kenneth.maguire.ctr@us.af.mil>
2023-03-01 18:32:30 -0500

    refactor methods+attributes to AbstractFinding

--- ironbank/pipeline/scan_report_parsers/report_parser.py
+++ ironbank/pipeline/scan_report_parsers/report_parser.py
@@ -4,27 +4,40 @@ from pathlib import Path
 import csv
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(frozen=True)
 class AbstractFinding(ABC):
     # this class can hold similar attributes between different vuln
     # will be used for typing until we can start tying similarities between vulns
-    pass
+    identifier: str
+    severity: str
+    description: str
+    scan_source: str = ""
+    package: str = ""
+    package_path: str = ""
+
+    def as_dict(self) -> dict:
+        return self.__dict__
+
+    def get_dict_from_fieldnames(self, fieldnames: list[str]) -> dict:
+        return {k: v for k, v in self.as_dict().items() if k in fieldnames}
+
+    def get_justification(self, justifications: dict) -> str:
+        id = (
+            self.identifier,
+            self.package,
+            self.package_path if self.package_path != "pkgdb" else None,
+        )
+        return justifications.get(id, "") if justifications else ""  # type: ignore
 
 
 # make this an abstract class once all inheriting classes are defined
 @dataclass
 class ReportParser:
     @classmethod
-    def get_justification(cls, vuln: AbstractFinding, justifications: dict):
-        id = (
-            vuln.cve,
-            vuln.package,
-            vuln.package_path if vuln.package_path != "pkgdb" else None,
-        )
-        return justifications[id] if id in justifications else None
-
     @classmethod
-    def write_csv_from_dict_list(cls, csv_dir, dict_list, fieldnames, filename):
+    def write_csv_from_dict_list(
+        cls, csv_dir: Path, dict_list: list[dict], fieldnames: list, filename: str
+    ) -> None:
         """
         Create csv file based off prepared data. The data must be provided as a list
         of dictionaries and the rest will be taken care of.
