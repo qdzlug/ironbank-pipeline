a4f78317238d79e60701344ca70c73189fec912e
Cody Miller <codymiller@seed-innovations.com>
2023-03-15 16:02:52 -0600

    Add twistlock cve report test

--- stages/csv-output/tests/test_pipeline_csv_gen.py
+++ stages/csv-output/tests/test_pipeline_csv_gen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from io import TextIOWrapper
+import json
 import sys
 from typing import Any, Iterable
 from unittest.mock import patch
@@ -24,6 +25,7 @@ from dataclasses import dataclass
 import pytest
 import csv
 from ironbank.pipeline.utils.testing import raise_
+from ironbank.pipeline.scan_report_parsers.report_parser import ReportParser
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pipeline_csv_gen  # noqa E402
@@ -31,10 +33,51 @@ import pipeline_csv_gen  # noqa E402
 log = logger.setup("test_pipeline_csv_gen")
 
 
-OSCAP_FINDINGS: list[OscapComplianceFinding] = [
+MOCK_OSCAP_FINDINGS: list[OscapComplianceFinding] = [
     OscapComplianceFinding(identifier="test1", severity="test1", result="fail"),
     OscapComplianceFinding(identifier="test2", severity="test2", result="notchecked"),
 ]
+
+MOCK_GOOD_TWISTLOCK_CVE_FINDINGS: dict = {
+    "results": [
+        {
+            "vulnerabilities": [
+                {
+                    "id": "t_id",
+                    "status": "t_status",
+                    "cvss": 10,
+                    "vector": "t_vector",
+                    "description": "t_desc",
+                    "severity": "t_sev",
+                    "packageName": "t_pkg_name",
+                    "packageVersion": "t_pkg_ver",
+                    "link": "t_link",
+                    "riskFactors": [
+                        "t_risk",
+                    ],
+                    "impactedVersions": ["t_imp_vers"],
+                    "publishedDate": "t_pub_date",
+                    "discoveredDate": "t_disc_date",
+                    "layerTime": "t_layer_time",
+                }
+            ]
+        }
+    ]
+}
+
+MOCK_BAD_TWISTLOCK_CVE_FINDINGS: dict = {
+    "results": [
+        {
+            "vulnerabilities": [
+                {
+                    "id": "bad_id",
+                    "packageName": "bad_packageName",
+                    "packageVersion": "bad_packageVersion",
+                }
+            ]
+        }
+    ]
+}
 
 
 @dataclass
@@ -49,11 +92,50 @@ class MockWriter:
         self.rows.append(row)
 
 
+@pytest.mark.only
+def test_generate_twistlock_cve_report(monkeypatch, caplog):
+    log.info("Test successful report generation")
+    monkeypatch.setattr(Path, "open", mock_open())
+    monkeypatch.setattr(
+        ReportParser, "write_csv_from_dict_list", lambda *args, **kwargs: None
+    )
+    monkeypatch.setattr(
+        json, "load", lambda *args, **kwargs: MOCK_GOOD_TWISTLOCK_CVE_FINDINGS
+    )
+    cve_len = pipeline_csv_gen.generate_twistlock_cve_report(
+        report_path=Path("report"),
+        csv_output_dir=Path("csv_out"),
+        justifications={("t_id", "t_pkg_name-t_pkg_ver", None): "t_just"},
+    )
+    assert cve_len == 1
+
+    log.info("Test key error")
+    monkeypatch.setattr(
+        json, "load", lambda *args, **kwargs: MOCK_BAD_TWISTLOCK_CVE_FINDINGS
+    )
+    with pytest.raises(SystemExit):
+        cve_len = pipeline_csv_gen.generate_twistlock_cve_report(
+            report_path=Path("report"),
+            csv_output_dir=Path("csv_out"),
+            justifications={},
+        )
+    assert "Missing key." in caplog.text
+
+    log.info("Test no cves")
+    monkeypatch.setattr(
+        json, "load", lambda *args, **kwargs: {"results": [{"vulnerabilities": []}]}
+    )
+    cve_len = pipeline_csv_gen.generate_twistlock_cve_report(
+        report_path=Path("report"), csv_output_dir=Path("csv_out"), justifications={}
+    )
+    assert cve_len == 0
+
+
 def test_generate_oscap_compliance_report(monkeypatch, caplog):
     log.info("Test successful report generated")
     monkeypatch.setattr(Path, "open", mock_open())
     monkeypatch.setattr(
-        OscapReportParser, "get_findings", lambda *args, **kwargs: OSCAP_FINDINGS
+        OscapReportParser, "get_findings", lambda *args, **kwargs: MOCK_OSCAP_FINDINGS
     )
     mock_writer = MockWriter()
     monkeypatch.setattr(csv, "writer", lambda *args, **kwargs: mock_writer)
