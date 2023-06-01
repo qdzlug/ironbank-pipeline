3d54ec59cff9d04a3dcf5a3d919a595d2940f9c5
Sam Suttman <s.suttman@wearemetronome.com>
2023-06-01 09:26:00 -0600

    mock skopeo

--- ironbank/pipeline/test/test_upload_to_harbor.py
+++ ironbank/pipeline/test/test_upload_to_harbor.py
@@ -13,6 +13,8 @@ def mock_environ(monkeypatch):
     monkeypatch.setenv("CI_PROJECT_DIR", "/path/to/project_dir")
     monkeypatch.setenv("ACCESS_LOG_DIR", "/path/to/access_log_dir")
     monkeypatch.setenv("SBOM_DIR", "/path/to/sbom_dir")
+    monkeypatch.setenv("DOCKER_AUTH_FILE_PRE_PUBLISH", "/path/to/auth_file")
+    monkeypatch.setenv("IMAGE_PODMAN_SHA", "image_digest")
 
 
 def test_generate_attestation_predicates(mock_environ, monkeypatch, tmp_path):
@@ -43,37 +45,33 @@ def test_generate_attestation_predicates(mock_environ, monkeypatch, tmp_path):
     mock_generate_vat_response_lineage.assert_called_once()
 
 
-# def test_compare_digests(monkeypatch):
-#     # Mock the necessary environment variables
-#     monkeypatch.setenv("DOCKER_AUTH_FILE_PRE_PUBLISH", "/path/to/auth_file")
-#     monkeypatch.setenv("IMAGE_PODMAN_SHA", "image_digest")
-#     # 
-#     # Patch the Skopeo class and its methods
-#     mock_inspect = "remote_inspect_raw"
-    # mock_skopeo = patch.object(upload_to_harbor.Skopeo, "inspect", return_value=mock_inspect)
+def test_compare_digests(monkeypatch):
+    # Patch the Skopeo class and its methods
+    mock_inspect = "remote_inspect_raw"
+    mock_skopeo = patch.object(upload_to_harbor.Skopeo, "inspect", return_value=mock_inspect)
     
-    # # Patch the logger
-    # mock_info = patch.object(upload_to_harbor.log, "info")
-    # mock_error = patch.object(upload_to_harbor.log, "error")
+    # Patch the logger
+    mock_info = patch.object(upload_to_harbor.log, "info")
+    mock_error = patch.object(upload_to_harbor.log, "error")
 
-    # monkeypatch.setattr(upload_to_harbor.log, "info", mock_info)
-    # monkeypatch.setattr(upload_to_harbor.log, "error", mock_error)
-    # monkeypatch.setattr(upload_to_harbor, "Skopeo", mock_skopeo)
+    monkeypatch.setattr(upload_to_harbor.log, "info", mock_info)
+    monkeypatch.setattr(upload_to_harbor.log, "error", mock_error)
+    monkeypatch.setattr(upload_to_harbor, "Skopeo", mock_skopeo)
 
-    # # Call the function under test
-    # staging_image = upload_to_harbor.Image()
-    # upload_to_harbor.compare_digests(staging_image)
+    # Call the function under test
+    staging_image = upload_to_harbor.Image()
+    upload_to_harbor.compare_digests(staging_image)
 
-    # # Assertions
-    # mock_skopeo.inspect.assert_called_once_with(
-    #     staging_image.from_image(transport="docker://"),
-    #     raw=True,
-    #     log_cmd=True
-    # )
-    # mock_info.assert_called_with("Pulling manifest_file with skopeo")
-    # mock_info.assert_called_with("Inspecting image in registry")
-    # mock_error.assert_not_called()
-    # assert mock_info.call_count == 2
+    # Assertions
+    mock_skopeo.inspect.assert_called_once_with(
+        staging_image.from_image(transport="docker://"),
+        raw=True,
+        log_cmd=True
+    )
+    mock_info.assert_called_with("Pulling manifest_file with skopeo")
+    mock_info.assert_called_with("Inspecting image in registry")
+    mock_error.assert_not_called()
+    assert mock_info.call_count == 2
 
 # def test_promote_tags(monkeypatch: MonkeyPatch):
 #     # Mock the necessary environment variables
