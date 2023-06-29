cd476b2b2eecc13fbba53ac95ea65aa9b5c7c741
Sam Suttman <s.suttman@wearemetronome.com>
2023-06-29 13:19:39 -0600

    linting updates

--- stages/scanning/anchore/anchore_scan.py
+++ stages/scanning/anchore/anchore_scan.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+from pathlib import Path
+import sys
+
 from ironbank_py39_modules.scanner_api_handlers.anchore import (  # pylint: disable=C0413
     Anchore,
 )
 
-import os
-from pathlib import Path
-import sys
 
 # need to keep this path append until this repo migrates to using ironbank-modules
 sys.path.append(Path(__file__).absolute().parents[3].as_posix())
