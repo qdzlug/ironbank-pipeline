8968f884dba1ff275fabb25939e9d5ec16783b40
Jacob Dorlac <j.dorlac@wearemetronome.com>
2023-08-04 19:30:39 +0000

    add Args version 2

--- stages/vat/args.py
+++ stages/vat/args.py
@@ -1,77 +1,111 @@
-from dataclasses import dataclass
-from datetime import datetime
 import os
+from datetime import datetime
 
 
-@dataclass
 class Args:
     """DCCSCR processing of CVE reports from various sources."""
+
+    def __init__(self) -> None:
+        self._timestamp = datetime.utcnow().strftime("%FT%TZ")
 
     ###
     # Required
     ###
 
-    # Pipeline job ID
-    job_id: str = os.environ.get("CI_PIPELINE_ID", "")
+    @property
+    def job_id(self) -> str:
+        """Pipeline job ID."""
+        return os.environ.get("CI_PIPELINE_ID", "")
 
-    # Timestamp for current pipeline run
-    timestamp: str = datetime.utcnow().strftime("%FT%TZ")
+    @property
+    def timestamp(self) -> str:
+        """Timestamp for current pipeline run."""
+        return self._timestamp
 
-    # Scan date for pipeline run
-    scan_date: str = os.environ.get("BUILD_DATE", "")
+    @property
+    def scan_date(self) -> str:
+        """Scan date for pipeline run."""
+        return os.environ.get("BUILD_DATE", "")
 
-    # Build date for pipeline run
-    build_date: str = os.environ.get("BUILD_DATE_TO_SCAN", "")
+    @property
+    def build_date(self) -> str:
+        """Build date for pipeline run."""
+        return os.environ.get("BUILD_DATE_TO_SCAN", "")
 
-    # Commit hash for container build
-    commit_hash: str = os.environ.get("COMMIT_SHA_TO_SCAN", "")
+    @property
+    def commit_hash(self) -> str:
+        """Commit hash for container build."""
+        return os.environ.get("COMMIT_SHA_TO_SCAN", "")
 
-    # Container VENDOR/PRODUCT/CONTAINER
-    container: str = os.environ.get("IMAGE_NAME", "")
+    @property
+    def container(self) -> str:
+        """Container VENDOR/PRODUCT/CONTAINER."""
+        return os.environ.get("IMAGE_NAME", "")
 
-    # Container Version from VENDOR/PRODUCT/CONTAINER/VERSION format
-    version: str = os.environ.get("IMAGE_VERSION", "")
+    @property
+    def version(self) -> str:
+        """Container Version from VENDOR/PRODUCT/CONTAINER/VERSION format."""
+        return os.environ.get("IMAGE_VERSION", "")
 
-    # Container Digest as SHA256 Hash
-    digest: str = os.environ.get("DIGEST_TO_SCAN", "")
+    @property
+    def digest(self) -> str:
+        """Container Digest as SHA256 Hash."""
+        return os.environ.get("DIGEST_TO_SCAN", "")
 
-    # Location of the twistlock JSON scan file
-    twistlock: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/twistlock/twistlock_cve.json"
+    @property
+    def twistlock(self) -> str:
+        """Location of the twistlock JSON scan file."""
+        return f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/twistlock/twistlock_cve.json"
 
-    # Location of the anchore_security.json scan file
-    anchore_sec: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_security.json"
+    @property
+    def anchore_sec(self) -> str:
+        """Location of the anchore_security.json scan file."""
+        return f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_security.json"
 
-    # Location of the anchore_gates.json scan file
-    anchore_gates: str = (
-        f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_gates.json"
-    )
+    @property
+    def anchore_gates(self) -> str:
+        """Location of the anchore_gates.json scan file."""
+        return f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_gates.json"
 
-    # More weird bash
-    # Link to openscap compliance reports directory
-    comp_link: str = os.environ.get("OSCAP_COMPLIANCE_URL:-''", "")
+    @property
+    def comp_link(self) -> str:
+        """Link to openscap compliance reports directory."""
+        return os.environ.get("OSCAP_COMPLIANCE_URL", "")
 
     ###
     # Optional
     ###
 
-    # Url for API POST
-    api_url: str = (
-        f"{os.environ.get('VAT_BACKEND_URL')}/internal/import/scan"
-        if os.environ.get("VAT_BACKEND_URL")
-        else "http://localhost:4000/internal/import/scan"
-    )
+    @property
+    def api_url(self) -> str:
+        """Url for API POST."""
+        return (
+            f"{os.environ.get('VAT_BACKEND_URL')}/internal/import/scan"
+            if os.environ.get("VAT_BACKEND_URL")
+            else "http://localhost:4000/internal/import/scan"
+        )
 
-    # Location of the oscap scan XML file
-    oscap: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/openscap/compliance_output_report.xml"
+    @property
+    def oscap(self) -> str:
+        """Location of the oscap scan XML file."""
+        return f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/openscap/compliance_output_report.xml"
 
-    # Parent VENDOR/PRODUCT/CONTAINER
-    parent: str = os.environ.get("BASE_IMAGE", "")
+    @property
+    def parent(self) -> str:
+        """Parent VENDOR/PRODUCT/CONTAINER."""
+        return os.environ.get("BASE_IMAGE", "")
 
-    # Parent Version from VENDOR/PRODUCT/CONTAINER/VERSION format
-    parent_version: str = os.environ.get("BASE_TAG", "")
+    @property
+    def parent_version(self) -> str:
+        """Parent Version from VENDOR/PRODUCT/CONTAINER/VERSION format."""
+        return os.environ.get("BASE_TAG", "")
 
-    # Link to container repository
-    repo_link: str = os.environ.get("CI_PROJECT_URL", "")
+    @property
+    def repo_link(self) -> str:
+        """Link to container repository."""
+        return os.environ.get("CI_PROJECT_URL", "")
 
-    # Dump payload for API to out.json file
-    use_json: bool = bool(os.environ.get("USE_JSON", False))
+    @property
+    def use_json(self) -> bool:
+        """Dump payload for API to out.json file."""
+        return bool(os.environ.get("USE_JSON", False))
