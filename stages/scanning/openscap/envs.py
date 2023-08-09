1acf5b3f43022be946e5bfcbd28c21b6cbd5b043
Jacob Dorlac <j.dorlac@wearemetronome.com>
2023-08-09 16:49:54 -0500

    linting and formating

--- stages/scanning/openscap/envs.py
+++ stages/scanning/openscap/envs.py
@@ -3,22 +3,25 @@ from log import log
 
 # TODO: move the envs
 
+
 def fetch_env_var(func_name: str, docstring: str, default: str = "") -> str:
+    """Gets the env or returns the default"""
     uppercase_name = func_name.upper()
     env = os.getenv(uppercase_name, default)
-    
+
     if not env:
         log_msg = f"Environment variable {uppercase_name} is not set."
         if docstring:
             log_msg += f"\n{uppercase_name}: {docstring}"
         log.info(log_msg)
-    
+
     return env
 
-def ci_var(func=None , *, default: str = ""):
+
+def ci_var(func=None, *, default: str = ""):
     """
     Decorator to fetch corresponding environment variable for a function's name.
-    
+
     Examples
     --------
     class Config:
@@ -31,26 +34,31 @@ def ci_var(func=None , *, default: str = ""):
             "This would fetch DATABASE_HOST or default to 'localhost'."
     """
     if func:
-        return property(lambda instance: fetch_env_var(func.__name__, func.__doc__, default))
-    
-    return lambda func: property(lambda instance: fetch_env_var(func.__name__, func.__doc__, default))
+        return property(
+            lambda instance: fetch_env_var(func.__name__, func.__doc__, default)
+        )
+
+    return lambda func: property(
+        lambda instance: fetch_env_var(func.__name__, func.__doc__, default)
+    )
+
 
 # pylint: disable=missing-function-docstring
 class Envs:
     """
     A class to represent the CI/CD environment variables.
-    
-    This class is designed to use decorators for fetching environment variables based on method names. 
-    Each method represents an environment variable, and when it's accessed, the corresponding 
-    environment variable is fetched. If the environment variable is not set, the system will log 
+
+    This class is designed to use decorators for fetching environment variables based on method names.
+    Each method represents an environment variable, and when it's accessed, the corresponding
+    environment variable is fetched. If the environment variable is not set, the system will log
     the missing variable and its description.
 
     Notes
     -----
-    While the method docstrings describe the purpose and source of each environment 
-    variable, the methods don't execute any functional logic. Instead, when accessed, 
-    the `ci_var` decorator ensures the environment variable corresponding to the method 
-    name is fetched. If a default value is provided and the environment variable doesn't exist, 
+    While the method docstrings describe the purpose and source of each environment
+    variable, the methods don't execute any functional logic. Instead, when accessed,
+    the `ci_var` decorator ensures the environment variable corresponding to the method
+    name is fetched. If a default value is provided and the environment variable doesn't exist,
     the default value will be returned.
     """
 
@@ -92,7 +100,7 @@ class Envs:
     def oscap_scans(self) -> str:
         """
         Path for OpenSCAP stage artifacts
-        
+
         Source
         ------
         Gitlab Configuration
@@ -113,7 +121,7 @@ class Envs:
     def scap_url(self) -> str:
         """
         The URI to download scap content from
-        
+
         Source
         ------
         Unknown, probably does not exist.
