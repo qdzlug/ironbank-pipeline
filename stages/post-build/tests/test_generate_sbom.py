6d8c854647ec0f551375e1187e2a96928628fb99
dakusu <d.akusu@wearemetronome.com>
2023-08-11 14:27:27 -0500

    MR Tasks

--- stages/post-build/tests/test_generate_sbom.py
+++ stages/post-build/tests/test_generate_sbom.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
-from unittest.mock import patch
 from common.utils import logger
 
 sys.path.append(Path(__file__).absolute().parents[1].as_posix())
@@ -11,13 +10,8 @@ from ironbank_py39_modules.scanner_api_handlers.anchore import Anchore  # noqa: 
 log = logger.setup("test_generate_sbom")
 
 
-class MockAnchore(Anchore):
-    def generate_sbom(*args, **kwargs):
-        return None
-
-
-@patch("generate_sbom.Anchore", new=MockAnchore)
 def test_main(monkeypatch, caplog):
+    monkeypatch.setattr(Anchore, "generate_sbom", lambda *args, **kwargs: None)
     monkeypatch.setenv("ANCHORE_URL", "mock_ANCHORE_URL")
     monkeypatch.setenv("ANCHORE_USERNAME", "mock_ANCHORE_USERNAME")
     monkeypatch.setenv("ANCHORE_VERIFY", "mock_ANCHORE_VERIFY")
