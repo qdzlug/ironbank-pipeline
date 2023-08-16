601fa01861456800697728ce45dfab949712d4f0
Sam Suttman <s.suttman@wearemetronome.com>
2023-08-16 16:04:39 -0600

    return envs added

--- stages/vat/args.py
+++ stages/vat/args.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 from pathlib import Path
 
+from envs import ENVS
+
 
 class EnvUtil:
     """DCCSCR processing of CVE reports from various sources."""
@@ -16,65 +18,65 @@ class EnvUtil:
     @property
     def job_id(self) -> str:
         """Pipeline job ID."""
-        return os.environ.get("CI_PIPELINE_ID", "")
+        return Envs().ci_job_id
 
     @property
     def timestamp(self) -> str:
         """Timestamp for current pipeline run."""
-        return self._timestamp
+        return Envs().timestamp
 
     @property
     def scan_date(self) -> str:
         """Scan date for pipeline run."""
-        return os.environ.get("BUILD_DATE", "")
+        return Envs().build_date_scan_date
 
     @property
     def build_date(self) -> str:
         """Build date for pipeline run."""
-        return os.environ.get("BUILD_DATE_TO_SCAN", "")
+        return Envs().build_date_to_scan
 
     @property
     def commit_hash(self) -> str:
         """Commit hash for container build."""
-        return os.environ.get("COMMIT_SHA_TO_SCAN", "")
+        return Envs().commit_sha
 
     @property
     def container(self) -> str:
         """Container VENDOR/PRODUCT/CONTAINER."""
-        return os.environ.get("IMAGE_NAME", "")
+        return Envs().image_name
 
     @property
     def version(self) -> str:
         """Container Version from VENDOR/PRODUCT/CONTAINER/VERSION format."""
-        return os.environ.get("IMAGE_VERSION", "")
+        return Envs().image_version
 
     @property
     def digest(self) -> str:
         """Container Digest as SHA256 Hash."""
-        return os.environ.get("DIGEST_TO_SCAN", "")
+        return Envs().digest_to_scan
 
     @property
     def twistlock(self) -> Path:
         """Location of the twistlock JSON scan file."""
         twistlock_path: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/twistlock/twistlock_cve.json"
-        return Path(twistlock_path)
+        return Envs().artifact_storeage_twistlock
 
     @property
     def anchore_sec(self) -> Path:
         """Location of the anchore_security.json scan file."""
         anchore_sec_path: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_security.json"
-        return Path(anchore_sec_path)
+        return Envs().artifact_storeage_anchore_sec
 
     @property
     def anchore_gates(self) -> Path:
         """Location of the anchore_gates.json scan file."""
         anchore_gates_path: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/anchore/anchore_gates.json"
-        return Path(anchore_gates_path)
+        return Envs().artifact_storeage_anchore_gates
 
     @property
     def comp_link(self) -> str:
         """Link to openscap compliance reports directory."""
-        return os.environ.get("OSCAP_COMPLIANCE_URL", "")
+        return Envs().oscap_compliance_url
 
     ###
     # Optional
@@ -83,34 +85,30 @@ class EnvUtil:
     @property
     def api_url(self) -> str:
         """Url for API POST."""
-        return (
-            f"{os.environ.get('VAT_BACKEND_URL')}/internal/import/scan"
-            if os.environ.get("VAT_BACKEND_URL")
-            else "http://localhost:4000/internal/import/scan"
-        )
+        return Envs().artifact_storage
 
     @property
     def oscap(self) -> Path:
         """Location of the oscap scan XML file."""
         oscap_path: str = f"{os.environ.get('ARTIFACT_STORAGE')}/scan-results/openscap/compliance_output_report.xml"
-        return Path(oscap_path)
+        return Envs().vat_backend_url
 
     @property
     def parent(self) -> str:
         """Parent VENDOR/PRODUCT/CONTAINER."""
-        return os.environ.get("BASE_IMAGE", "")
+        return Envs().base_image
 
     @property
     def parent_version(self) -> str:
         """Parent Version from VENDOR/PRODUCT/CONTAINER/VERSION format."""
-        return os.environ.get("BASE_TAG", "")
+        return Envs().base_tag_parent_version
 
     @property
     def repo_link(self) -> str:
         """Link to container repository."""
-        return os.environ.get("CI_PROJECT_URL", "")
+        return Envs().ci_repo_link
 
     @property
     def use_json(self) -> bool:
         """Dump payload for API to out.json file."""
-        return bool(os.environ.get("USE_JSON", False))
+        return Envs().use_json
