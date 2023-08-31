b4d61bb15f8a9441c753b073a04191c18c23dbbc
dakusu <d.akusu@wearemetronome.com>
2023-08-31 14:07:16 -0500

    MR Tasks

--- stages/post-build/tests/test_generate_sbom.py
+++ stages/post-build/tests/test_generate_sbom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call
 from common.utils import logger
 
 sys.path.append(Path(__file__).absolute().parents[1].as_posix())
@@ -10,6 +10,7 @@ from ironbank_py39_modules.scanner_api_handlers.anchore import Anchore  # noqa: 
 
 log = logger.setup("test generate_sbom.main")
 
+
 def test_main(monkeypatch, caplog):
     # monkeypatch.setattr(Anchore, "generate_sbom", lambda *args, **kwargs: None)
     monkeypatch.setenv("ANCHORE_URL", "mock_ANCHORE_URL")
@@ -17,20 +18,22 @@ def test_main(monkeypatch, caplog):
     monkeypatch.setenv("ANCHORE_VERIFY", True)
     monkeypatch.setenv("SBOM_DIR", "mock_SBOM_DIR")
     monkeypatch.setenv("IMAGE_FULLTAG", "mock_IMAGE_FULLTAG")
-    
-    with patch("generate_sbom.Anchore", new=MagicMock) as MockedAnchore:
-        mock_anchore_instance = MockedAnchore.return_value 
+
+    with patch("generate_sbom.Anchore", new=MagicMock()) as MockedAnchore:
+        mock_anchore_instance = MockedAnchore.return_value
         generate_sbom.main()
-        MockedAnchore.assert_called_with(url='mock_ANCHORE_URL',
-            username='mock_ANCHORE_USERNAME',
-            password='',
-            verify='True'
+        MockedAnchore.assert_called_with(
+            url="mock_ANCHORE_URL",
+            username="mock_ANCHORE_USERNAME",
+            password="",
+            verify="True",
         )
 
-        mock_anchore_instance.generate_sbom.assert_has_calls([
-                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'cyclonedx-json', 'json'),
-                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'spdx-tag-value', 'txt'),
-                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'spdx-json', 'json'),
-                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'json', 'json', 'syft')
-            ])
-    
+        mock_anchore_instance.generate_sbom.assert_has_calls(
+            [
+                call("mock_IMAGE_FULLTAG", "mock_SBOM_DIR", "cyclonedx-json", "json"),
+                call("mock_IMAGE_FULLTAG", "mock_SBOM_DIR", "spdx-tag-value", "txt"),
+                call("mock_IMAGE_FULLTAG", "mock_SBOM_DIR", "spdx-json", "json"),
+                call("mock_IMAGE_FULLTAG", "mock_SBOM_DIR", "json", "json", "syft"),
+            ]
+        )
