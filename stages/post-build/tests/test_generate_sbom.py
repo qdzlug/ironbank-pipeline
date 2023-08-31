bd1358708d1d97d1cab4909facf23e65852e2911
dakusu <d.akusu@wearemetronome.com>
2023-08-31 13:52:47 -0500

    MR Tasks

--- stages/post-build/tests/test_generate_sbom.py
+++ stages/post-build/tests/test_generate_sbom.py
@@ -10,20 +10,27 @@ from ironbank_py39_modules.scanner_api_handlers.anchore import Anchore  # noqa: 
 
 log = logger.setup("test generate_sbom.main")
 
-
 def test_main(monkeypatch, caplog):
     # monkeypatch.setattr(Anchore, "generate_sbom", lambda *args, **kwargs: None)
     monkeypatch.setenv("ANCHORE_URL", "mock_ANCHORE_URL")
     monkeypatch.setenv("ANCHORE_USERNAME", "mock_ANCHORE_USERNAME")
-    monkeypatch.setenv("ANCHORE_VERIFY", "mock_ANCHORE_VERIFY")
+    monkeypatch.setenv("ANCHORE_VERIFY", True)
     monkeypatch.setenv("SBOM_DIR", "mock_SBOM_DIR")
     monkeypatch.setenv("IMAGE_FULLTAG", "mock_IMAGE_FULLTAG")
-
-    with patch(
-        "ironbank_py39_modules.scanner_api_handlers.anchore.Anchore.generate_sbom",
-        new=MagicMock(),
-    ) as generate_sbom_call:
+    
+    with patch("generate_sbom.Anchore", new=MagicMock) as MockedAnchore:
+        mock_anchore_instance = MockedAnchore.return_value 
         generate_sbom.main()
-    generate_sbom_call.assert_called_with(
-        "mock_IMAGE_FULLTAG", "mock_SBOM_DIR", "json", "json", "syft"
-    )
+        MockedAnchore.assert_called_with(url='mock_ANCHORE_URL',
+            username='mock_ANCHORE_USERNAME',
+            password='',
+            verify='True'
+        )
+
+        mock_anchore_instance.generate_sbom.assert_has_calls([
+                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'cyclonedx-json', 'json'),
+                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'spdx-tag-value', 'txt'),
+                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'spdx-json', 'json'),
+                MagicMock.call('mock_IMAGE_FULLTAG', 'mock_SBOM_DIR', 'json', 'json', 'syft')
+            ])
+    
