f5338de108c72f09efbe2eb7ff1ebee6f8558251
David Akusu <d.akusu@wearemetronome.com>
2024-02-14 16:38:00 -0600

    testing arm64 job

--- stages/build/build.py
+++ stages/build/build.py
@@ -76,7 +76,7 @@ def load_resources(
         if resource_file_obj.is_file() and not resource_file_obj.is_symlink():
             if resource_type == "image" and skopeo:
                 manifest = subprocess.run(
-                    ["cat", f"{Path(resource_dir, resource_file)}/manifest.json"],
+                    ["tar", "-xf", resource_file_obj.as_posix(), "-O", "manifest.json"],
                     capture_output=True,
                     check=True,
                 )
