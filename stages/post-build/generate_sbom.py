869e984e8cba2823a7f0ed071938c52564981cbf
Rob McCarthy <r.mccarthy@wearemetronome.com>
2024-02-14 14:41:17 +0000

    generation of sboms w/multiprocess

--- stages/post-build/generate_sbom.py
+++ stages/post-build/generate_sbom.py
@@ -3,12 +3,23 @@
 import os
 import sys
 from pathlib import Path
+from multiprocessing.pool import ThreadPool as Pool
+from functools import partial
 
 # pylint: disable=C0413
 sys.path.append(Path(__file__).absolute().parents[2].as_posix())
 from ironbank_py39_modules.scanner_api_handlers.anchore import (
     Anchore,
 )
+
+
+def generate_sbom_parallel(anchore_scan, image, artifacts_path, fmt) -> None:
+    """Helper function which selects the appropriate args when calling
+    generate_sbom."""
+    if len(fmt) < 3:
+        anchore_scan.generate_sbom(image, artifacts_path, fmt[0], fmt[1])
+    else:
+        anchore_scan.generate_sbom(image, artifacts_path, fmt[0], fmt[1], fmt[2])
 
 
 def main() -> None:
@@ -42,10 +53,20 @@ def main() -> None:
 
     image = os.environ["IMAGE_FULLTAG"]
 
-    anchore_scan.generate_sbom(image, artifacts_path, "cyclonedx-json", "json")
-    anchore_scan.generate_sbom(image, artifacts_path, "spdx-tag-value", "txt")
-    anchore_scan.generate_sbom(image, artifacts_path, "spdx-json", "json")
-    anchore_scan.generate_sbom(image, artifacts_path, "json", "json", "syft")
+    sbom_formats = [
+        ("cyclonedx-json", "json"),
+        ("spdx-tag-value", "txt"),
+        ("spdx-json", "json"),
+        ("json", "json", "syft"),
+    ]
+
+    with Pool() as pool:
+        # create intermediate function to hold arguments which are always the same (scan object, image, and artifact path)
+        partial_generate_sbom = partial(
+            generate_sbom_parallel, anchore_scan, image, artifacts_path
+        )
+        # map each format tuple to partial_generate_sbom and add it to the pool
+        pool.map(partial_generate_sbom, sbom_formats)
 
 
 if __name__ == "__main__":
