79696a7bd42589edc5c189f411fb294abf9cb7ee
c.geary <c.geary@wearemetronome.com>
2024-02-18 04:58:41 -0500

    path

--- stages/scanning/openscap/oscap_scan.sh
+++ stages/scanning/openscap/oscap_scan.sh
@@ -12,31 +12,33 @@ else
   echo "INFO: 'oscap_profiler.py ${OS_TYPE}' returned '${OSCAP_PROFILE}' begin scan"
 fi
 
-mkdir -p /"${OSCAP_SCANS}"
+mkdir -p /opt/scan 
 PROFILE=$(echo "${OSCAP_PROFILE}" | grep -o '"profile": "[^"]*' | grep -o '[^"]*$')
 SECURITY_GUIDE=$(echo "${OSCAP_PROFILE}" | grep -o '"securityGuide": "[^"]*' | grep -o '[^"]*$')
 SCANNER=$(echo "${OSCAP_PROFILE}" | grep -o '"scanner": "[^"]*' | grep -o '[^"]*$')
 
 podman load -q -i "/opt/$SCANNER.tar" > /dev/null
 podman run \
+  --detach \
   --privileged \
+  --name scanner \
   -v /opt:/opt \
-  -v /"${OSCAP_SCANS}":/tmp/openscap \
   -v "${DOCKER_AUTH_FILE_PULL}":/run/containers/0/auth.json \
-  "ib-oscap-${SCANNER}:0.1" \
-    bash -c "podman pull ${IMAGE_TO_SCAN} && \
-    /opt/oscap-podman ${IMAGE_TO_SCAN} \
+  "ib-oscap-${SCANNER}:0.1" sleep 900
+podman exec scanner podman pull "${IMAGE_TO_SCAN}"
+podman exec scanner /opt/oscap-podman "${IMAGE_TO_SCAN}" \
       xccdf \
       eval \
       --verbose ERROR \
-      --profile ${PROFILE} \
-      --stig-viewer /tmp/openscap/compliance_output_report_stigviewer.xml \
-      --results /tmp/openscap/compliance_output_report.xml \
-      --report /tmp/openscap/report.html \
+      --profile "${PROFILE}" \
+      --stig-viewer /opt/scan/compliance_output_report_stigviewer.xml \
+      --results /opt/scan/compliance_output_report.xml \
+      --report /opt/scan/report.html \
       --local-files /opt/ \
-      /opt/scap-security-guide/${SECURITY_GUIDE}"
+      /opt/scap-security-guide/"${SECURITY_GUIDE}"
 
-cp /opt/oscap-version.txt "${OSCAP_SCANS}/oscap-version.txt"
+cp /opt/oscap-version.txt "./${OSCAP_SCANS}/oscap-version.txt"
+cp /opt/scan/* "./${OSCAP_SCANS}/"
 echo "OSCAP_COMPLIANCE_URL=${CI_JOB_URL}" > oscap-compliance.env
 
 cat oscap-compliance.env
