589bc1bad950c28a1881cd06ca6181c270fa5c84
c.geary <c.geary@wearemetronome.com>
2024-02-28 21:32:26 -0500

    lines

--- stages/os-type/image_inspect.py
+++ stages/os-type/image_inspect.py
@@ -4,12 +4,12 @@ import os
 import sys
 from pathlib import Path
 
+from common.utils import logger
 from pipeline.container_tools.skopeo import Skopeo
 from pipeline.hardening_manifest import HardeningManifest
 from pipeline.image import Image
 from pipeline.project import DsopProject
 from pipeline.utils.exceptions import GenericSubprocessError
-from common.utils import logger
 
 log = logger.setup("pipeline_trigger")
 
@@ -17,74 +17,67 @@ log = logger.setup("pipeline_trigger")
 def template_type(os_type: str) -> None:
     """Writes the template type to an env file, based on the os_type var."""
     template_dict = {
-        "alpine317-container": """\
-DISTRO_REPO_DIR="apk-repos/repositories"
-DISTRO_REPO_MOUNT="/etc/apk/repositories"
-OS_TYPE="alpine317-container"
-""",
-        "alpine3-container": """\
-DISTRO_REPO_DIR="apk-repos/repositories"
-DISTRO_REPO_MOUNT="/etc/apk/repositories"
-OS_TYPE="alpine3-container"
-""",
-        "chainguard-container": """\
-OS_TYPE="chainguard"
-""",
-        "debian11-container": """\
-DISTRO_REPO_DIR="debian-repos/11-bullseye-ironbank.list"
-DISTRO_REPO_MOUNT="/etc/apt/sources.list"
-OS_TYPE="debian11-container"
-""",
-        "debian12-container": """\
-DISTRO_REPO_DIR="debian-repos/12-bookworm-debian.sources"
-DISTRO_REPO_MOUNT="/etc/apt/sources.list.d/debian.sources"
-OS_TYPE="debian12-container"
-""",
-        "distroless-container": """\
-OS_TYPE="distroless-container"
-""",
-        "scratch-container": """\
-OS_TYPE="scratch-container"
-""",
-        "sle15-bci-container": """\
-DISTRO_REPO_DIR="zypper-repos"
-DISTRO_REPO_MOUNT="/etc/zypp/repos.d"
-OS_TYPE="sle15-bci-container"
-""",
-        "ubi7-container": """\
-DISTRO_REPO_DIR="ubi-repos"
-DISTRO_REPO_MOUNT="/etc/yum.repos.d"
-OS_TYPE="ubi7-container"
-""",
-        "ubi8-container": """\
-DISTRO_REPO_DIR="ubi-repos"
-DISTRO_REPO_MOUNT="/etc/yum.repos.d"
-OS_TYPE="ubi8-container"
-""",
-        "ubi9-container": """\
-DISTRO_REPO_DIR="ubi-repos"
-DISTRO_REPO_MOUNT="/etc/yum.repos.d"
-OS_TYPE="ubi9-container"
-""",
-        "ubuntu2004-container": """\
-DISTRO_REPO_DIR="apt-repos/2004-focal-ironbank.list"
-DISTRO_REPO_MOUNT="/etc/apt/sources.list"
-OS_TYPE="ubuntu2004-container"
-UBUNTU="1"
-""",
-        "ubuntu2204-container": """\
-DISTRO_REPO_DIR="apt-repos/2204-jammy-ironbank.list"
-DISTRO_REPO_MOUNT="/etc/apt/sources.list"
-OS_TYPE="ubuntu2204-container"
-UBUNTU="1"
-""",
+        # PEP8 discourages \ and gitlab wants no blank lines
+        "alpine317-container": (
+            'DISTRO_REPO_DIR="apk-repos/repositories"\n'
+            'DISTRO_REPO_MOUNT="/etc/apk/repositories"\n'
+            'OS_TYPE="alpine317-container"'
+        ),
+        "alpine3-container": (
+            'DISTRO_REPO_DIR="apk-repos/repositories"\n'
+            'DISTRO_REPO_MOUNT="/etc/apk/repositories"\n'
+            'OS_TYPE="alpine3-container"'
+        ),
+        "chainguard-container": ('OS_TYPE="chainguard"'),
+        "debian11-container": (
+            'DISTRO_REPO_DIR="debian-repos/11-bullseye-ironbank.list"\n'
+            'DISTRO_REPO_MOUNT="/etc/apt/sources.list"\n'
+            'OS_TYPE="debian11-container"'
+        ),
+        "debian12-container": (
+            'DISTRO_REPO_DIR="debian-repos/12-bookworm-debian.sources"\n'
+            'DISTRO_REPO_MOUNT="/etc/apt/sources.list.d/debian.sources"\n'
+            'OS_TYPE="debian12-container"'
+        ),
+        "distroless-container": ('OS_TYPE="distroless-container"'),
+        "scratch-container": ('OS_TYPE="scratch-container"'),
+        "sle15-bci-container": (
+            'DISTRO_REPO_DIR="zypper-repos"\n'
+            'DISTRO_REPO_MOUNT="/etc/zypp/repos.d"\n'
+            'OS_TYPE="sle15-bci-container"'
+        ),
+        "ubi7-container": (
+            'DISTRO_REPO_DIR="ubi-repos"\n'
+            'DISTRO_REPO_MOUNT="/etc/yum.repos.d"\n'
+            'OS_TYPE="ubi7-container"'
+        ),
+        "ubi8-container": (
+            'DISTRO_REPO_DIR="ubi-repos"\n'
+            'DISTRO_REPO_MOUNT="/etc/yum.repos.d"\n'
+            'OS_TYPE="ubi8-container"'
+        ),
+        "ubi9-container": (
+            'DISTRO_REPO_DIR="ubi-repos"\n'
+            'DISTRO_REPO_MOUNT="/etc/yum.repos.d"\n'
+            'OS_TYPE="ubi9-container"'
+        ),
+        "ubuntu2004-container": (
+            'DISTRO_REPO_DIR="apt-repos/2004-focal-ironbank.list"\n'
+            'DISTRO_REPO_MOUNT="/etc/apt/sources.list"\n'
+            'OS_TYPE="ubuntu2004-container"'
+        ),
+        "ubuntu2204-container": (
+            'DISTRO_REPO_DIR="apt-repos/2204-jammy-ironbank.list"\n'
+            'DISTRO_REPO_MOUNT="/etc/apt/sources.list"\n'
+            'OS_TYPE="ubuntu2204-container"'
+        ),
     }
     template = template_dict.get(os_type)
     if not template:
-        log.error("Unknown template type for os-type: %s", os_type)
+        log.error("Unknown template for os-type: %s", os_type)
         sys.exit(1)
     log.info("Using pipeline template: %s", template)
-    with Path("template.env").open(mode="w", encoding="utf-8") as f:
+    with Path("os.env").open(mode="w", encoding="utf-8") as f:
         f.write(template)
 
 
