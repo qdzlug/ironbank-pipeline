5ec40a6db77b0b3a031097e2572b0e4e8053ebe0
c.geary <c.geary@wearemetronome.com>
2024-03-05 17:38:41 -0500

    oscap with pipeline-runner

--- stages/scanning/openscap/oscap_profiler.py
+++ stages/scanning/openscap/oscap_profiler.py
@@ -12,88 +12,106 @@ def get_oscap_guide(os_type):
         f"Determining OSCAP profile and security guide for os_type: {os_type}"
     )
 
+    oscap_guides = {}
     oscap_guides = {
         "ubi9-container": {
+            "oval": "security-data-oval-v2-RHEL9-rhel-9.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel9-ds.xml",
             "scanner": "redhat",
         },
         "ubi9-minimal-container": {
+            "oval": "security-data-oval-v2-RHEL9-rhel-9.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel9-ds.xml",
             "scanner": "redhat",
         },
         "ubi9-micro-container": {
+            "oval": "security-data-oval-v2-RHEL9-rhel-9.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel9-ds.xml",
             "scanner": "redhat",
         },
         "ubi8-container": {
+            "oval": "security-data-oval-v2-RHEL8-rhel-8.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel8-ds.xml",
             "scanner": "redhat",
         },
         "ubi8-minimal-container": {
+            "oval": "security-data-oval-v2-RHEL8-rhel-8.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel8-ds.xml",
             "scanner": "redhat",
         },
         "ubi8-micro-container": {
+            "oval": "security-data-oval-v2-RHEL8-rhel-8.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel8-ds.xml",
             "scanner": "redhat",
         },
         "ubi7-container": {
+            "oval": "security-data-oval-v2-RHEL7-rhel-7.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel7-ds.xml",
             "scanner": "redhat",
         },
         "ubi7-minimal-container": {
+            "oval": "security-data-oval-v2-RHEL7-rhel-7.oval.xml.bz2",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-rhel7-ds.xml",
             "scanner": "redhat",
         },
         "ubuntu2004-container": {
+            "oval": "none",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-ubuntu2004-ds.xml",
             "scanner": "debian",
         },
         "sle15-bci-container": {
+            "oval": "pub-projects-security-oval-suse.linux.enterprise.15.xml",
             "profile": "xccdf_org.ssgproject.content_profile_stig",
             "securityGuide": "ssg-sle15-ds.xml",
             "scanner": "suse",
         },
         "debian11-container": {
+            "oval": "none",
             "profile": "xccdf_org.ssgproject.content_profile_anssi_np_nt28_average",
             "securityGuide": "ssg-debian11-ds.xml",
             "scanner": "debian",
         },
         "debian12-container": {
+            "oval": "none",
             "profile": "xccdf_org.ssgproject.content_profile_anssi_np_nt28_average",
             "securityGuide": "ssg-debian12-ds.xml",
             "scanner": "debian",
         },
         "alpine317-container": {
+            "oval": "none",
             "profile": "none",
             "securityGuide": "none",
             "scanner": "none",
         },
         "alpine3-container": {
+            "oval": "none",
             "profile": "none",
             "securityGuide": "none",
             "scanner": "none",
         },
         "chainguard-container": {
+            "oval": "none",
             "profile": "none",
             "securityGuide": "none",
             "scanner": "none",
         },
         "distroless-container": {
+            "oval": "none",
             "profile": "none",
             "securityGuide": "none",
             "scanner": "none",
         },
         "scratch-container": {
+            "oval": "none",
             "profile": "none",
             "securityGuide": "none",
             "scanner": "none",
--- stages/scanning/openscap/oscap_scan.sh
+++ stages/scanning/openscap/oscap_scan.sh
@@ -5,20 +5,29 @@ set -euo pipefail
 OSCAP_PROFILE=$(python3 "${PIPELINE_REPO_DIR}/stages/scanning/openscap/oscap_profiler.py" "${OS_TYPE}")
 
 # if OS_TYPE has a 'none' OSCAP_PROFILE, exit gracefully
-if [ "$OSCAP_PROFILE" = '{"profile": "none", "securityGuide": "none", "scanner": "none"}' ]; then
+if [ "$OSCAP_PROFILE" = '{"oval": "none", "profile": "none", "securityGuide": "none", "scanner": "none"}' ]; then
   echo "INFO 'oscap_profiler.py ${OS_TYPE}' returned '${OSCAP_PROFILE}' nothing to do"
   exit 0
 else
   echo "INFO 'oscap_profiler.py ${OS_TYPE}' returned '${OSCAP_PROFILE}' begin scan"
 fi
 
+OVAL=$(echo "${OSCAP_PROFILE}" | grep -o '"oval": "[^"]*' | grep -o '[^"]*$')
 PROFILE=$(echo "${OSCAP_PROFILE}" | grep -o '"profile": "[^"]*' | grep -o '[^"]*$')
 SECURITY_GUIDE=$(echo "${OSCAP_PROFILE}" | grep -o '"securityGuide": "[^"]*' | grep -o '[^"]*$')
 SCANNER=$(echo "${OSCAP_PROFILE}" | grep -o '"scanner": "[^"]*' | grep -o '[^"]*$')
 
+# if OVAL fetch from ci-files
+if [ "$OVAL" = 'none' ]; then
+  echo "INFO no oval, skipping"
+else
+  echo "INFO oval $OVAL, retrieving"
+  aws s3 cp --quiet s3://${CI_FILES_BUCKET}/gitlab-runner-dsop-privileged/oscap/"$OSCAP_OVAL" /opt/oscap/"$OSCAP_OVAL"
+fi
+
 # scan artifact(s)
 mkdir -p "${CI_PROJECT_DIR}"/"${OSCAP_SCANS}"
-cp /opt/version.txt "${CI_PROJECT_DIR}/${OSCAP_SCANS}/oscap-version.txt"
+cp /opt//oscap/version.txt "${CI_PROJECT_DIR}/${OSCAP_SCANS}/oscap-version.txt"
 
 # env artifact(s)
 echo "OSCAP_COMPLIANCE_URL=${CI_JOB_URL}" >"${CI_PROJECT_DIR}/oscap-compliance.env"
@@ -31,7 +40,7 @@ cp "${DOCKER_AUTH_FILE_PULL}" /run/containers/0/auth.json
 # if redhat, natively scan
 if [ "${SCANNER}" = 'redhat' ]; then
   echo "INFO performing scan"
-  /opt/oscap-podman "${IMAGE_TO_SCAN}" \
+  /usr/local/bin/oscap-podman "${IMAGE_TO_SCAN}" \
     xccdf \
     eval \
     --verbose ERROR \
@@ -39,20 +48,20 @@ if [ "${SCANNER}" = 'redhat' ]; then
     --stig-viewer "${CI_PROJECT_DIR}/${OSCAP_SCANS}/compliance_output_report_stigviewer.xml" \
     --results "${CI_PROJECT_DIR}/${OSCAP_SCANS}/compliance_output_report.xml" \
     --report "${CI_PROJECT_DIR}/${OSCAP_SCANS}/report.html" \
-    --local-files /opt/ \
-    /opt/scap-security-guide/"${SECURITY_GUIDE}" || true
+    --local-files /opt/oscap/ \
+    /opt/oscap/scap-security-guide/"${SECURITY_GUIDE}" || true
 
 # if debian/suse, use a scanner pod
 else
   # load the scanner
   echo "INFO loading scanner"
-  podman load -q -i "/opt/$SCANNER.tar" >/dev/null
+  podman load -q -i "/opt/oscap/$SCANNER.tar" >/dev/null
 
   # save the target, scanners may not have ca certs
   echo "INFO pulling target"
   podman pull "${IMAGE_TO_SCAN}"
   echo "INFO saving target"
-  podman image save -q -o /opt/target.tar "${IMAGE_TO_SCAN}"
+  podman image save -q -o /opt/oscap/target.tar "${IMAGE_TO_SCAN}"
 
   # sleep
   podman run \
@@ -60,6 +69,7 @@ else
     --detach \
     --privileged \
     --name scanner \
+    -v /usr/local/bin/oscap-podman:/usr/local/bin/oscap-podman \
     -v "${CI_PROJECT_DIR}/${OSCAP_SCANS}":"${CI_PROJECT_DIR}/${OSCAP_SCANS}" \
     -v "${DOCKER_AUTH_FILE_PULL}":/run/containers/0/auth.json \
     -v /opt:/opt \
@@ -67,11 +77,11 @@ else
 
   # load the saved target
   echo "INFO scanner load target"
-  podman exec scanner podman load -q -i /opt/target.tar >/dev/null
+  podman exec scanner podman load -q -i /opt/oscap/target.tar >/dev/null
 
   # scanner scan target
   echo "INFO performing scan"
-  podman exec scanner /opt/oscap-podman "${IMAGE_TO_SCAN}" \
+  podman exec scanner /usr/local/bin/oscap-podman "${IMAGE_TO_SCAN}" \
     xccdf \
     eval \
     --verbose ERROR \
@@ -79,8 +89,8 @@ else
     --stig-viewer "${CI_PROJECT_DIR}/${OSCAP_SCANS}/compliance_output_report_stigviewer.xml" \
     --results "${CI_PROJECT_DIR}/${OSCAP_SCANS}/compliance_output_report.xml" \
     --report "${CI_PROJECT_DIR}/${OSCAP_SCANS}/report.html" \
-    --local-files /opt/ \
-    /opt/scap-security-guide/"${SECURITY_GUIDE}" || true
+    --local-files /opt/oscap/ \
+    /opt/oscap/scap-security-guide/"${SECURITY_GUIDE}" || true
 fi
 
 # IMPORTANT oscap-podman completes successfully with a nonzero RC, hence '|| true' ..
