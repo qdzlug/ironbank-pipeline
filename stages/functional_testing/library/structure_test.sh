68473f26027b6e54d11e6b950cfbc0c149a0f44a
Nimesh Pandeya <ironbank@dsop.io>
2024-03-10 12:43:52 -0500

    moving report to /tmp

--- stages/functional_testing/library/structure_test.sh
+++ stages/functional_testing/library/structure_test.sh
@@ -31,9 +31,10 @@ print_cyan() {
 
 if [[ -f "/tmp/structure.yaml" ]]; then
     print_header "Running Container Structure Test"
-    $CST_BINARY test --image "$(cat /tmp/image)" --config "/tmp/structure.yaml" --output junit --test-report /home/ci/report.xml || true
+    $CST_BINARY test --image "$(cat /tmp/image)" --config "/tmp/structure.yaml" --output junit --test-report /tmp/report.xml || true
+else
     # Create a dummy JUnit report indicating no structure tests were defined
-    cat <<EOF > /home/ci/report.xml
+    cat <<EOF > /tmp/report.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <testsuites>
   <testsuite name="Container Structure Test" tests="1" errors="0" failures="0" skip="0">
