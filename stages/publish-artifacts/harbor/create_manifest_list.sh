87cfc3664343dc9ae7b2eb43416be04baf1ccdbd
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-11 16:28:41 -0500

    I believe the manifest list is working

--- stages/publish-artifacts/harbor/create_manifest_list.sh
+++ stages/publish-artifacts/harbor/create_manifest_list.sh
@@ -4,19 +4,12 @@ env
 
 # Creating a manifest list for each tag specified in the hardening_manifest.
 # The images created by this pipeline are added to the manifest list and then the manifest list is pushed to harbor.
-echo $DOCKER_AUTH_FILE_PUBLISH
 IFS=','
 read -ra tags_array <<< "$TAGS_ARM64"
 for tag in "${tags_array[@]}"; do 
-  echo "podman manifest create $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag"
-  podman manifest create $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag
-
-  echo "podman manifest add $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_X86 --authfile $DOCKER_AUTH_FILE_PUBLISH"
-  podman manifest add $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_X86
-
-  echo "podman manifest add $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_ARM64 --authfile $DOCKER_AUTH_FILE_PUBLISH"
-  podman manifest add $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_ARM64
-
-  echo "podman manifest push --all $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag --authfile $DOCKER_AUTH_FILE_PUBLISH"
-  podman manifest push --all $REGISTRY_PUBLISH_URL_ARM64/$IMAGE_NAME_ARM64:$tag
+  echo "Creating manifest list for $REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64:$tag"
+  podman manifest create $REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64:$tag
+  podman manifest add $REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_X86 --authfile $DOCKER_AUTH_FILE_PULL
+  podman manifest add $REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64:$tag docker://$REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64@$DIGEST_TO_SCAN_ARM64 --authfile $DOCKER_AUTH_FILE_PULL
+  podman manifest push --all $REGISTRY_PUBLISH_URL/$IMAGE_NAME_ARM64:$tag --authfile $DOCKER_AUTH_FILE_PUBLISH
 done
