7ece09606ce129f42768ab15f104f1afc845282b
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-13 10:14:57 -0500

    Something is up with import-artifacts

--- pipeline1/1-pre-build/downloader.py
+++ pipeline1/1-pre-build/downloader.py
@@ -73,9 +73,13 @@ def set_artifact_path(artifact):
     """
     if isinstance(artifact, AbstractFileArtifact):
         artifact.dest_path = artifact.dest_path / "external-resources"
+        # if "arm64" in artifact.architecture:
+        #     artifact.dest_path = artifact.dest_path / "arm64"
         artifact.artifact_path = artifact.dest_path / artifact.filename
     elif isinstance(artifact, ContainerArtifact):
         artifact.dest_path = artifact.dest_path / "images"
+        # if "arm64" in artifact.architecture:
+        #     artifact.dest_path = artifact.dest_path / "arm64"
         artifact.artifact_path = (
             artifact.dest_path
             / f"{artifact.tag.replace('/', '-').replace(':', '-')}.tar"
@@ -98,17 +102,27 @@ def main():
     exit_code = 1
     artifact = None
     try:
+        # TODO: Remove me
+        log.info(f"hardening_manifest.resources: {hardening_manifest.resources}")
+
         # TODO: refactor into a separate function
         for resource in hardening_manifest.resources:
+            log.info(f"resource: {resource}")  # TODO: Remove me.
             parsed_url = (
                 urlparse(resource["url"])
                 if "url" in resource
                 else urlparse(resource["urls"][0])
             )
+            log.info(f"parsed_url: {parsed_url}")  # TODO: Remove me
             scheme = parsed_url.scheme
+            log.info(f"scheme: {scheme}")  # TODO: Remove me
             netloc = parsed_url.netloc
+            log.info(f"netloc: {netloc}")  # TODO: Remove me
             artifact_type = get_artifact_type(resource, scheme, netloc)
+            log.info(f"artifact_type: {artifact_type}")  # TODO: Remove me
             artifact = set_artifact_path(artifact_type)
+            log.info(f"artifact: {artifact}")  # TODO: Remove me
+
             # download also gathers any relevant auth and runs any pre download validation
             artifact.download()
             if isinstance(artifact, AbstractFileArtifact):
@@ -143,6 +157,9 @@ def main():
         log.error("Unexpected runtime error occurred.")
     except Exception as e:  # pylint:  disable=broad-except
         log.error("Unexpected error occurred. Exception class: %s", e.__class__)
+        log.error(
+            "Unexpected error occurred. Exception class: %s", e
+        )  # TODO: Remove me
     finally:
         if artifact is not None and exit_code == 1:
             artifact.delete_artifact()
