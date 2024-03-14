4872cd0622802ccfa3da1843412f62d04255506b
c.geary <c.geary@wearemetronome.com>
2024-03-14 17:29:01 -0400

    ok

--- pipeline1/5-scan/twistlock_scan.sh
+++ pipeline1/5-scan/twistlock_scan.sh
@@ -8,18 +8,14 @@ do
   # amd64, arm64, ..
   PLATFORM=$(basename "$SCAN_LOGIC_DIR")
 
-  IMAGE_TO_SCAN=$(grep 'IMAGE_TO_SCAN' "$SCAN_LOGIC_DIR/scan_logic.env")
-  DIGEST_TO_SCAN=$(grep 'DIGEST_TO_SCAN' "$SCAN_LOGIC_DIR/scan_logic.env")
+  URI_BASENAME=$(awk -F'=' '/IMAGE_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env" | awk -F':' '{ print $1 }')
+  URI_DIGEST=$(awk -F'=' '/DIGEST_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env")
 
-  # scan by sha uri
-  URI_BASENAME=$(echo "$IMAGE_TO_SCAN" | awk -F':' '{ print $1 }')
-  URI_TO_SCAN="$URI_BASENAME@$DIGEST_TO_SCAN"
-
-  echo "scanning $PLATFORM ($URI_TO_SCAN)"
+  echo "scanning $PLATFORM (${$URI_BASENAME}@{$URI_DIGEST})"
   # artifacts
   mkdir -p "${ARTIFACT_DIR}/${PLATFORM}"
 
-  podman pull --authfile "${DOCKER_AUTH_FILE_PULL}" "${URI_TO_SCAN}"
+  podman pull --authfile "${DOCKER_AUTH_FILE_PULL}" "${$URI_BASENAME}@{$URI_DIGEST}"
   twistcli --version > "${ARTIFACT_DIR}/${PLATFORM}/twistlock-version.txt"
   twistcli images scan --address "${TWISTLOCK_URL}" --podman-path podman --custom-labels --output-file "${ARTIFACT_DIR}/${PLATFORM}/twistlock_cve.json" --details "${URI_TO_SCAN}" | tee "${ARTIFACT_DIR}/${PLATFORM}/twistcli-details.txt"
   ls "${ARTIFACT_DIR}/${PLATFORM}/twistlock_cve.json"
