31a93aa21e8e9eaa8041003f4b39b96a81e8f69d
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-15 14:16:19 -0500

    Added the path to the vat artifact dir

--- pipeline1/6-pre-publish/vat_import.py
+++ pipeline1/6-pre-publish/vat_import.py
@@ -426,6 +426,7 @@ def main(platform) -> None:
     else:
         parent_vat_response_content = {"parentVatResponses": None}
 
+    Path(f"{os.environ['ARTIFACT_DIR']}/{platform}").mkdir(parents=True, exist_ok=True)
     vat_request_json = Path(f"{os.environ['ARTIFACT_DIR']}/{platform}/vat_request.json")
     if not args.use_json:
         large_data = create_api_call(platform)
