c4baa734576e2cd69b806eede4b2bd10d22087d8
c.geary <c.geary@wearemetronome.com>
2024-03-18 14:06:24 -0400

    quote

--- pipeline1/0-setup/base_image_validation.py
+++ pipeline1/0-setup/base_image_validation.py
@@ -101,7 +101,7 @@ async def main():
 
         base_image_info = {"BASE_SHA": base_img_inspect["Digest"]}
         log.info("Dump SHA to file")
-        platform_artifact_dir = Path(f"{os.environ["ARTIFACT_DIR"]}/{platform}").mkdir(parents=True, exist_ok=True)
+        platform_artifact_dir = Path(f"{os.environ['ARTIFACT_DIR']}/{platform}").mkdir(parents=True, exist_ok=True)
         with Path(platform_artifact_dir, "base_image.json").open(
             "w", encoding="utf-8"
         ) as f:
