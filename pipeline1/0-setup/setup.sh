3122aed543d33d360ea67aabd714a0169038689f
c.geary <c.geary@wearemetronome.com>
2024-03-18 14:04:16 -0400

    echo

--- pipeline1/0-setup/setup.sh
+++ pipeline1/0-setup/setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-# wrap the setup jobs in a shell script to support pipefail
-
 ## image_inspect.py
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/image_inspect.py"
 
 ## lint
-set +e
-( python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/lint_jobs.py"; ) || (($?==100 ? 1 : 0)) && echo "lint OK"
-set -e
+python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/lint_jobs.py"
+echo $?
 
 ## trufflehog
 git config --global --add safe.directory "${CI_PROJECT_DIR}"
