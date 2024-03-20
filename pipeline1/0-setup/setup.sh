399da2742ac0c424de06da269798fde2dd566179
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-20 11:04:49 -0500

    setup.sh matches pipeline1

--- pipeline1/0-setup/setup.sh
+++ pipeline1/0-setup/setup.sh
@@ -1,13 +1,13 @@
+
 #!/bin/bash
 set -Eeuo pipefail
+
+## trufflehog
+git config --global --add safe.directory "${CI_PROJECT_DIR}"
+python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/trufflehog.py"
 
 ## image_inspect.py
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/image_inspect.py"
 
 ## lint
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/lint_jobs.py"
-echo $?
-
-## trufflehog
-git config --global --add safe.directory "${CI_PROJECT_DIR}"
-python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/trufflehog.py"
