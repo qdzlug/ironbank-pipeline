d42150c7529abb9e7cb86ee0eb12754be0d7e917
c.geary <c.geary@wearemetronome.com>
2024-03-20 09:21:40 -0400

    var

--- pipeline1/5-scan/twistlock_scan.sh
+++ pipeline1/5-scan/twistlock_scan.sh
@@ -2,23 +2,16 @@
 
 set -euo pipefail
 
+SCAN_LOGIC_DIR="$ARTIFACT_STORAGE/scan-logic/$PLATFORM"
+URI_BASENAME=$(awk -F'=' '/IMAGE_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env" | awk -F':' '{ print $1 }')
+URI_DIGEST=$(awk -F'=' '/DIGEST_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env")
 
-for SCAN_LOGIC_DIR in "$ARTIFACT_STORAGE/scan-logic"/*; do
+# artifacts
+mkdir -p "${ARTIFACT_DIR}"
 
-  # amd64, arm64, ..
-  PLATFORM=$(basename "$SCAN_LOGIC_DIR")
+echo "scanning $PLATFORM (${URI_BASENAME}@${URI_DIGEST})"
+podman pull --authfile "${DOCKER_AUTH_FILE_PULL}" "${URI_BASENAME}@${URI_DIGEST}"
+twistcli images scan --address "${TWISTLOCK_URL}" --podman-path podman --custom-labels --output-file "${ARTIFACT_DIR}/twistlock_cve.json" --details "${URI_BASENAME}@${URI_DIGEST}" | tee "${ARTIFACT_DIR}/twistcli-details.txt"
 
-  URI_BASENAME=$(awk -F'=' '/IMAGE_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env" | awk -F':' '{ print $1 }')
-  URI_DIGEST=$(awk -F'=' '/DIGEST_TO_SCAN/ { print $2 }' "$SCAN_LOGIC_DIR/scan_logic.env")
-
-  # artifacts
-  mkdir -p "${ARTIFACT_DIR}"
-
-  echo "scanning $PLATFORM (${URI_BASENAME}@${URI_DIGEST})"
-  podman pull --authfile "${DOCKER_AUTH_FILE_PULL}" "${URI_BASENAME}@${URI_DIGEST}"
-  twistcli images scan --address "${TWISTLOCK_URL}" --podman-path podman --custom-labels --output-file "${ARTIFACT_DIR}/twistlock_cve.json" --details "${URI_BASENAME}@${URI_DIGEST}" | tee "${ARTIFACT_DIR}/twistcli-details.txt"
-
-  ls "${ARTIFACT_DIR}/twistlock_cve.json"
-  chmod 0644 "${ARTIFACT_DIR}/twistlock_cve.json"
-
-done
+ls "${ARTIFACT_DIR}/twistlock_cve.json"
+chmod 0644 "${ARTIFACT_DIR}/twistlock_cve.json"
