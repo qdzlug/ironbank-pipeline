1e3a05afcc87f83d8d56335ed6806509873b5466
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-20 11:25:33 -0500

    Test

--- pipeline1/0-setup/setup.sh
+++ pipeline1/0-setup/setup.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-## trufflehog
-git config --global --add safe.directory "${CI_PROJECT_DIR}"
-python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/trufflehog.py"
-
 ## image_inspect.py
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/image_inspect.py"
 
 ## lint
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/lint_jobs.py"
+
+## trufflehog
+git config --global --add safe.directory "${CI_PROJECT_DIR}"
+python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/trufflehog.py"
