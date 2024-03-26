59fd9a6a89d2d3c58e1dcf7eeb7194669ddfad63
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-03-26 11:13:56 -0500

    Changed the call asyncio.run(base_image_validation.main()) to asyncio.run(base_image_validation.validate_base_image()) in pipeline1/0-setup/tests/test_base_image_validation2.py

--- pipeline1/0-setup/tests/test_base_image_validation2.py
+++ pipeline1/0-setup/tests/test_base_image_validation2.py
@@ -50,8 +50,8 @@ def test_base_image_validation_main(monkeypatch, caplog, raise_):
         base_image_validation.Cosign, "verify", lambda *args, **kwargs: True
     )
 
-    asyncio.run(base_image_validation.main())
-    log.info(f"{caplog.text}")
+    asyncio.run(base_image_validation.validate_base_image())
+    log.info(f"{caplog.text}") # TODO: Delete me
     assert "Dump SHA to file" in caplog.text
 
     monkeypatch.delenv("STAGING_BASE_IMAGE")
@@ -64,7 +64,8 @@ def test_base_image_validation_main(monkeypatch, caplog, raise_):
     monkeypatch.setattr(
         MockSkopeo, "inspect", lambda *args, **kwargs: {"Digest": "1234qwer"}
     )
-    asyncio.run(base_image_validation.main())
+    asyncio.run(base_image_validation.validate_base_image())
+    log.info(f"{caplog.text}") # TODO: Delete me
     assert "Dump SHA to file" in caplog.text
 
     log.info("Test cosign verify fails")
@@ -72,7 +73,8 @@ def test_base_image_validation_main(monkeypatch, caplog, raise_):
         base_image_validation.Cosign, "verify", lambda *args, **kwargs: False
     )
     with pytest.raises(SystemExit) as e:
-        asyncio.run(base_image_validation.main())
+        asyncio.run(base_image_validation.validate_base_image())
+    log.info(f"{caplog.text}") # TODO: Delete me
     assert e.value.code == 1
 
     log.info("Test base image validation throws exception")
@@ -83,5 +85,6 @@ def test_base_image_validation_main(monkeypatch, caplog, raise_):
         MockSkopeo, "inspect", lambda *args, **kwargs: raise_(GenericSubprocessError)
     )
     with pytest.raises(SystemExit) as e:
-        asyncio.run(base_image_validation.main())
+        asyncio.run(base_image_validation.validate_base_image())
+    log.info(f"{caplog.text}") # TODO: Delete me
     assert e.value.code == 1
