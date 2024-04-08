0f204c6f27a476f872770eec3757b64886fa6765
j.wuebbles <j.wuebbles@wearemetronome.com>
2024-04-08 12:52:06 -0500

    Added log message

--- pipeline1/0-setup/setup.sh
+++ pipeline1/0-setup/setup.sh
@@ -2,8 +2,10 @@
 set -Eeuo pipefail
 
 ## trufflehog
+ls # Remove me
 git config --global --add safe.directory "${CI_PROJECT_DIR}"
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/trufflehog.py"
+ls # Remove me
 
 ## image_inspect.py
 python3 "${PIPELINE_REPO_DIR}/pipeline1/0-setup/image_inspect.py"
